9f8f9a765bf1d9f0f95a6ff55319fba08804cae7
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-02-13 21:58:36 +0000

    More refinements to tag retrieval

--- application/controllers/TagsController.php
+++ application/controllers/TagsController.php
@@ -15,15 +15,7 @@ class TagsController extends Kea_Controller_Action
 	
 	public function browseAction()
 	{
-//		$tags = $this->_table->findAll();
-		
-		$item = Doctrine_Manager::getInstance()->getTable('Item')->find(1);
-	try{
-		$tags = $this->_table->getSome(100, true, false, $item);
-	}catch(Exception $e) {
-		echo $e->getMessage();exit;
-	}
-	
+		$tags = $this->_table->findAll();		
 		$this->render('tags/browse.php', compact('tags'));
 	}
 }
--- application/models/TagTable.php
+++ application/models/TagTable.php
@@ -20,8 +20,15 @@ class TagTable extends Doctrine_Table
 	}
 	
 	/**
-	 * @todo Sort by alphabetical, by count
-	 */
+	 * Retrieve a certain number of tags
+	 *
+	 * @param int limit
+	 * @param bool alphabetical order
+	 * @param bool ordered by count
+	 * @param Item only tags from this Item
+	 * @param User only tags from this User
+	 * @return Doctrine_Collection tags
+	 **/
 	public function getSome($limit = 100, $alpha = true, $count = false, $item = null, $user = null )
 	{
 		$query = $this->createQuery()->select('t.*, COUNT(t.id) tagCount')->from('Tag t')->limit($limit);
@@ -42,6 +49,33 @@ class TagTable extends Doctrine_Table
 		}
 		$query->groupby('t.id');
 		return $query->execute();
+	}
+	
+	/**
+	 * Overloaded as a wrapper for getSome()
+	 *
+	 * @return Doctrine_Collection
+	 **/
+	public function findAll($alpha = false, $count = false, $item = null, $user = null)
+	{
+		return $this->getSome($this->count(), $alpha, $count, $item, $user);
+	}
+	
+	/**
+	 * Overloaded to include tagCount within all retrieved tags
+	 *
+	 * @param int id
+	 * @return Tag
+	 **/
+	public function find($id) 
+	{
+		$id = (int) $id;
+		return $this->createQuery()
+					->select("t.*, COUNT(t.id) tagCount")
+					->from('Tag t')
+					->where('t.id = ?',array( $id ) )
+					->groupby('t.id')
+					->execute()->getFirst();
 	}
 } // END class TagTable extends Doctrine_Table
 
