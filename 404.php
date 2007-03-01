109b763901d86a1db57df051c8c25a9ac7d87cff
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-03-01 23:12:43 +0000

    See [947]

--- 404.php
+++ 404.php
@@ -1,10 +1,18 @@
+<?php
+if($_REQUEST['moreinfo']) {
+	Zend::dump( $e );
+}
+?>
+
+
 <h1>404 - Development Only</h1>
 
 <h3><?php echo $e->getMessage(); ?></h3>
 
 <h4>Backtrace: <?php Zend::dump($e->getTraceAsString()); ?></h4>
-<?php
-	
-	Zend::dump( $e );
 
-?>
+<form id="error-details">
+
+	<input type="checkbox" name="moreinfo" value="more" />
+	<input type="submit" name="submit" value="Dump Error" />
+</form>
