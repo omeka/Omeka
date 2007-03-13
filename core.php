44cc036ecbddf28588278e400c1d87d04d21bc9b
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-03-13 19:18:05 +0000

    Disable Zend_Search_Lucene until a solution for the search problem can be found

--- core.php
+++ core.php
@@ -32,11 +32,11 @@ $manager->setAttribute(Doctrine::ATTR_FETCHMODE, Doctrine::FETCH_LAZY);
 
 
 // tack on the search capabilities
-require_once 'Kea'.DIRECTORY_SEPARATOR.'SearchListener.php';
+//require_once 'Kea'.DIRECTORY_SEPARATOR.'SearchListener.php';
 require_once 'Kea'.DIRECTORY_SEPARATOR.'TimestampListener.php';
 $chainListeners = new Doctrine_EventListener_Chain();
 $chainListeners->add(new Kea_TimestampListener());
-$chainListeners->add(new Kea_SearchListener());
+//$chainListeners->add(new Kea_SearchListener());
 $manager->setAttribute(Doctrine::ATTR_LISTENER, $chainListeners);
 
 
