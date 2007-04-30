180a787f5b75ca48a304e4d88fa67c6cb2330372
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-04-30 19:09:26 +0000

    Fixes #60

--- admin/themes/default/collections/show.php
+++ admin/themes/default/collections/show.php
@@ -4,7 +4,7 @@
 	<?php nav(array('Browse Collections' => uri('collections'), 'Add a Collection' => uri('collections/add/'))); ?>
 </ul>
 <div id="collection-info">
-<h2>Collection: <?php echo $collection->name;?> <a class="edit" href="<?php echo uri('collections/edit/').$collection->id; ?>">(Edit)</a></h2>
+<h2>Collection: <?php echo $collection->name;?> <a class="edit" href="<?php echo uri('collections/edit/').$collection->id; ?>">(Edit)</a>  <a class="delete" href="<?php echo uri('collections/delete/').$collection->id; ?>">(Delete)</a></h2>
 <p><?php echo $collection->description; ?></p>
 </div>
 <div id="collection-items">
