bc48cfcc7314fb29fd262e932f8c9114ad65fed4
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-05-20 18:30:01 +0000

    Fixes #75, no way to delete an Item

--- admin/themes/default/items/edit.php
+++ admin/themes/default/items/edit.php
@@ -1,10 +1,22 @@
 <?php head();?>
 <?php common('archive-nav'); ?>
 <h2>Edit &#8220;<?php echo $item->title; ?>&#8221;</h2>
+<script type="text/javascript" charset="utf-8">
+	Event.observe(window,'load',function() {
+		$('delete_item').onclick = function() {
+			return confirm( 'Are you sure you want to delete this item, all of it\'s files, tags, and other data from the archive?' );
+		};
+	});
+</script>
+
 <form method="post" enctype="multipart/form-data">
 	<?php include 'form.php'; ?>
 	<fieldset>
 	<input type="submit" name="submit" value="Save Item" />
 	</fieldset>
+</form>
+
+<form action="<?php echo uri('items/delete/'.$item->id); ?>" method="post" accept-charset="utf-8">
+	<input type="submit" name="delete_item" id="delete_item" value="Delete This Item" />
 </form>
 <?php foot();?>
