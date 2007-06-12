a0755671132ba82be56af603adc535bc840185de
Jeremy Boggs <jeremy@clioweb.org>
2007-06-12 15:25:47 +0000

    deleted old, stupid page layouts.

--- shared/exhibit_layouts/gallery-full-right/form.php
+++
@@ -1,8 +0,0 @@
-<fieldset>
-	<?php 
-		text(array('name'=>'Text[1]'), page_text(1), 'Text Field'); 
-		text(array('name'=>'Item[2]', 'size'=>2), page_item_id(2), 'Enter an Item ID #');
-		text(array('name'=>'Text[2]'),page_text(2) , 'Text Field #2');
-	?>
-	
-</fieldset>
