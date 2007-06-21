a5c2270b38c450f6240c7f24c3698bc9a8656606
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-06-21 15:08:25 +0000

    Fixes #186, warning when deleting exhibits

--- admin/themes/default/exhibits/browse.php
+++ admin/themes/default/exhibits/browse.php
@@ -1,4 +1,17 @@
 <?php head(); ?>
+<script type="text/javascript" charset="utf-8">
+//<![CDATA[
+	Event.observe(window,'load',function() {
+		var deleteLinks = document.getElementsByClassName('delete-link');
+		for (var i=0; i < deleteLinks.length; i++) {
+			deleteLinks[i].onclick = function() {
+				return confirm( 'Are you sure you want to delete this exhibit and all of its data from the archive?' );
+			};
+		};
+	});
+//]]>	
+</script>
+
 <ul id="tertiary-nav" class="navigation">
 	<?php 
 		nav(array('Browse Exhibits' => uri('exhibits/browse'), 'Add Exhibit' => uri('exhibits/add')));
@@ -21,7 +34,7 @@
 		<td><a href="<?php echo uri('exhibits/show/'.$exhibit->id); ?>"><?php echo $exhibit->title; ?></a></td>
 		<td><?php echo tag_string($exhibit, uri('exhibits/browse/tag/')); ?></td>
 		<td><a href="<?php echo uri('exhibits/edit/'.$exhibit->id); ?>">[Edit]</a></td>
-		<td><a href="<?php echo uri('exhibits/delete/'.$exhibit->id); ?>">[Delete]</a></td>
+		<td><a class="delete-link" href="<?php echo uri('exhibits/delete/'.$exhibit->id); ?>">[Delete]</a></td>
 	</tr>
 <?php endforeach; ?>
 </tbody>
