2a943d10b14fa8b93b9c2ef8bc42e20ad3bcdd0a
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-06-25 17:40:56 +0000

    Also part of [1226]

---
+++ application/models/FileMetaLookup.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * FileMetaLookup
+ * @package: Omeka
+ */
+class FileMetaLookup extends Kea_Record
+{
+    public function setTableDefinition()
+    {
+		$this->option('type', 'MYISAM');
+		$this->hasColumn('mime_type', 'string', 255, array('notblank'=>true));
+		$this->hasColumn('table_name', 'string', 255, array('notblank'=>true));
+		$this->hasColumn('table_class', 'string', 255, array('notblank'=>true));
+		
+		$this->index('mime_type', array('fields' => array('mime_type')));
+    }
+    public function setUp()
+    {
+		$this->hasMany('File as Files', 'File.lookup_id');
+    }
+}
+
+?>
