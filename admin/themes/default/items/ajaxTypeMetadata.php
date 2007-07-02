83cdeb3fb91a0cd66521b4411cd74fccbfda0270
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-07-02 18:21:20 +0000

    Fixes #207

--- admin/themes/default/items/ajaxTypeMetadata.php
+++ admin/themes/default/items/ajaxTypeMetadata.php
@@ -1,7 +1,11 @@
 <?php 
 	$_POST['change_type'] = true;
 
-	$item = _make_omeka_request('Items','edit', array('id'=>$_REQUEST['id']), 'item');
+	if($_REQUEST['id']) {
+		$item = _make_omeka_request('Items','edit', array('id'=>$_REQUEST['id']), 'item');
+	}else {
+		$item = _make_omeka_request('Items', 'add', array(), 'item');
+	}
 	
 	metatext_form($item,'textarea');
 ?>
