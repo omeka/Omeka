f54b938df8d829402f2ad96a8a650bea57663e0b
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-07-23 18:15:08 +0000

    Removing geolocation plugin from trunk

--- plugins/GeoLocation/header.php
+++
@@ -1,7 +0,0 @@
-<?php $key = $this->getConfig('Google Maps API Key'); ?>
-<?php if($key): ?>
-
-<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=<?php echo $key;?>" type="text/javascript"></script>
-<script src="<?php echo $this->webPath() . DIRECTORY_SEPARATOR. 'map.js';?>" type="text/javascript" charset="utf-8"></script>
-
-<?php endif; ?>
