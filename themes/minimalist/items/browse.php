6da4cf658fbb72c954e37ea04267147faf0bfadf
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-08-24 21:42:58 +0000

    Fixing syntax error on minimalist theme

--- themes/minimalist/items/browse.php
+++ themes/minimalist/items/browse.php
@@ -4,6 +4,5 @@
 		<h1>Items</h1>
 			<div id="pagination"><?php echo pagination_links(); ?></div>
 				<?php display_item_list($items,false,false); ?>
-			<a href="<?php echo uri('items/show/'.$item->id); ?>"><?php echo thumbnail($item); ?></a>
 	</div>
 <?php foot(); ?>
