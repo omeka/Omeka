8df5276ca158852a63a73cd1639ef3b83392fd5d
Jeremy Boggs <jeremy@clioweb.org>
2007-08-24 14:44:24 +0000

    renamed exhibit themes.

--- themes/minimalist/items/browse.php
+++ themes/minimalist/items/browse.php
@@ -1,9 +1,9 @@
 <?php head(); ?>
 
 	<div id="primary">
-		<h2>Items</h2>
+		<h1>Items</h1>
 			<div id="pagination"><?php echo pagination_links(); ?></div>
 				<?php display_item_list($items,false,false); ?>
 			<a href="<?php echo uri('items/show/'.$item->id); ?>"><?php echo thumbnail($item); ?></a>
-
+	</div>
 <?php foot(); ?>
