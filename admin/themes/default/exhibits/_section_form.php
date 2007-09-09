679235c8ae2d2a08c6a698b09a11f1a9d35df25e
Jeremy Boggs <jeremy@clioweb.org>
2007-09-09 16:18:23 +0000

    minor bug fixes to exhibit builder.

--- admin/themes/default/exhibits/_section_form.php
+++ admin/themes/default/exhibits/_section_form.php
@@ -4,7 +4,7 @@
 	<div class="field"><?php text(array('name'=>'Section[title]', 'id'=>'title', 'class'=>'textinput'), $section->title, 'Section Title'); ?></div>
 	<?php /* ?><div class="field"><?php textarea(array('name'=>'Section[description]', 'id'=>'description', 'class'=>'textinput', 'rows'=>'10','cols'=>'40'), $section->description, 'Description for the Section'); ?></div>
 	<div class="field"><?php text(array('name'=>'Section[slug]', 'class'=>'textinput'), $section->slug, 'URL Slug (optional)'); ?></div><?php */ ?>
-	<p><button id="add_section" href="javascript:void(0);">Add This Section</button> or <a id="cancel-add" href="javascript:void(0);">Cancel</a></p>
+	<p><a id="add_section" href="javascript:void(0);">Add This Section</a> or <a id="cancel-add" href="javascript:void(0);">Cancel</a></p>
 	
 	</fieldset>
 	
