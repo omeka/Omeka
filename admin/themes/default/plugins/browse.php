c3ee2d45c09fc6e214fb01f0a36223f317e4ea9f
Dave Lester <davelester@gmail.com>
2007-09-17 20:27:31 +0000

    fixes #273, displays message instead of an error when no plugins installed

--- admin/themes/default/plugins/browse.php
+++ admin/themes/default/plugins/browse.php
@@ -6,7 +6,8 @@
 
 <form action="<?php echo uri('plugins/activate'); ?>" method="post" accept-charset="utf-8">
 <table>
-<?php foreach( $plugins as $key => $plugin ): ?>
+<?php if ($plugins): 
+	foreach( $plugins as $key => $plugin ): ?>
 	<tr>
 		<td><?php echo h($plugin->directory); ?></td>
 		<td><?php echo h($plugin->name); ?></td>
@@ -14,7 +15,10 @@
 		<td><?php echo h($plugin->author);?></td>
 		<td><button name="activate" type="submit" value="<?php echo $plugin->directory; ?>"><?php echo ($plugin->active) ? 'De-activate' : 'Activate'; ?></button>
 	</tr>
-<?php endforeach; ?>
+<?php endforeach; 
+	else:
+		echo "You don't have any plugins installed.  Add them to the plugins directory to see them listed here.";
+	endif; ?>
 </table>
 </form>
 
