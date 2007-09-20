873cb6b81e5d292c7fd195788d0ff57c6cac965d
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-09-20 21:53:11 +0000

    Make the upgrader script a little more user-friendly.

--- application/libraries/Kea/Upgrader.php
+++ application/libraries/Kea/Upgrader.php
@@ -20,11 +20,92 @@ class Kea_Upgrader
 		$this->start = $fromVersion;
 		$this->end = $toVersion;
 		
+		//Display a nice omeka header for the upgrade tool
+		$this->displayHeader();
+		
 		for ($i = $fromVersion+1; $i < $toVersion+1; $i++) { 
-			$this->upgrade($i);
+			
+			//Start capturing the output
+			ob_start();
+			try {
+				//Start the upgrade script
+				$this->upgrade($i);
+			} catch (Exception $e) {
+				
+				$error = "Error in Migration #$i" . "\n\n";
+				$error .= "Message: " . $e->getMessage() . "\n\n"; 
+				$error .= "Code: " . $e->getCode() . "\n\n";
+				$error .= "Line: " . $e->getLine() . "\n\n";
+				$error .= "Output from upgrade: ". ob_get_contents();
+				
+				//If there was a problem with the upgrade, display the error message 
+				//and email it to the administrator
+				$email = get_option('administrator_email');
+				
+				$header = 'From: '.$email. "\n" . 'X-Mailer: PHP/' . phpversion();
+				$title = "Omeka Upgrade Error";
+				$body = "This error was thrown when attempting to upgrade your Omeka installation:\n\n" . $error;
+				mail($email, $title, $body, $header);
+				$this->displayError($error);
+				
+				
+			}
 			$this->current = $i;
 			$this->incrementMigration();
+			
+			//Clean the contents of the output buffer
+			ob_end_clean();
+			
+			if(!isset($error)) {
+				$this->displaySuccess();
+			}
+			
+			unset($error);
+			
+			
 		}
+		
+		$this->displayFooter();
+	}
+	
+	public function displayError($text) {
+?>
+	<p class="error">Omeka encountered an error when upgrading your installation.  The full text of this error has been emailed to your administrator:</p>
+	
+	<p class="error_text"><?php echo htmlentities($text); ?></p>
+<?php 		
+	}
+	
+	public function displaySuccess() {
+		?>
+		<p class="success">Successfully migrated #<?php echo $this->current; ?></p>
+<?php		
+	}
+		
+	public function displayHeader() {
+		?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Upgrading Omeka</title>
+<style type="text/css" media="screen">
+	body {
+		font: arial, sans-serif;
+	}
+	
+</style>
+</head>
+
+<body>
+	<h2 class="instruction">Omeka is now upgrading itself.  Please refresh your screen once this page finishes loading.</h2>	
+<?php		
+	}
+	
+	public function displayFooter() {
+		?>
+	</body>	
+<?php		
 	}
 	
 	public function upgrade($version) {
