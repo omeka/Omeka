430bacf9f140a105980f59ee827fdff6103a84f3
Kristopher Kelly <kristopherbkelly@gmail.com>
2007-11-29 18:00:33 +0000

    Fix migration #12 in case people still need to run it

--- application/migrations/12/upgrade.php
+++ application/migrations/12/upgrade.php
@@ -1,4 +1,4 @@
 <?php 
-$this->query("ALTER TABLE `files_videos` CHANGE `bitrate` `bitrate` int(10) unsigned default NULL"); 
+get_db()->query("ALTER TABLE `files_videos` CHANGE `bitrate` `bitrate` int(10) unsigned default NULL"); 
 ?>
 
