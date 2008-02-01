439de179ceb7908c1d7a21ace3e8463edd6487d4
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-02-01 03:27:43 +0000

    Moving desertoasis theme from trunk to themes directory

--- themes/desertoasis/common/search.php
+++
@@ -1,7 +0,0 @@
-<div id="searchwrap" style="border: 2px solid #ffcccc">
-	<div class="padding">
-		<h3>Search The Archive</h3>
-		<?php echo simple_search(array('id'=>'simple-search','name'=>'simple-search'),uri('items/browse')); ?>
-
-	</div>
-</div>
--- themes/desertoasis/common/tagcloud.php
+++
@@ -1,6 +0,0 @@
-		<div id="tagcloud" class="clear">
-			<div class="padding">
-				<h2>Tags</h2>
-				<?php tag_cloud(recent_tags(), uri('items')); ?>
-			</div>
-		</div>
