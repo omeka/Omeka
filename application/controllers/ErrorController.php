e2d739ec508c05625f22e095a8d4e0fdfc221220
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-02-06 22:19:56 +0000

    Fix the ErrorController so that error/debug pages can take advantage of built-in view templates

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -10,9 +10,7 @@ class ErrorController extends Omeka_Controller_Action
 {
 	public function errorAction()
 	{
-		//Are we in debugging mode?
-		$debug = Zend_Registry::get('config_ini')->debug->exceptions;
-		
+		//Are we in debugging mode?		
 		$handler = $this->_getParam('error_handler');
 		
 		//The exception that barfed (may need to handle this differently in future)
@@ -23,29 +21,87 @@ class ErrorController extends Omeka_Controller_Action
 			$this->getRequest()->setParam('output', 'xhtml');
 		}		
 		
+		//Try to determine what kind of error occurred
 		switch ($handler->type) {
+			
 			//Errors that involve missing controller/action may be requests for static pages
 			case 'EXCEPTION_NO_CONTROLLER':
 			case 'EXCEPTION_NO_ACTION':
+				
 				try {
 					return $this->renderStaticPage($handler->request);
-				} catch (Exception $e) {}
+				} catch (Exception $e) {
+				    //If there is an exception thrown from this, it means render the 404 page
+				    return $this->render404();
+				}
+				
 				break;
 			default:
 				//Log errors that aren't just for pages that don't exist
 				Omeka_Logger::logError( $e );
+				
 				break;
 		}		
 				
-		if($debug) {
-			//The built-in global error page, used only when debugging (provides a dump of the exception)
-			include CORE_DIR . DIRECTORY_SEPARATOR .'404.php';
-			return;
-		}	
-		else {
-			//Try to render the theme's 404 page.  If the 404 page doesn't exist, no idea what happens
-			return $this->render('404.php');
-		}
+        return $this->renderOtherError($e);
+	}
+	
+	protected function renderOtherError(Exception $e)
+	{
+//	    $this->_view->setScriptPath(CORE_DIR . DIRECTORY_SEPARATOR . 'templates');
+	    
+	    if($this->isInDebugMode()) {
+	        ini_set('memory_limit', '64M');
+//	        Zend_Debug::dump( $this->_view );exit;
+	        return $this->renderCoreTemplate('errors/debug.php', compact('e'));
+	    }
+	    else {
+	        return $this->renderCoreTemplate('errors/index.php', compact('e'));
+	    }
+	}
+	
+	/**
+	 * We need to have this workaround for the Error Controller to be able to render pages that are built in to the app
+	 *
+	 * @duplication UpgradeController::init() and UpgradeController::render()
+	 * @return void
+	 **/
+	protected function renderCoreTemplate($file, $vars = array())
+	{
+        $this->_view = new Omeka_View($this);
+		$this->_view->addScriptPath(CORE_DIR . DIRECTORY_SEPARATOR . 'templates');
+		$this->_view->addAssetPath(
+		    CORE_DIR . DIRECTORY_SEPARATOR . 'templates', 
+		    WEB_ROOT . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'templates');
+		
+		require_once HELPERS;
+
+	    $this->_view->assign($vars);
+	    $body = $this->_view->render($file);
+	    $this->getResponse()->appendBody($body);
+	}
+	
+	/**
+	 * If we are in Debug mode, render the built-in 404 page.  Otherwise, render the theme's 404 page.
+	 *
+	 * @return void
+	 **/
+	protected function render404()
+	{
+	    $this->getResponse()->setHttpResponseCode(400);
+	    
+	    if($this->isInDebugMode()) {
+	        $badUri = $this->getRequest()->getRequestUri();
+	        return $this->renderCoreTemplate('errors/404.php', compact('badUri'));
+	    }
+	    else {
+	        return $this->render('404.php');
+	    }
+	}
+	
+	protected function isInDebugMode()
+	{
+	    return Zend_Registry::get('config_ini')->debug->exceptions;
 	}
 	
 	protected function renderStaticPage($req)
---
+++ application/core/templates/errors/404.php
@@ -0,0 +1,5 @@
+<?php head(array('title'=>'404!')); ?>
+    <h1>Omeka 404</h1>
+    
+    &quot;<?php echo $badUri ?>&quot; is not a valid URL.
+<?php foot(); ?>
