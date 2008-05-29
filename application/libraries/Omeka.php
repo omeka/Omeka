ddae5fa4a19d6ea12b03f3296c4011e8719ea97f
Jim Safley <jimsafley@gmail.com>
2008-05-29 15:13:08 +0000

    Cleaned up code.

--- application/libraries/Omeka.php
+++ application/libraries/Omeka.php
@@ -9,29 +9,28 @@
  **/
 final class Omeka
 {
-	static function autoload($classname)
-	{
-		if (class_exists($classname)) {
-			return false;
-		}
-
-		$path = dirname(__FILE__);
-		$class = $path . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR,$classname) . '.php';
-
-		if (file_exists($class)) {
-			require_once $class;
-			return;
-		}
-
-		return false;
-	}
-
-	/**
-	 * Convenience method returns the logged in user
-	 * or false depending on whether the user is 
-	 * logged in or not.
-	 */
+    static function autoload($classname)
+    {
+        if (class_exists($classname)) {
+            return false;
+        }
+        
+        $path = dirname(__FILE__);
+        $class = $path . DIRECTORY_SEPARATOR . str_replace('_', DIRECTORY_SEPARATOR,$classname) . '.php';
+        
+        if (file_exists($class)) {
+            require_once $class;
+            return;
+        }
+        return false;
+    }
+    
+    /**
+     * Convenience method returns the logged in user
+     * or false depending on whether the user is 
+     * logged in or not.
+     */
     static function loggedIn() {
         trigger_error('Omeka::loggedIn() has been deprecated!  Please use Omeka_Context::getInstance()->getCurrentUser() instead!');
-	}
+    }
 }
