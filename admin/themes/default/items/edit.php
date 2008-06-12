4f1201d0585e2ace0fa0aa64cc2d6b9fbec3b688
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-06-12 17:23:32 +0000

    Cleans up items/edit page on admin form.

--- admin/themes/default/items/edit.php
+++ admin/themes/default/items/edit.php
@@ -2,13 +2,20 @@
 <?php common('archive-nav'); ?>
 <div id="primary">
 	
-	<h1>Edit &#8220;<?php echo h($item->title); ?>&#8221;</h1>
+	<h1>Edit &#8220;<?php echo item('Title', ', '); ?>&#8221;</h1>
 
 	<form method="post" enctype="multipart/form-data" id="item-form">
 		<?php include 'form.php'; ?>
 		</div>
-		<p id="item-form-submits"><button type="submit" name="submit">Save Changes</button> or <a href="<?php echo uri("items/show/".$item->id); ?>" id="cancel_changes" class="cancel">Cancel</a></p>
-		<p id="delete_item_link"><a href="<?php echo uri('items/delete/'.$item->id); ?>" class="delete">Delete This Item</a></p>
+		<p id="item-form-submits">
+		    <button type="submit" name="submit">Save Changes</button> or 
+		    <?php echo link_to_item('show', 'Cancel', 
+		        array('id'=>'cancel_changes', 'class'=>'cancel')); ?>
+		</p>
+		<p id="delete_item_link">
+		    <?php echo link_to_item('delete', 'Delete This Item', 
+		        array('class'=>'delete')); ?>
+		</p>
 	</form>
 
 
