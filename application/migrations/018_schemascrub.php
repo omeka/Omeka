c60f94199a5df52b56f74db2acf07796c3c73566
Jim Safley <jimsafley@gmail.com>
2008-07-02 16:11:46 +0000

    Fixes migration 18.

--- application/migrations/018_schemascrub.php
+++ application/migrations/018_schemascrub.php
@@ -6,41 +6,41 @@ class SchemaScrub extends Omeka_Db_Migration
         $db = $this->db;
         $sql = "
         -- Alter character set
-        ALTER TABLE `$db->Taggings` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+        ALTER TABLE `{$db->prefix}taggings` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
         
         -- Alter engine
-        ALTER TABLE `$db->EntitiesRelations` ENGINE = MYISAM;
-        ALTER TABLE `$db->EntityRelationships` ENGINE = MYISAM;
-        ALTER TABLE `$db->FilesImages` ENGINE = MYISAM;
+        ALTER TABLE `{$db->prefix}entities_relations` ENGINE = MYISAM;
+        ALTER TABLE `{$db->prefix}entity_relationships` ENGINE = MYISAM;
+        ALTER TABLE `{$db->prefix}files_images` ENGINE = MYISAM;
         
         -- Alter data types
-        ALTER TABLE `$db->Collection` 
+        ALTER TABLE `{$db->prefix}collections` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;
-        ALTER TABLE `$db->Entity` 
+        ALTER TABLE `{$db->prefix}entities` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , 
             CHANGE `parent_id` `parent_id` INT UNSIGNED NULL DEFAULT NULL;
-        ALTER TABLE `$db->EntitiesRelations` 
+        ALTER TABLE `{$db->prefix}entities_relations` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `entity_id` `entity_id` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `relation_id` `relation_id` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `relationship_id` `relationship_id` INT UNSIGNED NULL DEFAULT NULL; 
-        ALTER TABLE `$db->EntityRelationships` 
+        ALTER TABLE `{$db->prefix}entity_relationships` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;  
         ALTER TABLE `{$db->prefix}exhibits` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;
-        ALTER TABLE `$db->File` 
+        ALTER TABLE `{$db->prefix}files` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `item_id` `item_id` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `size` `size` INT UNSIGNED NOT NULL DEFAULT '0',
             CHANGE `lookup_id` `lookup_id` INT UNSIGNED NULL DEFAULT NULL;
-        ALTER TABLE `$db->FilesImages` 
+        ALTER TABLE `{$db->prefix}files_images` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `width` `width` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `height` `height` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `bit_depth` `bit_depth` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `channels` `channels` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `file_id` `file_id` INT UNSIGNED NULL DEFAULT NULL;
-        ALTER TABLE `$db->FilesVideos` 
+        ALTER TABLE `{$db->prefix}files_videos` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `bitrate` `bitrate` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `duration` `duration` INT UNSIGNED NULL DEFAULT NULL ,
@@ -48,9 +48,9 @@ class SchemaScrub extends Omeka_Db_Migration
             CHANGE `file_id` `file_id` INT UNSIGNED NOT NULL ,
             CHANGE `width` `width` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `height` `height` INT UNSIGNED NULL DEFAULT NULL;
-        ALTER TABLE `$db->FileMetaLookup` 
+        ALTER TABLE `{$db->prefix}file_meta_lookup` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;
-        ALTER TABLE `$db->Item` 
+        ALTER TABLE `{$db->prefix}items` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `type_id` `type_id` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `collection_id` `collection_id` INT UNSIGNED NULL DEFAULT NULL; 
@@ -58,16 +58,16 @@ class SchemaScrub extends Omeka_Db_Migration
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `item_id` `item_id` INT UNSIGNED NULL DEFAULT NULL ,
             CHANGE `page_id` `page_id` INT UNSIGNED NOT NULL;
-        ALTER TABLE `$db->Metafield` 
+        ALTER TABLE `{$db->prefix}metafields` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `plugin_id` `plugin_id` INT UNSIGNED NULL DEFAULT NULL;
-        ALTER TABLE `$db->Metatext` 
+        ALTER TABLE `{$db->prefix}metatext` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `item_id` `item_id` INT UNSIGNED NOT NULL ,
             CHANGE `metafield_id` `metafield_id` INT UNSIGNED NOT NULL; 
-        ALTER TABLE `$db->Option` 
+        ALTER TABLE `{$db->prefix}options` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;  
-        ALTER TABLE `$db->Plugin` 
+        ALTER TABLE `{$db->prefix}plugins` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;  
         ALTER TABLE `{$db->prefix}sections` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
@@ -75,25 +75,25 @@ class SchemaScrub extends Omeka_Db_Migration
         ALTER TABLE `{$db->prefix}section_pages` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `section_id` `section_id` INT UNSIGNED NOT NULL; 
-        ALTER TABLE `$db->Taggings` 
+        ALTER TABLE `{$db->prefix}taggings` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `relation_id` `relation_id` INT UNSIGNED NOT NULL ,
             CHANGE `tag_id` `tag_id` INT UNSIGNED NOT NULL ,
             CHANGE `entity_id` `entity_id` INT UNSIGNED NOT NULL; 
-        ALTER TABLE `$db->Tag` 
+        ALTER TABLE `{$db->prefix}tags` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;  
-        ALTER TABLE `$db->Type` 
+        ALTER TABLE `{$db->prefix}types` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `plugin_id` `plugin_id` INT UNSIGNED NULL DEFAULT NULL; 
-        ALTER TABLE `$db->TypesMetafields` 
+        ALTER TABLE `{$db->prefix}types_metafields` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `type_id` `type_id` INT UNSIGNED NOT NULL ,
             CHANGE `metafield_id` `metafield_id` INT UNSIGNED NOT NULL ,
             CHANGE `plugin_id` `plugin_id` INT UNSIGNED NULL DEFAULT NULL; 
-        ALTER TABLE `$db->User` 
+        ALTER TABLE `{$db->prefix}users` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `entity_id` `entity_id` INT UNSIGNED NOT NULL; 
-        ALTER TABLE `$db->UsersActivations` 
+        ALTER TABLE `{$db->prefix}users_activations` 
             CHANGE `id` `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
             CHANGE `user_id` `user_id` INT UNSIGNED NOT NULL; 
         ";
