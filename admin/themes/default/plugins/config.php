9ac150da597d9df28af73c143aaf83be6671bbf2
Jim Safley <jimsafley@gmail.com>
2008-09-08 15:00:32 +0000

    Removes submit button style from the plugin config form.

--- admin/themes/default/plugins/config.php
+++ admin/themes/default/plugins/config.php
@@ -4,7 +4,7 @@
 <h2>Please Configure The <?php echo $plugin; ?> Plugin</h2>
 <form method="post">
 <?php echo $config; ?>
-<input type="submit" name="install_plugin" value="Save Configuration" class="submit submit-medium" />
+<input type="submit" name="install_plugin" value="Save Configuration" class="submit" />
 </form>
 </div>
 
