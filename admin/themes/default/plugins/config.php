2544615be70ae742d8218c3f86568d507bc41bce
Jim Safley <jimsafley@gmail.com>
2008-09-08 14:57:10 +0000

    Updates the plugin config form submit button.

--- admin/themes/default/plugins/config.php
+++ admin/themes/default/plugins/config.php
@@ -1,17 +1,11 @@
 <?php head(array('title'=>'Plugin Configuration', 'body_class'=>'plugins')); ?>
 
 <div id="primary">
-	
 <h2>Please Configure The <?php echo $plugin; ?> Plugin</h2>
-
 <form method="post">
-
 <?php echo $config; ?>
-
-<input type="submit" name="install_plugin" value="Save Configuration!" />
-
+<input type="submit" name="install_plugin" value="Save Configuration" class="submit submit-medium" />
 </form>
-
 </div>
 
 <?php foot(); ?>
