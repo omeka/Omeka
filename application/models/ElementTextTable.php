cb951c8dc41ff5fa1ffe184e888372bd5f84e665
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-09-16 18:46:40 +0000

    Removes unused code in ElementTextTable class and fixes a bug with element text being retrieved not in the correct order.

--- application/models/ElementTextTable.php
+++ application/models/ElementTextTable.php
@@ -16,22 +16,7 @@ class ElementTextTable extends Omeka_Db_Table
     
     protected $_alias = 'ie';
     
-    public function getSelectForItem($itemId=null)
-    {
-        $select = $this->getSelect();
-        $db = $this->getDb();
-        
-        // Join against the record_types table to only retrieve text for items.
-        $select->joinInner(array('rty'=>$db->RecordType), 
-            'rty.id = ' . $this->_alias . '.record_type_id AND rty.name = "Item"', array());
-        
-        $select->where( $this->_alias . '.record_id = ?', (int) $itemId);
-        
-        return $select;
-    }
-    
     /**
-     * @todo Refactor getSelectForItem() to use this instead.
      * @param integer
      * @param string
      * @return Omeka_Db_Select
@@ -46,6 +31,11 @@ class ElementTextTable extends Omeka_Db_Table
             'rty.id = ' . $this->_alias . '.record_type_id AND rty.name = "' . (string)$recordType . '"', array());
         
         $select->where( $this->_alias . '.record_id = ?', (int) $recordId);
+        
+        // Retrieve element texts ordered by ID, which is incremental.
+        // This means that element texts will be retrieved / displayed in the
+        // same order as they were saved to the database.
+        $select->order($this->_alias . '.id ASC');
         
         return $select;
     }
@@ -75,38 +65,5 @@ class ElementTextTable extends Omeka_Db_Table
         }
         
         return $this->_recordTypeId[$recordTypeName];
-    }
-    
-    /**
-     * @see Element::saveTextFor()
-     * @param integer
-     * @param integer
-     * @param integer
-     * @return array Set of ItemsElements records, some of which may be new.
-     **/
-    public function findOrNewByItemAndElement($itemId, $elementId, $numToFind)
-    {
-        $itemRecordTypeId = (int) $this->getRecordTypeId('Item');
-        
-        $records = array();
-        $select = $this->getSelectForItems($itemId);
-        
-        $select->where( $this->_alias . '.element_id = ?', $elementId);
-        
-        $select->limit($numToFind);
-        
-        $records = $this->fetchObjects($select);
-        
-        for ($i=0; $i < $numToFind; $i++) { 
-            if(!$records[$i]) {
-                $record = new ElementText;
-                $record->record_id = $itemId;
-                $record->element_id = $elementId;
-                $record->record_type_id = $itemRecordTypeId;
-                $records[$i] = $record;
-            }
-        }
-
-        return $records;
     }
 }
