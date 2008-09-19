d1bd1cfa281bc5a4351fc85f90f79bf25efecda0
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-09-19 19:15:13 +0000

    Fixes a bug where item type metadata was not being correctly retrieved by the ItemShow view helper.

--- application/views/scripts/items/item-metadata.php
+++ application/views/scripts/items/item-metadata.php
@@ -2,13 +2,17 @@
     <h2><?php echo $setName ?></h2>
     <?php foreach ($elementsInSet as $info): 
         $elementName = $info['elementName'];
+        $elementRecord = $info['element'];
         if ($info['isShowable']): ?>
     <div id="<?php echo text_to_id("$setName $elementName"); ?>" class="element">
         <h3><?php echo $elementName; ?></h3>
         <?php if ($info['isEmpty']): ?>
             <div class="element-text-empty"><?php echo $info['emptyText']; ?></div>
         <?php else: ?>
-        <?php foreach (item($elementName, array('all'=>true, 'element_set'=>$setName)) as $text): ?>
+        <?php   // We need to extract the element set name from the record b/c
+                // $setName contains the 'pretty' version of it that may be named differently
+                // than the actual element set. ?>
+        <?php foreach (item($elementName, array('all'=>true, 'element_set'=>$elementRecord->set_name)) as $text): ?>
             <div class="element-text"><?php echo $text; ?></div>
         <?php endforeach; ?>
         <?php endif; ?>
