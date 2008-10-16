6736f0ae2f58c3e83411bfec419e844e2dc8755e
Jeremy Boggs <jeremy@clioweb.org>
2008-10-16 18:41:37 +0000

    Improves design and layout for items/browse detailed view.

--- admin/themes/default/items/detailed-view.php
+++ admin/themes/default/items/detailed-view.php
@@ -2,24 +2,16 @@
 <?php while($item = loop_items()):?>
 <div class="item">
 	<h2><?php echo link_to_item(); ?></h2>
-
-	<div class="meta">
-		<ul>
-			<li><span class="fieldname">Creator:</span> <?php echo item('Dublin Core', 'Creator', array('delimiter'=>', ', 'all'=>true)); ?></li>
-			<li><span class="fieldname">Added:</span> <?php echo item('Date Added'); ?></li>
-			<?php if (item_belongs_to_collection()): ?>
-			<li><span class="fieldname">Collection:</span> <?php echo item('Collection Name'); ?></li>
-			<?php endif; ?>
-			<li><span class="fieldname">Public</span> <?php echo checkbox(array('name'=>"items[" . item('id') . "][public]",'class'=>"make-public"), item('Public')); ?></li>
-			<li><span class="fieldname">Featured</span> 
-			<?php echo checkbox(array('name'=>"items[" . item('id') . "][featured]",'class'=>"make-featured"), item('Featured')); ?>
-			<?php echo hidden(array('name'=>"items[" . item('id') . "][id]"), item('id')); ?>	
-			</li>
-		</ul>
-		<p><?php echo link_to_item('Edit', array('class'=>'edit'), 'edit'); ?></p>
-	</div>
-
-	<div class="description">
+	<p class="edit-item"><?php echo link_to_item('Edit', array('class'=>'edit'), 'edit'); ?>
+	
+	<ul class="public-featured-checkboxes">
+	<li><span class="fieldname">Public</span> <?php echo checkbox(array('name'=>"items[" . item('id') . "][public]",'class'=>"make-public"), item('Public')); ?></li>
+	<li><span class="fieldname">Featured</span> 
+	<?php echo checkbox(array('name'=>"items[" . item('id') . "][featured]",'class'=>"make-featured"), item('Featured')); ?>
+	<?php echo hidden(array('name'=>"items[" . item('id') . "][id]"), item('id')); ?>	
+	</li>
+	</ul>
+	<div class="item-description">
 	<?php if (!item_has_thumbnail()): ?>
 		<?php echo item('Dublin Core', 'Description', array('snippet'=>300)); ?>
 		<?php else: ?>
@@ -27,7 +19,16 @@
 		<?php echo item('Dublin Core', 'Description', array('snippet'=>300)); ?>
 	<?php endif; ?>
 	</div>
+	<div class="item-meta">
+		<ul>
+			<li><span class="fieldname">Creator:</span> <?php echo item('Dublin Core', 'Creator', array('delimiter'=>', ', 'all'=>true)); ?></li>
+			<li><span class="fieldname">Added:</span> <?php echo item('Date Added'); ?></li>
+			<li><span class="fieldname">Collection:</span> <?php if (item_belongs_to_collection()) echo item('Collection Name'); else echo 'No Collection'; ?></li>
+		</ul>
+	</div>
+	<div class="append-to-item-detail">
     <?php fire_plugin_hook('admin_append_to_items_browse_detailed_each'); ?>
+	</div>
 </div>
 <?php endwhile; ?>
 </div>
