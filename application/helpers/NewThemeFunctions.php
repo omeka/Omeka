dc3e6ca568918c1d5199ab37a4e74dbc400f4a4c
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-02-06 20:30:42 +0000

    Refs #459, the version check from api.omeka.org is now cached as an option in each omeka installation.
    This is done so that all the various Omeka installations do not poll the server 100 times a day.  Instead, get_latest_omeka_version() will only check once a day, and only for super users who access the dashboard.

--- application/helpers/NewThemeFunctions.php
+++ application/helpers/NewThemeFunctions.php
@@ -1361,21 +1361,36 @@ function strip_formatting($str, $allowableTags = '', $fallbackStr = '')
  * Retrieve the latest available version of Omeka by accessing the appropriate
  * URI on omeka.org.
  * 
- * @return string|false The latest available version of Omeka (or false if 
- * A) Omeka is up to date or B) The API service can't be reached.
+ * @return string|false The latest available version of Omeka, or false if the
+ * request failed for some reason.
  **/
 function get_latest_omeka_version()
 {
-    // $omekaApiUri = 'http://api.omeka.org/latest-version';
     $omekaApiUri = 'http://api.omeka.org/latest-version';
     $omekaApiVersion = '0.1';
     
-    try {
+    // Determine if we have already checked for the version lately.
+    $check = unserialize(get_option('omeka_update')) or $check = array();
+    // This a timestamp corresponding to the last time we checked for
+    // a new version.  86400 is the number of seconds in a day, so check
+    // once a day for a new version.
+    if (array_key_exists('last_updated', $check) 
+        and ($check['last_updated'] + 86400) > time()) {
+        // Return the value we got the last time we checked.
+        return $check['latest_version']; 
+    }
+    
+    try {   
         $client = new Zend_Http_Client($omekaApiUri);
         $client->setParameterGet('version', $omekaApiVersion);
         $client->setMethod('GET');
         $result = $client->request();
 	    if ($result->getStatus() == '200') {
+	        $latestVersion = $result->getBody();
+	        // Store the newer values 
+	        $check['latest_version'] = $latestVersion;
+	        $check['last_updated'] = time();
+	        set_option('omeka_update', serialize($check));
 	       return $result->getBody();
 	    } else {
 	       debug("Attempt to GET $omekaApiUri with version=$omekaApiVersion "
