c1853f55749f75b0e754ea80e7e498a4848a654f
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-02-11 20:58:47 +0000

    Refs #158, JSONP responses should all be given the 'text/javascript' MIME type.
    This is different from the regular JSON MIME type, which is 'application/json'.

--- application/libraries/Omeka/Controller/Action/Helper/ContextSwitch.php
+++ application/libraries/Omeka/Controller/Action/Helper/ContextSwitch.php
@@ -1,10 +1,13 @@
 <?php 
-
-/**
-* 
-*/
 class Omeka_Controller_Action_Helper_ContextSwitch extends Zend_Controller_Action_Helper_ContextSwitch
 {
+    
+    /**
+     * This extends the default ZF JSON serialization to work with JSONP, which
+     * enables cross-site AJAX using JSON.
+     * 
+     * @return void
+     **/
     public function postJsonContext()
     {
         parent::postJsonContext();
@@ -14,6 +17,9 @@ class Omeka_Controller_Action_Helper_ContextSwitch extends Zend_Controller_Actio
             $response = $this->getResponse();
             $json = $response->getBody();
             $response->setBody($callbackParam . '(' . $json . ')');
+            
+            // Also be sure to set the content header to 'text/javascript'.
+            $response->setHeader('Content-Type', 'text/javascript');
         }
-    }
+    }    
 }
