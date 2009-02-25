7909f588a9cc4a7a8b65b65469872c571d3a4ec4
Jeremy Boggs <jeremy@clioweb.org>
2009-02-25 18:36:01 +0000

    Fixes #485 file metadata page for public themes.

--- application/views/scripts/files/show.php
+++ application/views/scripts/files/show.php
@@ -8,30 +8,51 @@
 
 <?php echo show_file_metadata(); ?>
 
-<div id="format-metadata" class="section">
-<dl>
-<dt>Archive Filename:</dt> <dd><?php echo item_file('Archive Filename'); ?></dd>
-<dt>Original Filename:</dt> <dd><?php echo item_file('Original Filename'); ?></dd>
-<dt>File Size:</dt> <dd><?php echo item_file('Size'); ?> bytes</dd>
-</dl>
+<div id="format-metadata">
+    <div id="archive-filename" class="element">
+        <h3>Archive Filename</h3> 
+        <div class="element-text"><?php echo item_file('Archive Filename'); ?></div>
+    </div>
+
+    <div id="original-filename" class="element">
+        <h3>Original Filename</h3> 
+        <div class="element-text"><?php echo item_file('Original Filename'); ?></div>
+    </div>
+    
+    <div id="file-size" class="element">
+        <h3>File Size</h3> 
+        <div class="element-text"><?php echo item_file('Size'); ?> bytes</div>
+    </div>
 </div>
 
 <div id="type-metadata" class="section">
 <h2>Type Metadata</h2>
-<dl>
-<dt>Mime Type / Browser:</dt> <dd><?php echo item_file('MIME Type'); ?></dd>
-<dt>Mime Type / OS:</dt> <dd><?php echo item_file('MIME Type OS'); ?></dd>
-<dt>File Type / OS:</dt> <dd><?php echo item_file('File Type OS'); ?></dd>
-</dl>
+    <div id="mime-type-browser" class="element">
+        <h3>Mime Type / Browser:</h3> 
+        <div class="element-text"><?php echo item_file('MIME Type'); ?></div>
+    </div>
+    <div id="mime-type-os" class="element">
+        <h3>Mime Type / OS</h3> 
+        <div class="element-text"><?php echo item_file('MIME Type OS'); ?></div>
+    </div>
+    <div id="file-type-os" class="element">
+        <h3>File Type / OS</h3> 
+        <div class="element-text"><?php echo item_file('File Type OS'); ?></div>
+    </div>
 </div>
 	
 <div id="file-history" class="section">
 <h2>File History</h2>
-<dl>
-<dt>Date Added:</dt> <dd><?php echo item_file('Date Added'); ?></dd>
-<dt>Date Modified:</dt> <dd><?php echo item_file('Date Modified'); ?></dd>
-<dt>Authentication:</dt> <dd><?php echo item_file('Authentication'); ?></dd>
-</dl>
+<div id="date-added" class="element">
+<h3>Date Added</h3> 
+    <div class="element-text"><?php echo item_file('Date Added'); ?></div>
+    </div>
+<div id="date-modified" class="element">
+<h3>Date Modified</h3> <div class="element-text"><?php echo item_file('Date Modified'); ?></div>
+</div>
+<div id="authentication" class="element">
+<h3>Authentication</h3> <div class="element-text"><?php echo item_file('Authentication'); ?></div>
+</div>
 </div>
 
 </div><!--end primary-->
