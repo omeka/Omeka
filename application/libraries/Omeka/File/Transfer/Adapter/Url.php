304f7c8f9297984184026cabfadf674566e2161d
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-03-02 15:24:53 +0000

    Refs #651, refactors Url transfer adapter to use copy() if wget is not available.
    Note: this still requires testing to see if it works the other way on systems that DO have wget.

--- application/libraries/Omeka/File/Transfer/Adapter/Url.php
+++ application/libraries/Omeka/File/Transfer/Adapter/Url.php
@@ -6,6 +6,8 @@ class Omeka_File_Transfer_Adapter_Url implements Omeka_File_Transfer_Adapter_Int
 {
     protected $_fileInfo;
     
+    protected $_transferMethods = array('wget', 'copy');
+    
     public function setFileInfo(array $fileInfo)
     {
         $this->_fileInfo = $fileInfo;
@@ -15,15 +17,62 @@ class Omeka_File_Transfer_Adapter_Url implements Omeka_File_Transfer_Adapter_Int
     {
         return $this->_fileInfo['source'];
     }
-    
-    public function transferFile($source, $destination)
+        
+    protected function _wget($source, $destination)
     {
+        if (!$this->_isWgetAvailable()) {
+            return false;
+        }
+        
         // Only create the file if the URL is valid, otherwise the -O option 
         // will create an empty file, which is not expected behavior.
         $sourceArg      = escapeshellarg($source);
         $destinationArg = escapeshellarg($destination);
         $command        = "wget -O $destinationArg $sourceArg";
         exec($command, $output, $returnVar);
+        
+        return ($returnVar === 0);
+    }
+    
+    protected function _copy($source, $destination)
+    {
+        if (!$this->_canCopyFromUrl($source)) {
+            return false;
+        }
+
+        return copy($source, $destination);
+    }
+
+    protected function _isWgetAvailable()
+    {
+        exec('which wget', $output, $returnVar);
+        return !empty($output);      
+    }
+        
+    protected function _canCopyFromUrl($source)
+    {
+        // Only throw an exception here because this is our fallback.
+        if (!ini_get('allow_url_fopen')) {
+            throw new Exception('fopen stream wrappers must be enabled in order to copy files from a URL!');
+        }
+        
+        return true;
+    }
+    
+    public function transferFile($source, $destination)
+    {
+        $transferred = false;
+        foreach ($this->_transferMethods as $method) {
+            $classMethod = '_' . $method;
+            if ($transferred = $this->$classMethod($source, $destination)) {
+                break;
+            }
+        }
+        
+        if (!$transferred) {
+            throw new Exception('Could not transfer the file from "' . $source 
+                              . '" to "' . $destination . '"!');
+        }
     }
     
     public function isValid()
