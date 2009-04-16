9301bd925a0c345331304ee319bb9e10449c919e
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-04-16 21:50:05 +0000

    Moves the following deprecated functions to DeprecatedFunctions.php:     thumbnail(), fullsize(), square_thumbnail(), archive_image().

--- application/helpers/DeprecatedFunctions.php
+++ application/helpers/DeprecatedFunctions.php
@@ -52,3 +52,78 @@ function h($str, $allowedTags = "i|em|b|strong|del|span") {
 function unescapeTags($matches) {
   	return str_replace( array("&gt;", "&lt;", "&quot;", "&amp;"), array(">", "<", "\"", "&"), $matches[0]);
 }
+
+/**
+ * @deprecated
+ **/
+function thumbnail($record, $props=array(), $width=null, $height=null) 
+{
+       return archive_image($record, $props, $width, $height, 'thumbnail');
+}
+
+/**
+ * @deprecated
+ **/
+function fullsize($record, $props=array(), $width=null, $height=null)
+{
+       return archive_image($record, $props, $width, $height, 'fullsize');
+}
+
+/**
+ * @deprecated
+ **/
+function square_thumbnail($record, $props=array(), $width=null, $height=null)
+{
+       return archive_image($record, $props, $width, $height, 'square_thumbnail');
+}
+
+/**
+ * @deprecated Used internally by other theme helpers.  Implementation may change
+ * in future versions, do not rely on this within themes.
+ * @return string|false
+ **/
+function archive_image( $record, $props, $width, $height, $format) 
+{
+	if(!$record) {
+		return false;
+	}
+		
+       if($record instanceof File) {
+               $filename = $record->getDerivativeFilename();
+			   $file = $record;
+       }elseif($record instanceof Item) {
+               $file = get_db()->getTable('File')->getRandomFileWithImage($record->id);
+               if(!$file) return false;
+               $filename = $file->getDerivativeFilename();
+       }
+
+		$path = $file->getPath($format);
+		$uri = file_display_uri($file, $format);
+		
+	   if(!file_exists($path)) {
+			return false;
+	   }
+
+       list($oWidth, $oHeight) = getimagesize( $path );
+       if(!$width && !$height) 
+       {
+			$width = $oWidth;
+			$height = $oHeight;
+       }
+       elseif( $oWidth > $width && !$height )
+       {
+               $ratio = $width / $oWidth;
+               $height = $oHeight * $ratio;
+       }
+       elseif( !$width && $oHeight > $height)
+       {
+               $ratio = $height / $oHeight;
+               $width = $oWidth * $ratio;
+       }
+	   $props['width'] = $width;
+	   $props['height'] = $height;
+
+	   $html = '<img src="' . $uri . '" '._tag_attributes($props) . '/>' . "\n";
+	   return $html;
+}
+
