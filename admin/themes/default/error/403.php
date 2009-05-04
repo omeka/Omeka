9ed16bc3e24d8a276974dde37f66aa3b14d9e1c2
Jeremy Boggs <jeremy@clioweb.org>
2009-05-04 19:55:35 +0000

    Updates markup in error pages for consistency, changes the detailed view for items/browse to use square_thumbnail by default.

--- admin/themes/default/error/403.php
+++ admin/themes/default/error/403.php
@@ -1,4 +1,12 @@
-<?php head(); ?>
-<h1>403 - Forbidden</h1>
+<?php
+header ("HTTP/1.0 403 Forbidden"); 
+head(array('bodyclass'=>'error403 primary', 'title'=>'Forbidden')); 
+?>
+<h1>403: Forbidden</h1>
+
+<div id="primary" class="file-forbidden">
 <?php echo flash(); ?>
+
+<p>Sorry, because of your current user level (&#8220;<?php echo current_user()->role; ?>&#8221;), you don't have permission to access this page. If you think you should have access to this page, contact the <a href="mailto:<?php echo settings('administrator_email'); ?>">site administrator</a>.</p>
+</div>
 <?php foot(); ?>
