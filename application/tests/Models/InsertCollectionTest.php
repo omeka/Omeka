a22e30a2a7f90329ba5657459997110b590c98cd
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-06-08 14:55:06 +0000

    Adds first working test case for insert_collection().

--- application/tests/Models/InsertCollectionTest.php
+++ application/tests/Models/InsertCollectionTest.php
@@ -15,11 +15,22 @@ class InsertItemTypeTest extends PHPUnit_Framework_TestCase
         $this->_loadConfig();
         $this->_configDatabaseAdapter();
         $this->_buildDatabase();
+        $this->_buildOmekaDb();
     }
     
-    public function testIsTesting()
+    public function testCanInsertCollection()
     {
+        // Verify no collections exist.
         
+        $sql = "SELECT COUNT(*) FROM omeka_collections";
+        $count = $this->_zendDbAdapter->fetchOne($sql);
+        $this->assertEquals($count, 0);
+        
+        // Insert a collection and verify with a second query.
+        $collection = insert_collection(array('name'=>'Foo Bar', 'public'=>true, 'description'=>'foo'));
+        $sql = "SELECT id, public FROM omeka_collections";
+        $row = $this->_zendDbAdapter->fetchRow($sql);
+        $this->assertEquals(array('id'=>1, 'public'=>1), $row);
     }
         
     private function _buildDatabase()
@@ -74,5 +85,11 @@ class InsertItemTypeTest extends PHPUnit_Framework_TestCase
             $config = new Zend_Config_Ini(APP_DIR . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR.'config.ini', 'testing');
             Zend_Registry::set('test_config', $config);
         }
+    }
+    
+    private function _buildOmekaDb()
+    {
+        $omekaDb = new Omeka_Db($this->_zendDbAdapter, 'omeka_');
+        Omeka_Context::getInstance()->setDb($omekaDb);
     }
 }
