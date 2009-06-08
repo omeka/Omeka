03bfbb2642139eef179b288cc7e7220b7562274d
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-06-08 14:55:30 +0000

    Adds InsertItemTest to test insert_item().

---
+++ application/tests/Models/InsertItemTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2009
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka_Test
+ **/
+
+class InsertItemTest extends Omeka_Model_TestCase
+{
+    protected function _assertTableIsEmpty($tableName)
+    {
+        // Verify that there are no items in the test database.
+        $sql = "SELECT COUNT(*) FROM $tableName";
+        $count = $this->_zendDbAdapter->fetchOne($sql);
+        $this->assertEquals($count, 0);
+    }
+    
+    public function testCanInsertItem()
+    {
+        $this->_assertTableIsEmpty('omeka_items');
+        $this->_assertTableIsEmpty('omeka_element_texts');
+        
+        // Insert an item and verify with a second query.
+        $item = insert_item(
+            array('public'=>true), 
+            array('Dublin Core'=>array('Title'=>array(array('text'=>'foobar', 'html'=>true)))));
+        $sql = "SELECT id, public FROM omeka_items";
+        $row = $this->_zendDbAdapter->fetchRow($sql);
+        $this->assertEquals(array('id'=>1, 'public'=>1), $row);
+        
+        // Verify that element texts are inserted correctly into the database.
+        $sql = "SELECT COUNT(id) FROM omeka_element_texts WHERE html = 1 AND text = 'foobar'";
+        $this->assertEquals(1, $this->_zendDbAdapter->fetchOne($sql));
+    }
+}
