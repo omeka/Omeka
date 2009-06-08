8e9c476ff6c4f41a362db0de9dbe51c7f04d6aaa
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-06-08 18:03:53 +0000

    Adds a failing test case for #774.

---
+++ application/tests/All/Tickets/Ticket774Test.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* 
+*/
+class Tickets_Ticket774Test extends Omeka_Model_TestCase
+{
+    public function testItemFindByCanRetrieveNonPublicItems()
+    {
+        $publicItem = insert_item(array('public'=>true));
+        $nonPublicItem = insert_item(array('public'=>false));
+        
+        // This should only retrieve 1 item.
+        $items = get_db()->getTable('Item')->findBy(array('public'=>false));
+        $this->assertEquals(1, count($items), '2 items were retrieved instead of the expected 1.');
+    }
+}
