5c0883a62dab67854a46176a6a426ffb8597a6d2
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-06-08 14:55:26 +0000

    Adds failing test case for ticket #759.

---
+++ application/tests/Tickets/Ticket759Test.php
@@ -0,0 +1,21 @@
+<?php 
+class Ticket759Test extends Omeka_Model_TestCase
+{
+    public function testInsertItemTypeAndInsertElementSetHaveSimilarArguments()
+    {
+        // Insert an item type.
+        $itemType = insert_item_type(
+            array('name'=>'Foobar', 'description'=>'Changed description.'),
+            array(
+                array('name'=>'Wonder', 'data_type_name'=>'Text'), 
+                array('name'=>'Years',  'data_type_name'=>'Tiny Text')
+            ));
+            
+        $elementSet = insert_element_set(
+            array('name'=>'Foobar Element Set', 'description'=>'foobar'),
+            array(
+                array('name'=>'Element Name', 'description'=>'Element Description')
+            )
+        );
+    }
+}
