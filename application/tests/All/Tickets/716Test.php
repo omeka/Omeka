b715c4de9e437582eb066fb71bbd287d79291fa5
John Flatness <john@zerocrates.org>
2009-06-17 16:58:48 +0000

    Adds failing test case for ticket #716, as well as other snippet tests.

---
+++ application/tests/All/Tickets/716Test.php
@@ -0,0 +1,51 @@
+<?php 
+require_once HELPERS;
+
+class Tickets_716Test extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->reporting = error_reporting();
+        error_reporting(E_ALL);
+    }
+    
+    public function testSnippetEmptyText()
+    {
+        $this->assertEquals('', snippet('', 0, 15));
+    }
+    
+    public function testSnippetLongerThanText()
+    {
+        $this->assertEquals('text', snippet('text', 0, 15));
+    }
+    
+    public function testSnippetShorterThanFirstWord()
+    {
+        $this->assertEquals('…', snippet('text', 0, 2));
+    }
+    
+    public function testSnippetWithSpaces()
+    {
+        $this->assertEquals('this is some text', snippet('this is some text', 0, 250));
+    }
+    
+    public function testShortSnippetBreaksOnSpaces()
+    {
+        $this->assertEquals('this is…', snippet('this is some text', 0, 10));
+    }
+    
+    public function testSnippetWithAlternateAppend()
+    {
+        $this->assertEquals('this is...', snippet('this is some text', 0, 10, '...'));
+    }
+    
+    public function testSnippetWithNoAppend()
+    {
+        $this->assertEquals('this is', snippet('this is some text', 0, 10, ''));
+    }
+    
+    public function tearDown()
+    {
+        error_reporting($this->reporting);
+    }
+}
