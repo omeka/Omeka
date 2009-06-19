6c5e0bc429b82bc4f545fb44dae58411d85c895e
John Flatness <john@zerocrates.org>
2009-06-19 15:59:15 +0000

    Fixes #716, stops intermittent snippet() warnings on short text.

--- application/helpers/StringFunctions.php
+++ application/helpers/StringFunctions.php
@@ -45,23 +45,44 @@ function nls2p($str)
  * 
  * Note that this will only split strings on the space character.
  * 
- * @param string $text
- * @param int $startPos
- * @param int $endPos
- * @param string $append
- * @return string
- **/
+ * @param string $text Text to take snippet of
+ * @param int $startPos Starting position of snippet in string
+ * @param int $endPos Maximum length of snippet
+ * @param string $append String to append to snippet if truncated
+ * @return string Snippet of given text
+ */
 function snippet($text, $startPos, $endPos, $append = '…')
 {
-    $startPos = ( !$startPos ) ? 0 : strrpos( $text, ' ', $startPos - strlen($text) ); 
-    $endPos = strrpos( $text, ' ', ( $endPos ) - strlen($text) );
-    if (!$endPos) {
-        $endPos = strlen($text);  
-    } 
+    $textLength = strlen($text);
+
+    // Calculate the start position. Set to zero if the start position is
+    // null or 0, OR if the start offset is greater than the length of the
+    // original text.
+    $startPosOffset = $startPos - $textLength;
+    $startPos = !$startPos || $startPosOffset > $textLength
+                ? 0
+                : strrpos($text, ' ', $startPosOffset);
+
+    // Calculate the end position. Set to the length of the text if the
+    // end position is greater than or equal to the length of the original
+    // text, OR if the end offset is greater than the length of the
+    // original text.
+    $endPosOffset = $endPos - $textLength;
+    $endPos = $endPos >= $textLength || $endPosOffset > $textLength
+              ? $textLength
+              : strrpos($text, ' ', $endPosOffset);
+
+    // Set the snippet by getting its substring.
     $snippet = substr($text, $startPos, $endPos - $startPos);
-	if (strlen($snippet)) {
-		return  $snippet . $append; 
-	}
+
+    // Return the snippet without the append string if the text's original
+    // length equals to 1) the length of the snippet, i.e. when the return
+    // string is identical to the passed string; OR 2) the calculated
+    // end position, i.e. when the return string ends at the same point as
+    // the passed string.
+    return strlen($snippet) == $textLength || $endPos == $textLength
+            ? $snippet
+            : $snippet . $append;
 }
 
 /**
