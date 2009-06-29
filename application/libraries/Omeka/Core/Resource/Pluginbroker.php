5e945ab1bdd3c81b926c15b0778c70aa7e755ebe
John Flatness <john@zerocrates.org>
2009-06-29 21:07:25 +0000

    Adds dependency to Options resource for plugin broker, allows immediate access to options table.

--- application/libraries/Omeka/Core/Resource/Pluginbroker.php
+++ application/libraries/Omeka/Core/Resource/Pluginbroker.php
@@ -9,6 +9,9 @@ class Omeka_Core_Resource_Pluginbroker extends Zend_Application_Resource_Resourc
     {
         $bootstrap = $this->getBootstrap();
         $bootstrap->bootstrap('Db');
+        // Bootstrap the options as well, to make them immediately available
+        // to plugins.
+        $bootstrap->bootstrap('Options');
         // Initialize the plugin broker with the database object and the 
         // plugins/ directory
         $broker = new Omeka_Plugin_Broker($bootstrap->getResource('Db'), PLUGIN_DIR);   
