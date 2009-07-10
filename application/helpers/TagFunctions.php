49350b5440a6438b4ad16ab8fd807aeb344ef72a
Will Riley <wanderingwill@gmail.com>
2009-07-10 18:39:37 +0000

    Moves the tag functions from the LinkFunctions.php to the TagFunctions.php. Changes the item_tags_as_cloud and item_tags_as_string so that by default they sort the tags by alphabetical order, and so that they by default show all of the tags.

--- application/helpers/TagFunctions.php
+++ application/helpers/TagFunctions.php
@@ -32,12 +32,12 @@ function total_tags()
 /**
  * Returns the most recent tags.
  * 
- * @param integer $num The maximum number of recent tags to return
+ * @param integer $limit The maximum number of recent tags to return
  * @return array
  **/
-function recent_tags($num = 30) 
+function recent_tags($limit = 30) 
 {
-	return get_tags(array('recent'=>true), $num);
+	return get_tags(array('recent'=>true), $limit);
 }
 
 /**
@@ -53,4 +53,146 @@ function current_user_tags(Item $item)
         return false;
     }
     return get_tags(array('user'=>$user->id, 'record'=>$item, 'sort'=>array('alpha')));
+}
+
+/**
+ * Retrieve a tag cloud of all the tags for the current item.
+ * 
+ * @since 0.10
+ * @see item_tags_as_string()
+ * @param string|null $order Options include 'recent', 'most', 'least', 'alpha'.  
+ * If null, the tags will display in the order they were added to the database.
+ * @param boolean $tagsAreLinked Optional Whether or not to make each tag a link
+ * to browse all the items with that tag.  True by default.
+ * @param Item|null $item Check for this specific item record (current item if null).
+ * @param int|null $limit The maximum number of tags to return (get all of the tags if null)
+ * @return string
+ **/
+function item_tags_as_cloud($order = 'alpha', $tagsAreLinked = true, $item=null, $limit=null)
+{
+    if (!$item) {
+        $item = get_current_item();
+    }
+    $tags = get_tags(array('sort'=>$order, 'record'=>$item), $limit);
+    $urlToLinkTo = ($tagsAreLinked) ? uri('items/browse/tag/') : null;
+    return tag_cloud($tags, $urlToLinkTo);
+}
+
+/**
+ * Output the tags for the current item as a string.
+ * 
+ * @todo Should this take a set of parameters instead of $order?  That would be 
+ * good for limiting the # of tags returned by the query.
+ * @since 0.10
+ * @see item_tags_as_cloud()
+ * @param string $delimiter String that separates each tag.  Default is a comma 
+ * and space.
+ * @param string|null $order Options include 'recent', 'most', 'least', 'alpha'.  
+ * If null, the tags will display in the order they were added to the database.
+ * @param boolean $tagsAreLinked If tags should be linked or just represented as
+ * text.  Default is true.
+ * @param Item|null $item Check for this specific item record (current item if null).
+ * @param int|null $limit The maximum number of tags to return (get all of the tags if null)
+ * @return string HTML
+ **/
+function item_tags_as_string($delimiter = ', ', $order = 'alpha',  $tagsAreLinked = true, $item=null, $limit=null)
+{
+    if (!$item) {
+        $item = get_current_item();
+    }
+    $tags = get_tags(array('sort'=>$order, 'record'=>$item), $limit);
+    $urlToLinkTo = ($tagsAreLinked) ? uri('items/browse/tag/') : null;
+    return tag_string($tags, $urlToLinkTo, $delimiter);
+}
+
+/**
+ * Create a tag cloud made of divs that follow the hTagcloud microformat
+ *
+ * @param Omeka_Record|array $recordOrTags The record to retrieve tags from, or the actual array of tags
+ * @param string|null The URI to use in the link for each tag.  If none given,
+ *      tags in the cloud will not be given links.
+ * @return string HTML for the tag cloud
+ **/
+function tag_cloud($recordOrTags = null, $link = null, $maxClasses = 9)
+{
+	if (!$recordOrTags) {
+	    $recordOrTags = array();
+	}
+	
+	if ($recordOrTags instanceof Omeka_Record) {
+		$tags = $recordOrTags->Tags;
+	} else {
+		$tags = $recordOrTags;
+	}
+	
+	if (empty($tags)) {
+		$html = '<p>No tags are available.</p>';
+		return $html;
+	} 
+	
+	//Get the largest value in the tags array
+	$largest = 0;
+	foreach ($tags as $tag) {
+		if($tag["tagCount"] > $largest) {
+			$largest = $tag['tagCount'];
+		}
+	}
+	$html = '<div class="hTagcloud">';
+	$html .= '<ul class="popularity">';
+	
+	if ($largest < $maxClasses) {
+		$maxClasses = $largest;
+	}
+
+	foreach( $tags as $tag ) {
+		$size = (int)(($tag['tagCount'] * $maxClasses) / $largest - 1);
+		$class = str_repeat('v', $size) . ($size ? '-' : '') . 'popular';
+		$html .= '<li class="' . $class . '">';
+		if ($link) {
+			$html .= '<a href="' . $link . '?tags=' . urlencode($tag['name']) . '">';
+		}
+		$html .= html_escape($tag['name']);
+		if ($link) {
+			$html .= '</a>';
+		}
+		$html .= '</li>' . "\n";
+	}
+ 	$html .= '</ul></div>';
+
+	return $html;
+}
+
+/**
+ * Output a tag string given an Item, Exhibit, or a set of tags.
+ *
+ * @internal Any record that has the Taggable module can be passed to this function
+ * @param Omeka_Record|array $recordOrTags The record to retrieve tags from, or the actual array of tags
+ * @param string|null $link The URL to use for links to the tags (if null, tags aren't linked)
+ * @param string $delimiter ', ' (comma and whitespace) by default
+ * @return string HTML
+ **/
+function tag_string($recordOrTags = null, $link=null, $delimiter=', ')
+{
+	if (!$recordOrTags) {
+	    $recordOrTags = array();
+	}
+	
+	if ($recordOrTags instanceof Omeka_Record) {
+		$tags = $recordOrTags->Tags;
+	} else {
+		$tags = $recordOrTags;
+	}
+
+	$string = array();
+	if (!empty($tags)) {
+		foreach ($tags as $key=>$tag) {
+			if (!$link) {
+				$string[$key] = html_escape($tag['name']);
+			} else {
+				$string[$key] = '<a href="'.$link.urlencode($tag['name']).'" rel="tag">'.html_escape($tag['name']).'</a>';
+			}
+		}
+		$string = join($delimiter,$string);
+	}
+	return $string;
 }
