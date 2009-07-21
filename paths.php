a00ec08c764587c61fef0ac21b430e6df8c503e2
John Flatness <john@zerocrates.org>
2009-07-21 19:07:19 +0000

    Fixes #797, makes PHP send correct headers when compression is enabled.

--- paths.php
+++ paths.php
@@ -25,7 +25,7 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 // Set the zlib config values if the extension has been loaded.
 if (extension_loaded('zlib')) {
-    ini_set('zlib.output_compression', 'On');
+    ini_set('zlib.output_compression', true);
     ini_set('zlib.output_compression_level', '5');    
 }
 
