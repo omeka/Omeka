e7612f014ffd6fb14a2f285b81b356cbdc8e3cd6
Jeremy Boggs <jeremy@clioweb.org>
2009-08-14 15:32:08 +0000

    Changes Tags uri so the tab appears selected when navigation to Tags sub-pages.

--- admin/themes/default/common/primary-nav.php
+++ admin/themes/default/common/primary-nav.php
@@ -4,10 +4,8 @@
 	    'Items' => uri('items'), 
 	    'Collections' => uri('collections'),
 	    'Item Types' => uri('item-types'),
-	    'Tags' => uri('tags/browse')
-	    );
-
-				
+	    'Tags' => uri('tags')
+	    );				
 	echo nav(apply_filters('admin_navigation_main', $primaryNav));
 ?>
 </ul>
