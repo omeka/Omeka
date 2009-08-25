01e22a36b62e03b60b9f33e8c0ae563b0aaf4278
Will Riley <wanderingwill@gmail.com>
2009-08-25 17:40:41 +0000

    Adds the version column to the plugins table in the script that creates the database during installation.

--- install/install.sql.php
+++ install/install.sql.php
@@ -457,6 +457,7 @@ CREATE TABLE IF NOT EXISTS `{$db->prefix}plugins` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(255) collate utf8_unicode_ci NOT NULL,
   `active` tinyint(1) NOT NULL,
+  `version` text collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `active_idx` (`active`)
