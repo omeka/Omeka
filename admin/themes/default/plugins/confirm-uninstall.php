1209765308da8b601ab6025cc1d2f48b0003c81d
Jeremy Boggs <jeremy@clioweb.org>
2009-08-27 19:23:45 +0000

    Fixes some styling bugs with miscellaneous buttons throughout the admin theme, introduced in [4370].

--- admin/themes/default/plugins/confirm-uninstall.php
+++ admin/themes/default/plugins/confirm-uninstall.php
@@ -12,7 +12,7 @@
     <form action="<?php echo uri('plugins/uninstall'); ?>" method="post">
         <p><input type="checkbox" name="confirm" /> Yes, I want to uninstall this plugin.</p>
         <input type="hidden" name="name" value="<?php echo $pluginInfo->directoryName; ?>" />
-        <p><input type="submit" name="uninstall-confirm" class="uninstall submit-medium" value="Uninstall" /> or <a href="<?php echo uri('plugins') ?>">Cancel</a></p>
+        <p id="uninstall-confirm"><input type="submit" class="uninstall submit" name="uninstall-confirm" class="foo" value="Uninstall" /> <span>or <a href="<?php echo uri('plugins') ?>">Cancel</a></span></p>
     </form>
 </div>
 
