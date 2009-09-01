22d1cefd8ba54d41dcd83ec8e0ee1276299756ff
John Flatness <john@zerocrates.org>
2009-09-01 16:20:39 +0000

    Refs #786, adds migration missing from [4416].

---
+++ application/migrations/039_addShowEmptyElementsSetting.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Adds the `version` to the plugin table.
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2009
+ **/
+class addShowEmptyElementsSetting extends Omeka_Db_Migration
+{
+    public function up()
+    {
+        $optionsTable = "`{$db->prefix}options`";
+
+        // Sets the default for the 'show_empty_elements' setting to true for
+        // upgrades, unless a setting already exists.
+        $db = get_db(); 
+        $existingSql = "SELECT `id` FROM $optionsTable WHERE `name` = 'show_empty_elements'";
+        if (!$db->fetchOne($existingSql)) {
+            $addSql = "INSERT INTO $optionsTable (`name`, `value`) VALUES ('show_empty_elements', '1');";
+            $db->query($addSql);
+        }
+    }
+}
