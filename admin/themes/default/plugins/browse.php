159b09ac15c0e53c8d4d70489571b31efa478fd6
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-10-01 15:11:10 +0000

    Fixes admin plugins panel:     - display the human-readable name of the plugin where appropriate.     - properly signify errors for plugins that aren't loaded.

--- admin/themes/default/plugins/browse.php
+++ admin/themes/default/plugins/browse.php
@@ -31,8 +31,8 @@
     endforeach;
 
     $trClassName = null;
-    if ($plugin->isInstalled()):
-        if (($plugin->meetsOmekaMinimumVersion() || $missingPluginNames) && !$plugin->isLoaded()):
+    if ($plugin->isInstalled() && $plugin->isActive() && !$plugin->isLoaded()):
+        if (($plugin->meetsOmekaMinimumVersion() || $missingPluginNames)):
             $trClassName = "plugin-load-error"; 
         elseif ($plugin->hasNewVersion()): 
             $trClassName = "upgrade-plugin";
@@ -41,17 +41,17 @@
     ?>
     <tr <?php if ($trClassName): ?>class="<?php echo $trClassName; ?>"<?php endif; ?>>
         <td>
-            <?php if (!$plugin->meetsOmekaMinimumVersion() || $missingPluginNames): ?>
+            <?php if ($plugin->isInstalled() && $plugin->isActive() && !$plugin->isLoaded()): ?>
                     <div class="warnings">
-                        <strong>Warning!The <?php echo $plugin->getDirectoryName(); ?> plugin could not be loaded for the following reasons:</strong>
+                        <strong>Warning!The <?php echo $plugin->getDisplayName(); ?> plugin could not be loaded for the following reasons:</strong>
                         <ul>
                             <?php if (!$plugin->meetsOmekaMinimumVersion()): ?>
-                                <li class="omeka-minimum-version">The '<?php echo html_escape($plugin->getDirectoryName()); ?>' plugin requires at least Omeka <?php echo $plugin->getMinimumOmekaVersion(); ?>. You are using version Omeka <?php echo OMEKA_VERSION; ?>.</li>
+                                <li class="omeka-minimum-version">The '<?php echo html_escape($plugin->getDisplayName()); ?>' plugin requires at least Omeka <?php echo $plugin->getMinimumOmekaVersion(); ?>. You are using version Omeka <?php echo OMEKA_VERSION; ?>.</li>
                             <?php endif; ?>
 
                             <?php if ($missingPluginNames): ?>
                                 <li class="required-plugins">
-                                    The '<?php echo html_escape($plugin->getDirectoryName()); ?>' plugin requires 
+                                    The '<?php echo html_escape($plugin->getDisplayName()); ?>' plugin requires 
                                       the following plugins to be 
                                       installed, activated, and loaded: 
                                       <?php echo html_escape(implode_array_to_english($missingPluginNames)); ?> 
@@ -64,9 +64,9 @@
             <div class="plugin-info">
             <p class="plugin-title"><?php
                 if ($plugin->getLinkUrl()):
-                    echo '<a href="' . html_escape($plugin->getLinkUrl()) . '">' . html_escape($plugin->getDirectoryName()) . '</a>';
+                    echo '<a href="' . html_escape($plugin->getLinkUrl()) . '">' . html_escape($plugin->getDisplayName()) . '</a>';
                 else:
-                    echo html_escape($plugin->getDirectoryName());
+                    echo html_escape($plugin->getDisplayName());
                 endif; 
                 if (has_permission('Plugins', 'config') && $plugin->hasConfig() ): ?>
                     <a href="<?php echo uri('plugins/config', array('name'=>$plugin->getDirectoryName())); ?>" class="configure-button button">Configure</a>
@@ -91,10 +91,10 @@
 		    <?php endif; ?>
 		    
 		    <?php if ($plugin->hasNewVersion()): ?>    
-		        <p class="notice plugin-upgrade"><strong>Notice:</strong> You have a new version of <?php echo $plugin->getDirectoryName(); ?>. Please upgrade!</p>
+		        <p class="notice plugin-upgrade"><strong>Notice:</strong> You have a new version of <?php echo $plugin->getDisplayName(); ?>. Please upgrade!</p>
 		    <?php endif; ?>
 		    <?php if (!$plugin->meetsOmekaTestedUpToVersion()): ?>
-                <p class="notice omeka-tested-up-to"><strong>Notice:</strong> This version of the '<?php echo html_escape($plugin->getDirectoryName()); ?>' plugin has only been tested up to Omeka <?php echo $plugin->getTestedUpToOmekaVersion(); ?>. You are using version Omeka <?php echo OMEKA_VERSION; ?>.</p>
+                <p class="notice omeka-tested-up-to"><strong>Notice:</strong> This version of the '<?php echo html_escape($plugin->getDisplayName()); ?>' plugin has only been tested up to Omeka <?php echo $plugin->getTestedUpToOmekaVersion(); ?>. You are using version Omeka <?php echo OMEKA_VERSION; ?>.</p>
             <?php endif; ?>
             </div>
         </td>
