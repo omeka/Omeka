d98669b9668224ace9d9606c75bb947ecb1e13e0
Kristopher Kelly <kristopherbkelly@gmail.com>
2009-10-12 18:01:05 +0000

    Refs #838, fixes pagination_links(), adds test case.

--- application/views/scripts/common/pagination_control.php
+++ application/views/scripts/common/pagination_control.php
@@ -4,20 +4,20 @@
     <?php if ($this->first != $this->current): ?>
     <!-- First page link --> 
     <li class="pagination_first">
-    <a href="<?php echo $this->url(array('page' => $this->first), null, $_GET); ?>">First</a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->first), null, $_GET)); ?>">First</a>
     <?php endif; ?>
     
     <?php if (isset($this->previous)): ?>
     <!-- Previous page link --> 
     <li class="pagination_previous">
-    <a href="<?php echo $this->url(array('page' => $this->previous), null, $_GET); ?>">Previous</a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>">Previous</a>
     </li>
     <?php endif; ?>
     
     <!-- Numbered page links -->
     <?php foreach ($this->pagesInRange as $page): ?> 
     <?php if ($page != $this->current): ?>
-    <li class="pagination_range"><a href="<?php echo $this->url(array('page' => $page), null, $_GET); ?>"><?php echo $page; ?></a></li>
+    <li class="pagination_range"><a href="<?php echo html_escape($this->url(array('page' => $page), null, $_GET)); ?>"><?php echo $page; ?></a></li>
     <?php else: ?>
     <li class="pagination_current"><?php echo $page; ?></li>
     <?php endif; ?>
@@ -26,14 +26,14 @@
     <?php if (isset($this->next)): ?> 
     <!-- Next page link -->
     <li class="pagination_next">
-    <a href="<?php echo $this->url(array('page' => $this->next), null, $_GET); ?>">Next</a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>">Next</a>
     </li>
     <?php endif; ?>
     
     <?php if ($this->last != $this->current): ?>
     <!-- Last page link --> 
     <li class="pagination_last">
-    <a href="<?php echo $this->url(array('page' => $this->last), null, $_GET); ?>">Last</a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->last), null, $_GET)); ?>">Last</a>
     </li>
     <?php endif; ?>
 </ul>
