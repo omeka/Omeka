2983a03f4f5bc109a20b750169e33c18850e32b1
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-01-07 22:00:37 +0000

    Adds info about matched routes to Debug controller plugin.

--- application/libraries/Omeka/Controller/Plugin/Debug.php
+++ application/libraries/Omeka/Controller/Plugin/Debug.php
@@ -57,13 +57,25 @@ class Omeka_Controller_Plugin_Debug extends Zend_Controller_Plugin_Abstract
         
         $html .= "<h2>Routing Data</h2>";
         $html .= "<div>Current Route: <strong>$currentRoute</strong></div>";
-        $html .= "<div>Defined routes:<ul>";
+        $html .= "<div>Defined routes:\n\n";
         
+        $html .= "<table><tr><th>Route Name</th><th>Matches Current Request</th><th>Assembled with current params</th></tr>";
         foreach ($routes as $routeName => $route) {
-            $html .= "<li>" . $routeName . "</li>";
+            try {
+                $assembledRoute = $route->assemble($request->getParams(), true, true);
+            } catch (Exception $e) {
+                $assembledRoute = "Could not assemble: " . $e->getMessage();
+            }
+            if ($route instanceof Zend_Controller_Router_Route_Chain) {
+                $routeIsMatched = $route->match($request);
+            } else {
+                $routeIsMatched = $route->match($request->getPathInfo());
+            }
+            
+            $html .= "<tr><td>$routeName</td><td>" . ($routeIsMatched ? 'true' : 'false') . "</td><td>$assembledRoute</td></tr>";
         }
                 
-        $html .= "</ul>";
+        $html .= "</table>";
         
         $html .= "<h2>Cookie Data</h2>";
         $html .= '<pre>' . print_r($_COOKIE, true) . '</pre>';
