1952c8035ab2208eb109132847b0fd9163f8dd8f
Jeremy Boggs <jeremy@clioweb.org>
2010-01-19 18:17:28 +0000

    Fixes #812 get_collections() does not return non-public collections; Fixes #813 get_collections() does not return collections that are public and featured.

--- application/models/CollectionTable.php
+++ application/models/CollectionTable.php
@@ -16,6 +16,15 @@ class CollectionTable extends Omeka_Db_Table
 {    
     public function applySearchFilters($select, $params)
     {
+        
+        if(isset($params['public'])) {
+            $this->filterByPublic($select, $params['public']);
+        }
+        
+        if(isset($params['featured'])) {
+            $this->filterByFeatured($select, $params['featured']);
+        }
+        
         /****************************
          * FIND RECENT COLLECTIONS
          *
@@ -55,4 +64,42 @@ class CollectionTable extends Omeka_Db_Table
         $select = $this->getSelect()->where("c.featured = 1")->order("RAND()")->limit(1);        
         return $this->fetchObject($select);
     }    
+    
+    /**
+     * Apply a filter to the collections based on whether or not they are public
+     * 
+     * @param Zend_Db_Select
+     * @param boolean Whether or not to retrieve only public collections
+     * @return void
+     **/
+    public function filterByPublic($select, $isPublic)
+    {         
+        $isPublic = (bool) $isPublic; // this makes sure that empty strings and unset parameters are false
+
+        //Force a preview of the public collections
+        if ($isPublic) {
+            $select->where('c.public = 1');
+        } else {
+            $select->where('c.public = 0');
+        }
+    }
+    
+    /**
+     * Apply a filter to the collections based on whether or not they are featured
+     * 
+     * @param Zend_Db_Select
+     * @param boolean Whether or not to retrieve only public collections
+     * @return void
+     **/
+    public function filterByFeatured($select, $isFeatured)
+    {
+        $isFeatured = (bool) $isFeatured; // this make sure that empty strings and unset parameters are false
+        
+        //filter items based on featured (only value of 'true' will return featured collections)
+        if ($isFeatured) {
+            $select->where('c.featured = 1');
+        } else {
+            $select->where('c.featured = 0');
+        }     
+    }
 }
