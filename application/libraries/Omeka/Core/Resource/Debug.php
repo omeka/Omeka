e91e613417a7b9d3daf83c1eeeb13b471cf6adc3
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-02-08 16:43:26 +0000

    Fixes debug.exceptions config setting so that it displays exceptions in the 'debug' view script.

--- application/libraries/Omeka/Core/Resource/Debug.php
+++ application/libraries/Omeka/Core/Resource/Debug.php
@@ -17,19 +17,9 @@ class Omeka_Core_Resource_Debug extends Zend_Application_Resource_ResourceAbstra
     public function init()
     {
         $bootstrap = $this->getBootstrap();
-        $bootstrap->bootstrap('Config');
         $bootstrap->bootstrap('FrontController');
         $front = $bootstrap->getResource('FrontController');
-        $config = $bootstrap->getResource('Config');
-        
-        // Uncaught exceptions should bubble up to the browser level since we
-        // are essentially in debug/install mode. Otherwise, we should make use
-        // of the ErrorController, which WILL NOT LOAD IF YOU ENABLE EXCEPTIONS
-        // (took me awhile to figure this out).
-        if ((boolean)$config->debug->exceptions) {
-            $front->throwExceptions(true);  
-        }
-        
+                
         // This plugin allows for debugging request objects without inserting 
         // debugging code into the Zend Framework code files.        
         $front->registerPlugin(new Omeka_Controller_Plugin_Debug);
