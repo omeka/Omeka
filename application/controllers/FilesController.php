96b64970cb6a72d1ec0739032532a135c8b32b9a
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-03-23 21:37:51 +0000

    Refs #890, reduces a CPU/DB bottleneck from file URLs being loaded via Omeka instead of directly by Apache.     * Deprecates file_download_uri()     * Removes 'download' and 'display' routes from routes.ini.     * Removes get action from files controller.

--- application/controllers/FilesController.php
+++ application/controllers/FilesController.php
@@ -67,41 +67,5 @@ class FilesController extends Omeka_Controller_Action
         
         Zend_Registry::set('file', $file);
         $this->view->assign(compact('file'));
-    }
-    
-    protected function isValidFormat($format)
-    {
-        return in_array($format, array('fullsize', 'thumbnail', 
-                                       'archive', 'square_thumbnail'));
-    }
-    
-    protected function isValidDisposition($type)
-    {
-        return in_array($type, array('attachment', 'inline'));
-    }
-    
-    public function getAction()
-    {
-        $format = $this->_getParam('format');
-        
-        if (!$this->isValidFormat($format)) {
-            $this->forbiddenAction();
-        }
-        
-        $file = $this->findById();
-        
-        // If we don't have any images associated with this file, then use the 
-        // full archive path
-        if (!$file->has_derivative_image) {
-            $format = 'archive';
-        }
-        
-        // Otherwise use the chosen format of the image
-        $path = $file->getWebPath($format);
-        
-        header('Location: '.$path);
-        
-        //Don't render anything
-        $this->_helper->viewRenderer->setNoRender();
-    }
+    }    
 }
