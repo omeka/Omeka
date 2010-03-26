ef695f93d9e52d2b74bbc9a10bb31ce2adfc1166
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-03-26 20:13:57 +0000

    Adds a test case for changing user passwords.

---
+++ application/tests/integration/Controllers/ChangePasswordTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ **/
+
+/**
+ * 
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ **/
+class Omeka_Controllers_ChangePasswordTest extends Omeka_Test_AppTestCase
+{
+    const CURRENT_PASSWORD = 'foobar123';
+    
+    public function setUp()
+    {
+        parent::setUp();
+                
+        // Set the ACL to allow access to users.
+        $acl = $this->core->getBootstrap()->getResource('Acl');
+        $acl->allow(null, 'Users');
+        
+        $this->db = $this->core->getBootstrap()->db;
+        $this->user = $this->db->getTable('User')->find(1);
+        $this->salt = $this->user->salt;
+    }
+
+    public function assertPreConditions()
+    {
+        $this->assertTrue($this->user instanceof User);
+        $this->assertTrue($this->user->exists());
+        $this->assertNotNull($this->salt, "Salt not being set properly by installer.");
+        $this->_assertPasswordNotChanged();
+    }
+    
+    public function testChangingPassword()
+    {
+        $this->getRequest()->setPost(array(
+            'old_password'  => self::CURRENT_PASSWORD,
+            'new_password1' => 'foobar6789',
+            'new_password2' => 'foobar6789'
+        ));
+        $this->getRequest()->setMethod('post');
+        $this->dispatch('/users/change-password/1', true);
+        $this->_assertSaltNotChanged();
+        $this->_assertPasswordIs('foobar6789');
+    }
+    
+    public function testChangingPasswordFailsWithInvalidPassword()
+    {
+        $this->getRequest()->setPost(array(
+            'old_password'  => 'wrongpassword',
+            'new_password1' => 'foo',
+            'new_password2' => 'foo'
+        ));
+        $this->getRequest()->setMethod('post');
+        $this->dispatch('/users/change-password/1', true);
+        $this->_assertPasswordNotChanged();
+    }
+    
+    public function testChangePasswordFailsIfPasswordNotConfirmed()
+    {
+        $this->getRequest()->setPost(array(
+            'old_password'  => 'foobar123',
+            'new_password1' => 'foo'
+        ));
+        $this->getRequest()->setMethod('post');
+        $this->dispatch('/users/change-password/1', true);
+        $this->_assertPasswordNotChanged();
+    }
+    
+    private function _assertPasswordNotChanged()
+    {
+        $this->_assertPasswordIs(self::CURRENT_PASSWORD,
+                                 "Hashed password should not have changed.");
+    }
+    
+    private function _assertPasswordIs($pass, $msg = null)
+    {
+        $this->assertEquals($this->db->fetchOne("SELECT password FROM omeka_users WHERE id = 1"),
+                            $this->user->hashPassword($pass),
+                            $msg);
+    }
+    
+    private function _assertSaltNotChanged()
+    {
+        $this->assertEquals($this->db->fetchOne("SELECT salt FROM omeka_users WHERE id = 1"),
+                                                $this->salt,
+                                                "Salt should not have changed.");
+    }    
+}
