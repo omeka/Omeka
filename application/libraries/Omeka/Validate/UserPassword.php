1b48c36c92ba6dfedbbce4bdc081cd9c4fd15705
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-04-21 18:11:18 +0000

    Adds an error message template to Omeka_Validate_UserPassword.

--- application/libraries/Omeka/Validate/UserPassword.php
+++ application/libraries/Omeka/Validate/UserPassword.php
@@ -14,6 +14,12 @@
  **/
 class Omeka_Validate_UserPassword extends Zend_Validate_Abstract
 {
+    const INVALID = 'invalid';
+    
+    protected $_messageTemplates = array(
+        self::INVALID => "Password is invalid."
+    );
+    
     private $_user;
     
     public function __construct(User $user)
@@ -24,6 +30,10 @@ class Omeka_Validate_UserPassword extends Zend_Validate_Abstract
     public function isValid($value, $context = null)
     {
         assert('$this->_user->password !== null');
-        return $this->_user->hashPassword($value) === $this->_user->password;
+        $valid = $this->_user->hashPassword($value) === $this->_user->password;
+        if (!$valid) {
+            $this->_error(self::INVALID);
+        }
+        return $valid;
     }
 }
