a2777b657e7024c4ae4823bce47e376f2d10d350
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-05-10 21:31:47 +0000

    Adds tests for forgot-password action in UsersController.

--- application/tests/integration/Controllers/UsersControllerTest.php
+++ application/tests/integration/Controllers/UsersControllerTest.php
@@ -49,15 +49,54 @@ class Omeka_Controller_UsersControllerTest extends Omeka_Test_AppTestCase
         $this->getRequest()->setPost($post);
         $this->getRequest()->setMethod('post');
         $this->dispatch('users/add', true);
-        $mail = $this->_getFakemailFilenames();
-        $this->assertEquals(count($mail), 1);
-        $mailText = file_get_contents("$this->fakemail/{$mail[0]}");
+        $mailText = $this->_getSentMailText();
         $this->assertThat($mailText, $this->stringContains("Activate your account"));
+    }
+    
+    public function testShowForgotPasswordPage()
+    {
+        $this->dispatch('users/forgot-password');
+        $this->assertNotRedirect();
+        $this->assertQuery('form #email');
+    }
+    
+    public function testForgotPasswordForInvalidEmail()
+    {
+        $invalidEmail = 'bananabanana@example.com';
+        $this->request->setPost(array(
+            'email' => $invalidEmail
+        ));
+        $this->request->setMethod('post');
+        $this->dispatch('users/forgot-password');
+        $this->assertNotRedirect();
+        $this->assertQueryContentContains("div.error", "email address", 
+            "The form should have responded with an error message indicating that the email address was not found.");
+    }
+    
+    public function testSendingEmailForForgottenPassword()
+    {
+        $this->request->setPost(array(
+            'email' => Omeka_Test_Resource_Db::SUPER_EMAIL
+        ));
+        $this->request->setMethod('post');
+        $this->dispatch('users/forgot-password');
+        $mail = $this->_getSentMailText();
+        $this->assertThat($mail, $this->stringContains("Subject: [Automated Test Installation] Reset Your Password"));
+        
+        // FIXME: fails because flash is not reset between tests.
+        $this->assertQueryContentContains("div.success", "Please check your email");
     }
             
     private function _getFakemailFilenames()
     {
         $iter = new VersionedDirectoryIterator($this->fakemail, false);
         return $iter->getValid();
+    }
+    
+    private function _getSentMailText()
+    {
+        $mail = $this->_getFakemailFilenames();
+        $this->assertEquals(count($mail), 1);
+        return file_get_contents("$this->fakemail/{$mail[0]}");
     }
 }
