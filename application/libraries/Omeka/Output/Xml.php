993e8043dad65727c650ec176e06f3ac4047bd74
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-06-24 22:03:26 +0000

    Adds a couple of missing page-level and class-level docblocks.

--- application/libraries/Omeka/Output/Xml.php
+++ application/libraries/Omeka/Output/Xml.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ **/
+
+/**
+ * Should not exist.  Do NOT instantiate or reference this class in any way.
+ * 
+ * @deprecated
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ **/
 class Omeka_Output_Xml
-{
-}
+{}
