8559c2342258055970c554d65032366d3fd45a21
Jeremy Boggs <jeremy@clioweb.org>
2010-06-29 22:06:09 +0000

    Updates url_to_link function to use preg_replace instead of ereg_replace; Moves url_to_link function to the admin theme's custom.php file.

--- admin/themes/default/custom.php
+++ admin/themes/default/custom.php
@@ -42,3 +42,16 @@ if (!function_exists('show_untitled_items')) {
         return implode (', ', $array).' and '.$last;
     }
 }
+
+/**
+ * Converts any URLs in a given string to links.
+ *
+ * @param string $str The string to be searched for URLs to convert to links.
+ * @return string
+ **/
+function url_to_link($str)
+{
+    $pattern = "@\b(https?://)?(([0-9a-zA-Z_!~*'().&=+$%-]+:)?[0-9a-zA-Z_!~*'().&=+$%-]+\@)?(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-zA-Z_!~*'()-]+\.)*([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\.[a-zA-Z]{2,6})(:[0-9]{1,4})?((/[0-9a-zA-Z_!~*'().;?:\@&=+$,%#-]+)*/?)@";
+    $str = preg_replace($pattern, '<a href="\0">\0</a>', $str);
+    return $str;
+}
