022236811a5496d656064f0a093f627276c3b7b3
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-06-29 16:36:50 +0000

    Pulls 2.0 changes to install/ and Installer to trunk.
    Cleans up Installer and Install form, fixes to work on trunk (removes 2.0 changes).

--- application/models/Installer.php
+++ application/models/Installer.php
@@ -25,7 +25,6 @@ class Installer
     
     
     private $_db;
-    private $_form;
     
     public function __construct(Omeka_Db $db, Installer_Requirements $requirements)
     {   
@@ -33,8 +32,6 @@ class Installer
         
         // Set the database object;
         $this->_db = $db;
-        //die($this->_db->prefix);
-        $this->setForm();
     }
     
     public function checkRequirements()
@@ -61,187 +58,7 @@ class Installer
     {
         return $this->_requirements->getWarningMessages();
     }
-    
-    public function setForm()
-    {
-        // http://framework.zend.com/manual/en/zend.form.quickstart.html
-        // http://devzone.zend.com/article/3450
-        $form = new Zend_Form;
-        $form->setAction('index.php')->setMethod('post');
-        $form->removeDecorator('HtmlTag');
         
-        // Add form elements.
-        $elementDecorators = array('ViewHelper', 
-                                   'Errors', 
-                                   'Description',
-                                   'Label', 
-                                   array('HtmlTag', array('tag' => 'div')));
-        
-        $form->addElement('text', 'username', array(
-            'label' => 'Username',
-            'class' => 'textinput', 
-            'validators' => array(array('StringLength', false, array(User::USERNAME_MIN_LENGTH, User::USERNAME_MAX_LENGTH))), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('password', 'password', array(
-            'label' => 'Password',
-            'description' => 'Password for your super user, 6–40 characters.', 
-            'class' => 'textinput', 
-            'validators' => array(array('StringLength', false, array(User::PASSWORD_MIN_LENGTH))), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'super_email', array(
-            'label' => 'Email',
-            'class' => 'textinput', 
-            'validators' => array('EmailAddress'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'site_title', array(
-            'label' => 'Site Title',
-            'class' => 'textinput',
-            'class' => 'textinput', 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('textarea', 'description', array(
-            'label' => 'Site Description',
-            'class' => 'textinput', 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'administrator_email', array(
-            'label' => 'Administrator Email',
-            'class' => 'textinput', 
-            'validators' => array('EmailAddress'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'copyright', array(
-            'label' => 'Site Copyright Information',
-            'class' => 'textinput', 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'author', array(
-            'label' => 'Site Author Information',
-            'class' => 'textinput', 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'fullsize_constraint', array(
-            'label' => 'Fullsize Image Size',
-            'class' => 'textinput', 
-            'description' => 'Maximum fullsize image size constraint (in pixels).', 
-            'value' => self::DEFAULT_FULLSIZE_CONSTRAINT, 
-            'validators' => array('Digits'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'thumbnail_constraint', array(
-            'label' => 'Thumbnail Size',
-            'class' => 'textinput', 
-            'description' => 'Maximum thumbnail size constraint (in pixels).', 
-            'value' => self::DEFAULT_THUMBNAIL_CONSTRAINT, 
-            'validators' => array('Digits'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'square_thumbnail_constraint', array(
-            'label' => 'Square Thumbnail Size',
-            'class' => 'textinput', 
-            'description' => 'Maximum square thumbnail size constraint (in pixels).', 
-            'value' => self::DEFAULT_SQUARE_THUMBNAIL_CONSTRAINT, 
-            'validators' => array('Digits'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'per_page_admin', array(
-            'label' => 'Items Per Page (admin)', 
-            'class' => 'textinput',
-            'description' => 'Limit the number of items displayed per page in the administrative interface.', 
-            'value' => self::DEFAULT_PER_PAGE_ADMIN, 
-            'validators' => array('Digits'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('text', 'per_page_public', array(
-            'label' => 'Items Per Page (public)', 
-            'class' => 'textinput',
-            'description' => 'Limit the number of items displayed per page in the public interface.', 
-            'value' => self::DEFAULT_PER_PAGE_PUBLIC, 
-            'validators' => array('Digits'), 
-            'required' => true, 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('checkbox', 'show_empty_elements', array(
-            'label' => 'Show Empty Elements',
-            'class' => 'checkbox',
-            'description' => 'Whether metadata elements with no text will be displayed.',
-            'decorators' => $elementDecorators,
-            'value' => self::DEFAULT_SHOW_EMPTY_ELEMENTS
-        ));
-        
-        $form->addElement('text', 'path_to_convert', array(
-            'label' => 'Imagemagick Directory Path',
-            'class' => 'textinput', 
-            'value' => $this->_getPathToConvert(), 
-            'decorators' => $elementDecorators
-        ));
-        
-        $form->addElement('submit', 'install_submit', array(
-            'label' => 'Install',
-            'class' => 'textinput', 
-            'decorators' => array('Tooltip', 'ViewHelper')
-        ));
-
-        // Add fieldsets.
-        $displayGroupDecorators = array('FormElements', 'Fieldset');
-        
-        $form->addDisplayGroup(
-            array('username', 'password', 'super_email'), 
-            'superuser_account', 
-            array('legend' => 'Default Superuser Account', 
-                  'decorators' => $displayGroupDecorators)
-        );
-        
-        $form->addDisplayGroup(
-            array('administrator_email', 'site_title', 'description', 
-                  'copyright', 'author', 'fullsize_constraint', 
-                  'thumbnail_constraint', 'square_thumbnail_constraint', 
-                  'per_page_admin', 'per_page_public', 'show_empty_elements', 
-                  'path_to_convert'), 
-            'site_settings', 
-            array('legend' =>'Site Settings', 
-                  'decorators' => $displayGroupDecorators)
-        );
-        
-        $form->addDisplayGroup(
-            array('install_submit'), 
-            'submit', 
-            array('decorators' => $displayGroupDecorators)
-        );
-        
-        $form->setView(new Zend_View);
-        $this->_form = $form;
-    }
-    
-    public function getForm()
-    {
-        return $this->_form;
-    }
-    
     /**
      * @param array $values Set of values required by the installer.  Usually
      * passed in via the form.
@@ -313,7 +130,7 @@ class Installer
                 
         // If the fileinfo extension is not installed.
         $this->_db->exec($optionSql, array('enable_header_check_for_file_mime_types', (string)!extension_loaded('fileinfo')));
-        
+                
         return true;
     }
 
@@ -327,7 +144,6 @@ class Installer
         }
         
         // Assume Omeka is not installed if the `options` table contains no rows.
-        require_once 'Option.php';
         $optionCount = (int)$db->fetchOne("SELECT COUNT(id) FROM `{$db->prefix}options`");
         if (!$optionCount) {
             return false;
@@ -337,7 +153,7 @@ class Installer
         return true;
     }
     
-    private function _getPathToConvert()
+    public function getPathToConvert()
     {
         // Use the "which" command to auto-detect the path to ImageMagick;
         // redirect std error to where std input goes, which is nowhere. See: 
