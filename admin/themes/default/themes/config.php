10ccae281783e146b9d21c534aa5788c5069edef
Jeremy Boggs <jeremy@clioweb.org>
2010-06-29 20:58:09 +0000

    Fixes #944, adds explanatory text in theme config that theme configuration is on a per-theme basis.

--- admin/themes/default/themes/config.php
+++ admin/themes/default/themes/config.php
@@ -5,6 +5,7 @@
 <div id="primary">
     <?php echo flash(); ?>
     <h2>Please Configure The &quot;<?php echo html_escape($theme->title); ?>&quot; Theme</h2>
+    <p>Configurations apply to this theme only.</p>
         <?php echo $configForm; ?>
 </div>
 
