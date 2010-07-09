c523f24c8ca1fc5d3630f725de03050971b2957f
Will Riley <wanderingwill@gmail.com>
2010-07-09 18:33:12 +0000

    Fixes calls to head() in view scripts that lack a proper title.

--- application/views/scripts/error/403.php
+++ application/views/scripts/error/403.php
@@ -1,4 +1,4 @@
-<?php head(array('title' => 'Forbidden')); ?>
+<?php head(array('title' => '403 Page Forbidden')); ?>
 <h1>403 Forbidden</h1>
 <?php echo flash(); ?>
 <p>You do not have permission to access this page.</p>
