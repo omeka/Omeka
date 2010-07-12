b9cfde78160a6ee2618e0cb2a428e12a8f0e2c9f
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-07-12 21:27:26 +0000

    Refactors Omeka_Helper_TagFunctions_TagCloudTest into an instance of PHPUnit_Framework_TestCase.

--- application/tests/integration/Helpers/TagFunctions/TagCloudTest.php
+++ application/tests/integration/Helpers/TagFunctions/TagCloudTest.php
@@ -1,24 +1,27 @@
 <?php 
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+/**
+ * Require view helper functions.
+ */
 require_once HELPERS;
 
 /**
  * Tests snippet_by_word_count($phrase, $maxWords, $ellipsis)
  * in helpers/StringFunctions.php
- */
-class Helpers_TagFunctions_TagCloudTest extends Omeka_Test_AppTestCase
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ */ 
+class Omeka_Helper_TagFunctions_TagCloudTest extends PHPUnit_Framework_TestCase
 {       
-    public function setUp()
-    {
-        parent::setUp();
-    }
-
     public function testEmptyTagCloud()
     {
         $this->assertEquals('<p>No tags are available.</p>', tag_cloud());
-    }
-
-    public function tearDown()
-    {
-        parent::tearDown();
     }
 }
