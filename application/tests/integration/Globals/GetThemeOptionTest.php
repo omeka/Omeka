e731ddaf1138b519933cdc466cb52847f236ef46
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-07-12 15:47:25 +0000

    Refactors Globals_GetThemeOptionTest so that it actually tests something, and so it extends off PHPUnit_Framework_TestCase.

--- application/tests/integration/Globals/GetThemeOptionTest.php
+++ application/tests/integration/Globals/GetThemeOptionTest.php
@@ -3,26 +3,44 @@
  * @version $Id$
  * @copyright Center for History and New Media, 2010
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @package Omeka_Test
+ * @package Omeka
  **/
 
-class Globals_GetThemeOptionTest extends Omeka_Test_AppTestCase
+/**
+ * Test get_theme_option().
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ **/
+class Globals_GetThemeOptionTest extends PHPUnit_Framework_TestCase
 {   
-    public function testGetOptionWithoutThemeName()
+    const THEME_NAME = 'foobar';
+    
+    private $_themeOptions = array(
+        'logo' => 'foobar.jpg',
+        'show_title' => true,
+        'show_description' => false
+    );
+    
+    public function setUp()
     {
-        $themeName = get_option('public_theme');
-        
-        $optionName = 'someoptionname';
-        $optionValue = '';
-        $this->assertEquals($optionValue, get_theme_option($optionName));
-        $this->assertEquals($optionValue, get_theme_option($optionName, $themeName));
+        // Configure the options so that get_theme_option() can automatically retrieve 
+        // options for the given theme.
+        Omeka_Context::getInstance()->setOptions(array(
+            Theme::PUBLIC_THEME_OPTION => self::THEME_NAME,
+            'theme_' . self::THEME_NAME . '_options' => serialize($this->_themeOptions)
+        ));
+    }
+    
+    public function testWithoutThemeName()
+    {
+        $this->assertEquals(true, get_theme_option('show_title'));
+        $this->assertEquals('foobar.jpg', get_theme_option('logo'));
     }
         
-    public function testGetOptionWithThemeName()
+    public function testWithThemeName()
     {
-        $themeName = 'seasons';
-        $optionName = 'someoptionname';
-        $optionValue = '';
-        $this->assertEquals($optionValue, get_theme_option($optionName, $themeName));
+        $this->assertEquals('foobar.jpg', get_theme_option('logo', self::THEME_NAME));
+        $this->assertEquals(true, get_theme_option('show_title', self::THEME_NAME));
     }
 }
