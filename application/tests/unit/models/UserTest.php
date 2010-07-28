c30ea80d0dcb2bcf2ce27ec82c0b4b0591b47194
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-07-28 00:02:53 +0000

    Adds User::__get() so that properties of the User's Entity can be retrieve from the User transparently.

---
+++ application/tests/unit/models/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+/**
+ * Test the User model.
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ */
+class UserTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->dbAdapter = new Zend_Test_DbAdapter;
+        $this->db = new Omeka_Db($this->dbAdapter);
+        $this->user = new User($this->db);
+    }
+    
+    public function testGetSetEntityProperties()
+    {
+        $this->user->getEntity()->email = 'foobar@example.com';
+        $this->assertEquals('foobar@example.com', $this->user->email);
+    }
+}
