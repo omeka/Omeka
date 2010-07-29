fe3687248ce82bd23967f4533bc24a7cfba16ee9
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-07-29 16:13:21 +0000

    Fixes addPasswordSalt migration by writing out table names directly instead of relying on Omeka_Db::getTableName().

--- application/migrations/20100401000000_addPasswordSalt.php
+++ application/migrations/20100401000000_addPasswordSalt.php
@@ -21,14 +21,14 @@ class addPasswordSalt extends Omeka_Db_Migration
         // using timestamps.  Check if the salt column already exists before 
         // attempting to alter.
         
-        $columns = $this->db->fetchAll("DESCRIBE `{$this->db->User}");
+        $columns = $this->db->fetchAll("DESCRIBE `{$this->db->prefix}users`");
         foreach ($columns as $col) {
             if ($col['Field'] == 'salt') {
                 return;
             }
         }
         
-        $this->db->query("ALTER TABLE `{$this->db->User}` ADD `salt` VARCHAR( 16 ) 
+        $this->db->query("ALTER TABLE `{$this->db->prefix}users` ADD `salt` VARCHAR( 16 ) 
             CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL AFTER `password` ;");
     }
 }
