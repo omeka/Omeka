ba456dd94bbf6cc3ab513811be09d94b4e48af8e
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-08-12 23:52:01 +0000

    Ports 631 migration from 566 branch.  Mods collections schema to add new fields.

--- application/core/schema/collections.sql
+++ application/core/schema/collections.sql
@@ -2,9 +2,14 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%collections` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(255) collate utf8_unicode_ci NOT NULL,
   `description` text collate utf8_unicode_ci NOT NULL,
+  `collectors` text collate utf8_unicode_ci NOT NULL,
   `public` tinyint(1) NOT NULL,
   `featured` tinyint(1) NOT NULL,
-  PRIMARY KEY  (`id`),
+  `added` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `owner_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `public` (`public`),
-  KEY `featured` (`featured`)
+  KEY `featured` (`featured`),
+  KEY `owner_id` (`owner_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
