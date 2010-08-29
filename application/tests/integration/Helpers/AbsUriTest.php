54848eae97055fffe8bee18bf6a8ff73ff5c3b38
Kristopher Kelly <kristopherbkelly@gmail.com>
2010-08-29 20:51:10 +0000

    Fixes abs_uri() to use Zend Framework ServerUrl helper instead of hacked custom solution.
    Fixes undefined array index notice in abs_uri().  Adds test for abs_uri().

---
+++ application/tests/integration/Helpers/AbsUriTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2007-2010
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+/**
+ * 
+ *
+ * @package Omeka
+ * @copyright Center for History and New Media, 2007-2010
+ */
+class Omeka_Helper_AbsUriTest extends Omeka_Test_AppTestCase
+{
+    public function testAbsUriDoesNotGiveNoticeIfMissingHttpHost()
+    {
+        try {
+            $uri = abs_uri();
+        } catch (PHPUnit_Framework_Error_Notice $e) {
+            $this->assertNotContains("Undefined index:  HTTP_HOST", $e->getMessage(),
+                "abs_uri() should not give an 'Undefined index:  HTTP_HOST' notice.");
+        }
+    }
+    
+    public function testAbsUriUsesHttpHost()
+    {   
+        $_SERVER['HTTP_HOST'] = 'www.example.com';
+        $uri = abs_uri(array('controller' => 'items', 'action' => 'browse'), 'default');
+        $this->assertEquals('http://www.example.com/items/browse', $uri);
+    }
+}
