8f9aaf0fe295cbc0e8a3682cc1275e42cb572f28
John Flatness <john@zerocrates.org>
2010-10-08 20:47:32 +0000

    Adds global JS function for adding TinyMCE to pages.

--- admin/themes/default/javascripts/globals.js
+++ admin/themes/default/javascripts/globals.js
@@ -39,6 +39,43 @@ Omeka.alertBox = function() {
     }
 }
 
+/**
+ * Add the TinyMCE WYSIWG editor to a page.
+ * Default is to add to all textareas.
+ *
+ * @param {Object} [params] Parameters to pass to TinyMCE, these override the
+ * defaults.
+ */
+Omeka.wysiwyg = function (params) {
+    // Default parameters
+    initParams = {
+        convert_urls : false,
+        mode : "textareas", // All textareas
+        theme: "advanced",
+        theme_advanced_toolbar_location : "top",
+        force_br_newlines : false,
+        forced_root_block : 'p', // Needed for 3.x
+        remove_linebreaks : true,
+        fix_content_duplication : false,
+        fix_list_elements : true,
+        valid_child_elements:"ul[li],ol[li]",
+        theme_advanced_buttons1 : "bold,italic,underline,justifyleft,justifycenter,justifyright,bullist,numlist,link,formatselect,code",
+        theme_advanced_buttons2 : "",
+        theme_advanced_buttons3 : "",
+        theme_advanced_toolbar_align : "left"
+    };
+
+    // Overwrite default params with user-passed ones.
+    for (attribute in params) {
+        // Account for annoying scripts that mess with prototypes.
+        if (params.hasOwnProperty(attribute)) {
+            initParams[attribute] = params[attribute];
+        }
+    }
+
+    tinyMCE.init(initParams);
+};
+
 //This will add confirmation for deleting files and the item
 function confirmDelete() {
     $$('.delete').each( function(el) {
