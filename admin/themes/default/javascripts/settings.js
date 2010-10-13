0586807925ec430874d08c2b086099a8b8b73282
John Flatness <john@zerocrates.org>
2010-10-13 17:37:34 +0000

    Removes Prototype from settings JS.

--- admin/themes/default/javascripts/settings.js
+++ admin/themes/default/javascripts/settings.js
@@ -1,19 +1,33 @@
-function checkImageMagick(checkImageMagickUrl){
+if (typeof Omeka === 'undefined') {
+    Omeka = {};
+}
+
+Omeka.Settings = {};
+
+/**
+ * Adds button/div that uses AJAX to check the given ImageMagick path.
+ *
+ * @param {string} url Url to the checkImageMagick action.
+ */
+Omeka.Settings.checkImageMagick = function (url) {
     if (!$('path_to_convert')) {
         return;
     }
-    var testButton = new Element('button', {'type': 'button', 'id': 'test-button'});
-    var resultDiv = new Element('div', {'id': 'im-result'});
-    var imageMagickInput = $('path_to_convert');
-    imageMagickInput.insert({'after':resultDiv});
-    imageMagickInput.insert({'after': testButton});
-    testButton.update('Test').observe('click', function(e){
-        new Ajax.Request(checkImageMagickUrl, {
-            method: 'get',
-            parameters: 'path-to-convert=' + imageMagickInput.getValue(),
-            onComplete: function(t) {
-                resultDiv.update(t.responseText);
+    var testButton = '<button type="button" id="test-button">Test</button>';
+    var resultDiv = '<div id="im-result" />';
+    var imageMagickInput = jQuery('#path_to_convert');
+    imageMagickInput.after(resultDiv);
+    imageMagickInput.after(testButton);
+    jQuery('#test-button').click(function () {
+        jQuery.ajax({
+            url: url,
+            dataType: 'html',
+            data: {
+                'path-to-convert': imageMagickInput.val()
+            },
+            success: function (data) {
+                jQuery('#im-result').html(data);
             }
         });
     });
-}
+};
