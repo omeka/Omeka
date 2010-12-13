2361ea7638bbd6a31aa829d24d8556f7185b5095
Jeremy Boggs <jeremy@clioweb.org>
2010-12-13 16:25:57 +0000

    Fixes #987, removes call to header in upgrade and debug screens and replaced with simpler markup.

--- application/views/scripts/upgrade/completed.php
+++ application/views/scripts/upgrade/completed.php
@@ -1,6 +1,14 @@
-<?php head(array('title'=>'Omeka Upgrade Completed')); ?>
-<div id="primary">
-	<h2>You're Good to Go!</h2>
-<p>Your Omeka database is completely up-to-date.  Please return to the <?php echo link_to_admin_home_page('admin'); ?> or your site&#8217;s <?php echo link_to_home_page('home page'); ?>. If you have any questions please refer to Omeka documentation.</p>
-</div>
-<?php foot();?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <title>Upgrade Omeka</title>
+    <link rel="stylesheet" media="all" href="<?php echo html_escape(css('style')); ?>">
+</head>
+<body id="upgrade">
+    <div id="content">
+    <h1>Omeka Upgrade Completed</h1>
+    <p>Your Omeka database is completely up-to-date.  Please return to the <?php echo link_to_admin_home_page('admin'); ?> or your site&#8217;s <?php echo link_to_home_page('home page'); ?>. If you have any questions please refer to <a href="http://omeka.org/codex">Omeka documentation</a> or post a message on the <a href="http://omeka.org/forums">Omeka forums</a>.</p>
+    </div>
+</body>
+</html>
--- application/views/scripts/upgrade/index.php
+++ application/views/scripts/upgrade/index.php
@@ -1,10 +1,16 @@
-<?php head(array('title' => 'Upgrade Database')); ?>
-<div id="primary">
-<h2 class="instruction">Upgrade Your Omeka Database</h2> 
-<p>Your Omeka database is not compatible with your current
-version of Omeka.  
-
-Please backup your existing database and then click the button to upgrade:</p>
-<?php echo link_to('upgrade', 'migrate', 'Upgrade', array('id' => 'upgrade-database-link', 'class'=>'button')); ?>                        
-</div>
-<?php foot(); ?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <title>Upgrade Omeka</title>
+    <link rel="stylesheet" media="all" href="<?php echo html_escape(css('style')); ?>">
+</head>
+<body id="upgrade">
+    <div id="content">
+        <h1>Upgrade Your Omeka Database</h1> 
+        <p>Your Omeka database is not compatible with your current
+        version of Omeka. Please backup your existing database and then click the button to upgrade:</p>
+        <?php echo link_to('upgrade', 'migrate', 'Upgrade Database', array('id' => 'upgrade-database-link', 'class'=>'button')); ?>                        
+    </div>
+</body>
+</html>
