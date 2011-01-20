3dab29695d1a6bd00a9a91cb55818672f4c6f601
Jeremy Boggs <jeremy@clioweb.org>
2011-01-20 19:42:16 +0000

    Updates tests for auto_discovery_link_tag() helper to look for atom link.

--- application/tests/integration/Helpers/AutoDiscoveryLinkTagTest.php
+++ application/tests/integration/Helpers/AutoDiscoveryLinkTagTest.php
@@ -22,14 +22,17 @@ class Omeka_Helper_AutoDiscoveryLinkTagTest extends Omeka_Test_AppTestCase
     {
         $this->dispatch('/');
         $_GET['cookies&cream'] = 'tasty&delicious';
-        $html = auto_discovery_link_tag();
-        $this->assertEquals('<link rel="alternate" type="application/rss+xml" title="Omeka RSS Feed" href="/items/browse?cookies%26cream=tasty%26delicious&amp;output=rss2" />', $html);
+        $html = '<link rel="alternate" type="application/rss+xml" title="Omeka RSS Feed" href="/items/browse?cookies%26cream=tasty%26delicious&amp;output=rss2" />';
+		$html .= '<link rel="alternate" type="application/atom+xml" title="Omeka Atom Feed" href="/items/browse?cookies%26cream=tasty%26delicious&amp;output=atom" />';
+        $this->assertContains($html, auto_discovery_link_tag());
     }
     
     public function testLinkTagAvoidsXssAttack()
     {        
         $this->dispatch('/items/browse/%22%3e%3cscript%3ealert(11639)%3c/script%3e');
-        $this->assertEquals('<link rel="alternate" type="application/rss+xml" title="Omeka RSS Feed" href="/items/browse/%22%3E%3Cscript%3Ealert%2811639%29%3C/script%3E?output=rss2" />', 
+		$html = '<link rel="alternate" type="application/rss+xml" title="Omeka RSS Feed" href="/items/browse/%22%3E%3Cscript%3Ealert%2811639%29%3C/script%3E?output=rss2" />';
+		$html .= '<link rel="alternate" type="application/atom+xml" title="Omeka Atom Feed" href="/items/browse/%22%3E%3Cscript%3Ealert%2811639%29%3C/script%3E?output=atom" />';
+        $this->assertContains($html, 
                             auto_discovery_link_tag());
     } 
 }
