b0cb37fae96f609b057d27ee30e196ce8b2fa564
John Flatness <john@zerocrates.org>
2011-03-07 18:09:23 +0000

    Removes ThemesControllerTest's dependency on 'seasons'.

--- application/tests/integration/Controllers/ThemesControllerTest.php
+++ application/tests/integration/Controllers/ThemesControllerTest.php
@@ -14,12 +14,12 @@
  **/
 class Omeka_Controller_ThemesControllerTest extends Omeka_Test_AppTestCase
 {   
-    const SEASONS_THEME = 'seasons';
+    const THEME = 'default';
     
     public function setUp()
     {
-        if (!is_dir(PUBLIC_THEME_DIR . DIRECTORY_SEPARATOR . self::SEASONS_THEME)) {
-            $this->markTestSkipped("Cannot test ThemesController without the 'seasons' theme.");
+        if (!is_dir(PUBLIC_THEME_DIR . DIRECTORY_SEPARATOR . self::THEME)) {
+            $this->markTestSkipped("Cannot test ThemesController without the '" . self::THEME . "' theme.");
         }
         parent::setUp();   
         $this->_authenticateUser($this->_getDefaultUser());
@@ -27,18 +27,22 @@ class Omeka_Controller_ThemesControllerTest extends Omeka_Test_AppTestCase
     
     public function testDisplayConfigForm()
     {
-        set_option(Theme::PUBLIC_THEME_OPTION, self::SEASONS_THEME);
-        $this->request->setParam('name', self::SEASONS_THEME);
+        set_option(Theme::PUBLIC_THEME_OPTION, self::THEME);
+
+        $theme = Theme::getAvailable(self::THEME);
+        $name = $theme->title;
+
+        $this->request->setParam('name', self::THEME);
         $this->dispatch('themes/config');
         $this->assertController('themes');
         $this->assertAction('config');
-        $this->assertQueryContentContains('h2', 'Please Configure The "Seasons" Theme');
-        $this->assertQuery('select#style_sheet');
+        $this->assertQueryContentContains('h2', "Please Configure The \"$name\" Theme");
+        $this->assertQuery('input#logo');
     }
         
-    public function testConfigureSeasonsThemeWithNoLogoFileAndNoPreviousLogoFile()
+    public function testConfigureThemeWithNoLogoFileAndNoPreviousLogoFile()
     {
-        $themeName = self::SEASONS_THEME;
+        $themeName = self::THEME;
         $this->assertEquals('', (string)get_theme_option('logo', $themeName));
         
         // specify the files array for the post
@@ -50,12 +54,19 @@ class Omeka_Controller_ThemesControllerTest extends Omeka_Test_AppTestCase
                   'tmp_name' => '',
                   'error' => 4,
                   'size' => 0
-                )
+              ),
+            'header_background' =>
+                array(
+                  'name' => '',
+                  'type' => '',
+                  'tmp_name' => '',
+                  'error' => 4,
+                  'size' => 0
+              )
         );
         
         // specify the theme options for the post
         $themeOptions = array(
-          'style_sheet' => 'winter',
           'custom_header_navigation' => '',
           'display_featured_item' => '1',
           'display_featured_collection' => '1',
@@ -70,6 +81,7 @@ class Omeka_Controller_ThemesControllerTest extends Omeka_Test_AppTestCase
         // specify other post data
         $otherPostData = array(
           'hidden_file_logo' => '',
+          'hidden_file_header_background' => '',  
           'MAX_FILE_SIZE' => '33554432',
           'submit' => 'Save Changes'
         );
