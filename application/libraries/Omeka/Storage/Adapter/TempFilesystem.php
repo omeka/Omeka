3b2ec722979f8057c6c12ad26e0f40cfb4e9c571
Kristopher Kelly <kristopherbkelly@gmail.com>
2011-03-10 21:13:26 +0000

    Adds TempFilesystem storage to isolate storage during tests.

---
+++ application/libraries/Omeka/Storage/Adapter/TempFilesystem.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2011
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+/**
+ * Storage adapter that uses the system temp directory for its filesystem. 
+ *
+ * After the adapter is no longer needed (__destruct()), all the files that were 
+ * created during its lifetime are removed.
+ *
+ * Used primarily by the test framework.
+ *
+ * @package Omeka
+ */
+class Omeka_Storage_Adapter_TempFilesystem extends Omeka_Storage_Adapter_Filesystem
+{
+    private $_storageDir;
+
+    private $_files = array();
+
+    public function __construct(array $options = array())
+    {
+        $this->_storageDir = sys_get_temp_dir() . 'archive' . mt_rand();
+        mkdir($this->_storageDir);
+        $webDir = '/'; // CLI tests don't care whether the URL is valid.
+        parent::__construct(array(
+            'localDir' => $this->_storageDir,
+            'webDir' => $webDir,
+        ));
+    }
+
+    public function __destruct()
+    {
+        exec("rm -rf " . escapeshellarg($this->_storageDir));
+    }
+
+
+    /**
+     * No need to perform this check.
+     */
+    public function canStore()
+    {
+        return true;
+    }
+
+    /**
+     * Move a local file to "storage."
+     *
+     * @param string $source Local filesystem path to file.
+     * @param string $dest Destination path.
+     */
+    public function store($source, $dest)
+    {
+        $this->_mkdir($dest);
+        parent::store($source, $dest);
+    }
+
+    /**
+     * Move a file between two "storage" locations.
+     *
+     * @param string $source Original stored path.
+     * @param string $dest Destination stored path.
+     */
+    public function move($source, $dest)
+    {
+        $this->_mkdir($dest);
+        return parent::move($source, $dest);
+    }
+
+    public function getUri($path)
+    {
+        return '/' . $path;
+    }
+
+    private function _mkdir($filepath)
+    {
+        $absPath = $this->_storageDir . '/' . $filepath;
+        // Meant to stub out filesystem behavior, prevent failure due to 
+        // missing subdirectories.
+        if (!is_dir(dirname($absPath))) {
+            mkdir(dirname($absPath), 0770, true);
+        }
+    }
+}
---
+++ application/libraries/Omeka/Test/Resource/Storage.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version $Id$
+ * @copyright Center for History and New Media, 2011
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+/**
+ * Bootstrap resource for storage in test environment.
+ */
+class Omeka_Test_Resource_Storage extends Zend_Application_Resource_ResourceAbstract
+{
+    public function init()
+    {
+        $storage = new Omeka_Storage(array(
+            Omeka_Storage::OPTION_ADAPTER => 'Omeka_Storage_Adapter_TempFilesystem',
+        ));
+        Zend_Registry::set('storage', $storage);
+        return $storage;
+    }
+}
