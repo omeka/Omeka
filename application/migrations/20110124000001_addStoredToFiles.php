af4505b056061d6d069e05d72ce5838eb3e03884
John Flatness <john@zerocrates.org>
2011-03-10 16:32:26 +0000

    Updates storage migration to set all existing files as stored.

--- application/migrations/20110124000001_addStoredToFiles.php
+++ application/migrations/20110124000001_addStoredToFiles.php
@@ -7,6 +7,8 @@ class addStoredToFiles extends Omeka_Db_Migration
         $db = $this->getDb();
 
         $db->query("ALTER TABLE `{$db->File}` ADD `stored` TINYINT(1) NOT NULL DEFAULT '0'");
+        // All files present at upgrade are already "stored."
+        $db->query("UPDATE `{$db->File}` SET `stored` = '1'");
     }
 
     public function down()
