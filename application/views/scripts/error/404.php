1003141b48cb67bdcb01fa81e537c0ca787bf60c
John Flatness <john@zerocrates.org>
2011-03-15 19:42:08 +0000

    Fixes HTML entity error on global 404 view.

--- application/views/scripts/error/404.php
+++ application/views/scripts/error/404.php
@@ -1,6 +1,6 @@
 <?php head(array('title'=>'404 Page Not Found')); ?>
 <div id="primary">
     <h1>404 Page Not Found</h1>
-    <p>&#8220;<?php echo html_escape($badUri); ?>&8221; is not a valid URL.</p>
+    <p>&#8220;<?php echo html_escape($badUri); ?>&#8221; is not a valid URL.</p>
 </div>
 <?php foot(); ?>
