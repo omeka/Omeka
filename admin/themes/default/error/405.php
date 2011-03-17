929aaaf0bb6c329d0f150efeab3a4a29cafdfcbd
John Flatness <john@zerocrates.org>
2011-03-17 23:38:27 +0000

    Fixes typo in previous commit.

--- admin/themes/default/error/405.php
+++ admin/themes/default/error/405.php
@@ -4,7 +4,7 @@
 <div id="primary" class="method-not-allowed">
     <?php echo flash(); ?>
     
-<p>You must use a different method to acceess this URL. If you reached this page by following a link or clicking a button in Omeka, please contact the <a href="mailto:<?php echo settings('administrator_email'); ?>">site administrator</a>.</p>
+<p>You must use a different method to access this URL. If you reached this page by following a link or clicking a button in Omeka, please contact the <a href="mailto:<?php echo settings('administrator_email'); ?>">site administrator</a>.</p>
 </div>
 
 <?php foot();
