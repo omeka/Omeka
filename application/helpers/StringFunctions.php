0caabdabddf68c6d4604f17e9f4b5f6ad2c3d4d9
Jeremy Boggs <jeremy@clioweb.org>
2011-04-29 14:34:36 +0000

    Simplifies regex for url_to_link function.

--- application/helpers/StringFunctions.php
+++ application/helpers/StringFunctions.php
@@ -204,7 +204,7 @@ function text_to_id($text, $prepend=null, $delimiter='-')
  **/
 function url_to_link($str)
 {
-    $pattern = "@\b(https?://)?(([0-9a-zA-Z_!~*'().&=+$%-]+:)?[0-9a-zA-Z_!~*'().&=+$%-]+\@)?(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-zA-Z_!~*'()-]+\.)*([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\.[a-zA-Z]{2,6})(:[0-9]{1,4})?((/[0-9a-zA-Z_!~*'().;?:\@&=+$,%#-]+)*/?)@";
+    $pattern = "((https?://)+[\w\d:#@%/;$()~_?\+-=\\\.&]*)";
     $str = preg_replace($pattern, '<a href="\0">\0</a>', $str);
     return $str;
 }
