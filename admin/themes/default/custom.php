1861ba3669f3f86d1b54b13f49c4cf385f19ec53
Jeremy Boggs <jeremy@clioweb.org>
2011-04-29 14:34:31 +0000

    Removes add_filter from admin's custom.php file for showing untitled items, since that is already filtered in application/views/scripts/custom.php.

--- admin/themes/default/custom.php
+++ admin/themes/default/custom.php
@@ -1,14 +1,3 @@
-<?php 
-// This messes up the form inputs that use the item's ID, but it still serves as
-// an example of what the system can do.
-
-// add_filter(array('Display', 'Item', 'id'), 'display_item_id_numerically');
-// 
-// function display_item_id_numerically($id)
-// {
-//     return '#' . $id;
-// }
-
-add_filter(array('Display', 'Item', 'Dublin Core', 'Title'), 'show_untitled_items');
+<?php
 
 require_once dirname(__FILE__) . '/functions.php';
