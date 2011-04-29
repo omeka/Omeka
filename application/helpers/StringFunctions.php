7de89138873bb56fb1a1b910e0c9cec708b1fa25
Jeremy Boggs <jeremy@clioweb.org>
2011-04-29 14:34:34 +0000

    Moves url_to_link function from admin theme's functions.php to StringFunctions.
    Removes custom.php and functions.php in admin theme, since they no longer
    contain code.

--- application/helpers/StringFunctions.php
+++ application/helpers/StringFunctions.php
@@ -194,3 +194,17 @@ function text_to_id($text, $prepend=null, $delimiter='-')
 	$prepend = (string) $prepend;
 	return !empty($prepend) ? join($delimiter, array($prepend, $id)) : $id;
 }
+
+/**
+ * Converts any URLs in a given string to links.
+ *
+ * @since 1.4
+ * @param string $str The string to be searched for URLs to convert to links.
+ * @return string
+ **/
+function url_to_link($str)
+{
+    $pattern = "@\b(https?://)?(([0-9a-zA-Z_!~*'().&=+$%-]+:)?[0-9a-zA-Z_!~*'().&=+$%-]+\@)?(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-zA-Z_!~*'()-]+\.)*([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\.[a-zA-Z]{2,6})(:[0-9]{1,4})?((/[0-9a-zA-Z_!~*'().;?:\@&=+$,%#-]+)*/?)@";
+    $str = preg_replace($pattern, '<a href="\0">\0</a>', $str);
+    return $str;
+}
