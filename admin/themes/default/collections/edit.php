122c857a7998dd7807afba6755daebb03df7fb8e
John Flatness <john@zerocrates.org>
2011-06-15 20:39:17 +0000

    Fixes weird delete button styling.

--- admin/themes/default/collections/edit.php
+++ admin/themes/default/collections/edit.php
@@ -11,14 +11,14 @@
 <?php head(array('title'=> $collectionTitle, 'bodyclass'=>'collections')); ?>
 <h1><?php echo $collectionTitle; ?></h1>
 
+<?php echo delete_button(null, 'delete-collection', 'Delete this Collection', array(), 'delete-record-form'); ?>
+
 <div id="primary">
 	
 <form method="post">
 <?php include 'form.php';?>	
 <input type="submit" name="submit" class="submit" id="save-changes" value="Save Changes" />
 </form>
-
-<?php echo delete_button(null, 'delete-collection', 'Delete this Collection'); ?>
 
 </div>
 <?php foot(); ?>
