4b62127dc74a0769f20c060f5f5510e778731f5e
Kristopher Kelly <kristopherbkelly@gmail.com>
2011-08-22 10:53:27 -0700

    Remove @version $Id$ doctag from many files.

--- application/models/ProcessAbstract.php
+++ application/models/ProcessAbstract.php
@@ -1,6 +1,5 @@
 <?php 
 /**
- * @version $Id$
  * @copyright Roy Rosenzweig Center for History and New Media, 2007-2010
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package Omeka
@@ -9,7 +8,6 @@
 /**
  * Base class background processes descend from.
  * 
- * @version $Id$
  * @copyright Roy Rosenzweig Center for History and New Media, 2009
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package Omeka
