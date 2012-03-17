1d8908ac99fb8a8a7e266e66a49f1fa5d4638592
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-03-17 13:48:03 -0400

    Allows anonymous (no current user) use of background processes
    If there is no user (e.g., a submission to Commons sets the user_id for the process to 0.

--- application/models/ProcessDispatcher.php
+++ application/models/ProcessDispatcher.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 /**
  * @copyright Roy Rosenzweig Center for History and New Media, 2009
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package Omeka
  * @subpackage Models
  */
- 
+
 /**
  * Spawns and manages background processes.
  *
@@ -20,32 +20,38 @@ class ProcessDispatcher
      * @param string $className ProcessAbstract subclass name to spawn
      * @param User $user User to run process as, defaults to current user
      * @param Array|null $args Arguments specific to the child class process
-     * @param string $lastPhase The last phase to load during the phased loading process (see Omeka_Core) 
+     * @param string $lastPhase The last phase to load during the phased loading process (see Omeka_Core)
      * @return Process The model object for the background process
      */
     static public function startProcess($className, $user = null, $args = null, $lastPhase = 'initializeRoutes')
     {
         $cliPath = self::getPHPCliPath();
-                
+
         if (!$user) {
             $user = Omeka_Context::getInstance()->getCurrentUser();
         }
-        
+
+        if($user) {
+            $user_id = $user_id;
+        } else {
+            $user_id = 0;
+        }
+
         $process = new Process;
         $process->class = $className;
-        $process->user_id = $user->id;
+        $process->user_id = $user_id;
         $process->status = Process::STATUS_STARTING;
         $process->setArguments($args);
         $process->started = date('Y-m-d G:i:s');
         $process->stopped = '0000-00-00 00:00:00';
         $process->save();
-        
+
         $command = escapeshellcmd($cliPath) . ' '
                  . escapeshellarg(self::_getBootstrapFilePath())
                  . " -p " . escapeshellarg($process->id)
                  . " -l " . escapeshellarg($lastPhase);
         self::_fork($command);
-        
+
         return $process;
     }
 
@@ -75,22 +81,22 @@ class ProcessDispatcher
             return false;
         }
     }
-    
+
     static public function getPHPCliPath()
     {
         // Use the user-specified path, or attempt autodetection if no path
         // specified.
         $cliPath = Omeka_Context::getInstance()->getConfig('basic')->background->php->path;
-        
+
         if ($cliPath == "") {
             $cliPath = self::_autodetectCliPath();
         }
-        
+
         self::_checkCliPath($cliPath);
-        
+
         return $cliPath;
     }
-    
+
     /**
      * Checks if the configured PHP-CLI path points to a valid PHP binary.
      * Flash an appropriate error if the path is invalid.
@@ -105,18 +111,18 @@ class ProcessDispatcher
         $command = escapeshellcmd($cliPath).' -v';
         $output = array();
         exec($command, $output, $returnCode);
-        
+
         $error = "The configured PHP path ($cliPath)";
         if ($returnCode != 0) {
             throw new Exception($error.' is invalid.');
         }
-        
+
         // Attempt to parse the output from 'php -v' (the first line only)
         preg_match('/(^\\w+) ([\\d\\.]+)/', $output[0], $matches);
         $cliName    = $matches[1];
         $cliVersion = $matches[2];
         $phpVersion = phpversion();
-        
+
         if ($cliName != 'PHP'  || !$cliVersion) {
             throw new Exception($error.' does not point to a PHP-CLI binary.');
         } else if (version_compare($cliVersion, '5.2', '<')) {
@@ -127,14 +133,14 @@ class ProcessDispatcher
         }
         return true;
     }
-    
+
     static private function _autodetectCliPath()
     {
         $command = 'which php 2>&0';
         $lastLineOutput = exec($command, $output, $returnVar);
         return $returnVar == 0 ? $lastLineOutput : '';
     }
-    
+
     /**
      * Returns the path to the background bootstrap script.
      *
@@ -144,13 +150,13 @@ class ProcessDispatcher
     {
         return BACKGROUND_BOOTSTRAP_PATH;
     }
-    
+
     /**
      * Launch a background process, returning control to the foreground.
-     * 
+     *
      * @link http://www.php.net/manual/en/ref.exec.php#70135
      */
-    static private function _fork($command) 
+    static private function _fork($command)
     {
         exec("$command > /dev/null 2>&1 &");
     }
