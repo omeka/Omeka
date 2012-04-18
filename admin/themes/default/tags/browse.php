2b7da49f58339527879fabe686a224bd37d2b6c9
Kim Nguyen <design@kimisgold.com>
2012-04-18 11:02:26 -0400

    Added options for  and , reorganized tags/browse page, and included styles for the tab sorting and .

--- admin/themes/default/tags/browse.php
+++ admin/themes/default/tags/browse.php
@@ -3,12 +3,17 @@ $pageTitle = __('Browse Tags');
 head(array('title'=>$pageTitle, 'content_class' => 'horizontal-nav','bodyclass'=>'tags browse-tags primary')); ?>
 <?php common('tags-nav'); ?>
 <?php if ( total_results() ): ?>
-    <p id="tags-nav"><?php echo __('Sort by'); ?>:
+
+    <div class="two columns alpha">
+        <h3><?php echo __('Edit/Delete Tags'); ?></h3>
+    </div>
+    <div class="eight columns omega">
+    <p id="tags-nav">
         <?php
         $sortOptions = array(
             __('Most') => array('sort_field' => 'count', 'sort_dir' => 'd'),
-            __('Least') => array('sort_field' => 'count'),
-            __('Alphabetical') => array('sort_field' => 'name'),
+            __('Least') => array('sort_field' => 'count','sort_dir' => 'a'),
+            __('Alphabetical') => array('sort_field' => 'name', 'sort_dir'=> 'a'),
             __('Recent') => array('sort_field' => 'time', 'sort_dir' => 'd')
         );
 
@@ -16,13 +21,14 @@ head(array('title'=>$pageTitle, 'content_class' => 'horizontal-nav','bodyclass'=
             $uri = html_escape(current_uri($params));
             $class = ($sort == $params) ? ' class="current"' : '';
 
-            echo "<a href=\"$uri\"$class>$label</a>";
+            echo "<span $class><a href=\"$uri\">$label</a></span>";
         }
         ?>
-    </p>
-    <?php echo tag_cloud($tags, ($browse_for == 'Item') ? uri('items/browse/'): uri('exhibits/browse/')); ?>
+    </p>    
+    <?php echo tag_cloud($tags, ($browse_for == 'Item') ? uri('items/browse/'): uri('exhibits/browse/'), 9, true, 'before'); ?>
 <?php else: ?>
     <p><?php echo __('There are no tags to display. You must first tag some items.'); ?></p>
+    </div>
 <?php endif; ?>
 </div>
 <?php foot(); ?>
