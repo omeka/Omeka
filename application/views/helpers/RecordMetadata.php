1d00c6a0c69115785d0fdd90b2b3063c55e658ee
John Flatness <john@zerocrates.org>
2012-06-04 17:47:44 -0400

    Fix up RecordMetadata documentation.

--- application/views/helpers/RecordMetadata.php
+++ application/views/helpers/RecordMetadata.php
@@ -1,21 +1,19 @@
 <?php
 /**
- * @copyright Roy Rosenzweig Center for History and New Media, 2007-2010
+ * @copyright Roy Rosenzweig Center for History and New Media, 2007-2012
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package Omeka
  * @subpackage Omeka_View_Helper
  */
 
 /**
- * Helper used to retrieve metadata for a record that makes use of element 
- * texts.
+ * Helper used to retrieve record metadata for for display.
  *
  * @package Omeka
  * @subpackage Omeka_View_Helper
- * @copyright Roy Rosenzweig Center for History and New Media, 2007-2010
  */
 class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
-{    
+{
     const SNIPPET = 'snippet';
     const INDEX = 'index';
     const ALL = 'all';
@@ -32,45 +30,31 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
      * @var string|array
      */
     protected $_metadata;
-    
+
     /**
-     * Retrieve a specific piece of an item's metadata.
-     * 
-     * The most common use of this method is to retrieve a single text value for 
-     * a given element, e.g. item([Item Record], 'Dublin Core, 'Title') will 
-     * return a string corresponding to the first available 'Title' in the 
-     * 'Dublin Core' element set. For item metadata not belonging to an element 
-     * set (e.g. id, date added, and public), use the simple syntax: 
-     * item([Item Record], 'id'). There are a number of options that can be 
-     * passed via an array as the fourth argument.
-     * 
-     * @param Omeka_Record $record Database record representing the item from 
+     * Retrieve a specific piece of a record's metadata for display.
+     *
+     * @param Omeka_Record $record Database record representing the item from
      * which to retrieve field data.
      * @param string|array $metadata The metadata field to retrieve.
-     *  If a string, refers to a "special" field on the record itself,
-     *  if an array, refers to an Element: the first entry is the set name,
+     *  If a string, refers to a property of the record itself.
+     *  If an array, refers to an Element: the first entry is the set name,
      *  the second is the element name.
-     * @param array|string|integer $options Options for formatting the metadata 
+     * @param array|string|integer $options Options for formatting the metadata
      * for display.
-     * - Default array options:
-     *   - 'delimiter' => Return the entire set of metadata as a string, where 
-     *     each entry is separated by the string delimiter.
-     *   - 'index' => Return the metadata entry at the specific integer index, 
-     *     starting at 0.
-     *   - 'no_filter' => If set to true, return the set of metadata without 
-     *     running any of the filters.
-     *   - 'snippet' => Trim the length of each piece of text to the given 
-     *     integer length.
-     *   - 'all' => If set to true, this will retrieve an array containing all 
-     *     values for a single element rather than a specific value.
+     * - Array options:
+     *   - 'all': If true, return an array containing all values for the field.
+     *   - 'delimiter': Return the entire set of metadata as a string, where
+     *     entries are separated by the given delimiter.
+     *   - 'index': Return the metadata entry at the given zero-based index.
      *   - 'no_escape' => If true, do not escape the resulting values for HTML
      *     entities.
-     * - Default string option:
-     *   Passing the string 'all' will retrieve an array containing all values 
-     *   for a single element rather than a specific value.
-     * - Default integer option:
-     *   Passing an integer will return the metadata entry at the specific 
-     *   integer index, starting at 0.
+     *   - 'no_filter': If true, return the set of metadata without
+     *     running any of the filters.
+     *   - 'snippet': Trim the length of each piece of text to the given
+     *     length in characters.
+     * - Passing simply the string 'all' is equivalent to array('all' => true)
+     * - Passing simply an integer is equivalent to array('index' => [the integer])
      * @return string|array|null Null if field does not exist for item. Array
      * if certain options are passed.  String otherwise.
      */
@@ -78,7 +62,7 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
     {
         $this->_record = $record;
         $this->_metadata = $metadata;
-        
+
         // Convert the shortcuts for the options into a proper array.
         $options = $this->_getOptions($options);
 
@@ -89,12 +73,10 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
         $delimiter = isset($options[self::DELIMITER]) ? (string) $options[self::DELIMITER] : false;
         $index = isset($options[self::INDEX]) ? (int) $options[self::INDEX] : 0;
 
-        // Set the initial text value, which is either an array of ElementText 
-        // records, or a special value string.
         $text = $this->_getText($record, $metadata);
 
         if (is_array($text)) {
-            // If ALL or DELIMITER isn't specified, pare the array down to
+            // If $all or $delimiter isn't specified, pare the array down to
             // just one entry, otherwise we need to work on the whole thing
             if ($all || $delimiter) {
                 foreach ($text as $key => $value) {
@@ -117,14 +99,15 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
             }
         }
 
+        // If we get here, we're working with a single value only.
         return $this->_process($text, $snippet, $escape, $filter);
     }
-    
+
     /**
      * Options can sometimes be an integer or a string instead of an array,
      * which functions as a handy shortcut for theme writers.  This converts
      * the short form of the options into its proper array form.
-     * 
+     *
      * @param string|integer|array $options
      * @return array
      */
@@ -140,16 +123,16 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
         }
         return $converted;
     }
-    
+
     /**
      * Retrieve the text associated with a given element or field of the record.
-     * 
+     *
      * @param Omeka_Record $record
      * @param string|array $metadata
      * @return string|array Either an array of ElementText records or a string.
      */
     protected function _getText($record, $metadata)
-    {        
+    {
         // If $metadata is a string, we assume that it refers to a
         // special value, e.g. id, item type name, date added, etc.
         if (is_string($metadata)) {
@@ -165,12 +148,11 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
         // argument.
         throw new Omeka_View_Exception('Unrecognized metadata specifier.');
     }
-    
+
     /**
-     * Retrieve record metadata that is not stored in the element_texts table. 
-     * Examples include the database ID of the item, the name of the item type, 
-     * the name of the collection, etc.
-     * 
+     * Retrieve record metadata that is not stored as ElementTexts.
+     *
+     * @uses Omeka_Record::getProperty()
      * @param Omeka_Record $record
      * @param string $specialValue Field name.
      * @return mixed
@@ -179,29 +161,29 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
     {
         return $record->getProperty(strtolower($specialValue));
     }
-    
+
     /**
      * Retrieve the set of ElementText records that correspond to a given
      * element set and element.
-     * 
+     *
      * @param Omeka_Record $record
      * @param string $elementSetName
      * @param string $elementName
      * @return array Set of ElementText records.
      */
     protected function _getElementText($record, $elementSetName, $elementName)
-    {        
+    {
         $elementTexts = $record->getElementTextsByElementNameAndSetName($elementName, $elementSetName);
-        
-        // Lock the records so that they can't be accidentally saved back to the 
-        // database, since we are modifying their values directly at this point. 
-        // Also clone the record because otherwise it would be passed by 
+
+        // Lock the records so that they can't be accidentally saved back to the
+        // database, since we are modifying their values directly at this point.
+        // Also clone the record because otherwise it would be passed by
         // reference to all the display filters, which results in munged text.
         foreach ($elementTexts as $key => $textRecord) {
             $elementTexts[$key] = clone $textRecord;
             $textRecord->lock();
         }
-        
+
         return $elementTexts;
     }
 
@@ -228,7 +210,7 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
             $elementText = false;
             $isHtml = false;
         }
-        
+
         // Apply the snippet option before escaping text HTML. If applied after
         // escaping the HTML, this may result in invalid markup.
         if ($snippet) {
@@ -247,19 +229,19 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
 
         return $text;
     }
-    
+
     /**
-     * Apply filters to a set of element text.
-     * 
-     * @param array|string $text
+     * Apply filters to a text value.
+     *
+     * @param string $text
      * @param ElementText|bool $elementText
-     * @return array|string
+     * @return string
      */
     protected function _filterText($text, $elementText)
     {
         $record = $this->_record;
         $metadata = $this->_metadata;
-        // Build the name of the filter to use. This will end up looking like: 
+        // Build the name of the filter to use. This will end up looking like:
         // array('Display', 'Item', 'Dublin Core', 'Title') or something similar.
         $filterName = array('Display', get_class($record));
         if (is_array($metadata)) {
@@ -272,8 +254,8 @@ class Omeka_View_Helper_RecordMetadata extends Zend_View_Helper_Abstract
             $text = apply_filters($filterName, $text, $record, $elementText);
         } else {
             $text = apply_filters($filterName, $text, $record);
-        }     
-        
+        }
+
         return $text;
     }
 }
