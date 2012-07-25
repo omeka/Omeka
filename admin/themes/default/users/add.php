758f440d04695ba290e3a3eedccf3dec01da7adb
Kim Nguyen <design@kimisgold.com>
2012-07-25 10:47:19 -0400

    Updates user activation page with responsive markup and decreases content width for users/add.

--- admin/themes/default/users/add.php
+++ admin/themes/default/users/add.php
@@ -2,6 +2,8 @@
 $pageTitle = __('Add New User');
 head(array('title'=>$pageTitle, 'content_class' => 'vertical-nav', 'bodyclass'=>'users primary'));?>
 
+<div class="seven columns alpha">
 <?php echo $this->form; ?>
+</div>
 
 <?php foot();?>
