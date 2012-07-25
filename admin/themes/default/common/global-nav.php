28e5bad6afad8821f433ee1e6b8b0d6aa3efb0ce
Kim Nguyen <design@kimisgold.com>
2012-07-25 14:10:40 -0400

    Sets permissions on the global nav.

--- admin/themes/default/common/global-nav.php
+++ admin/themes/default/common/global-nav.php
@@ -1,11 +1,20 @@
+<?php if(has_permission('Plugins','edit')): ?>
 <ul id="global-nav">
 <?php
-    $globalNav = array(
-        __('Plugins') => uri('plugins'),
-        __('Themes') => uri('themes'),
-        __('Users') => uri('users'),
-        __('Settings') => uri('settings')
-        );
+    $globalNav = array();
+    if(has_permission('Plugins', 'edit')) {
+        $globalNav[__('Plugins')] = uri('plugins');
+    }
+    if(has_permission('Themes', 'edit')) {
+        $globalNav[__('Themes')] = uri('themes');
+    }
+    if(has_permission('Users', 'edit')) {
+        $globalNav[__('Users')] = uri('users');
+    }
+    if(has_permission('Settings', 'edit')) {
+        $globalNav[__('Settings')] = uri('settings');
+    }
     echo nav(apply_filters('admin_navigation_global', $globalNav));
 ?>    
 </ul>
+<?php endif; ?>
