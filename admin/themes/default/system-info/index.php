a59f783cedbd868dd00b0c0a9e40ce181b27443a
John Flatness <john@zerocrates.org>
2012-07-26 15:53:46 -0400

    Deuglify System Information panel.
    People haven't been cutting and pasting it anyway.

--- admin/themes/default/system-info/index.php
+++ admin/themes/default/system-info/index.php
@@ -3,19 +3,15 @@ $title = __('System Information');
 head(array('title' => $title, 'bodyclass' => 'system-info')); ?>
 
 <?php echo flash(); ?>
-
-<pre id="info-field">
-<?php
-foreach ($info as $category => $entries) {
-    echo html_escape(__($category)) . "\n";
-
-    foreach ($entries as $name => $value) {
-        printf("    %-20s%s\n", html_escape($name) . ':', html_escape($value));
-    }
-
-    echo "\n";
-}
-?>
-</pre>
-
+<table>
+<?php foreach ($info as $category => $entries): ?>
+    <tr><th colspan="2"><?php echo html_escape($category); ?></th></tr>
+    <?php foreach ($entries as $name => $value): ?>
+    <tr>
+        <td><?php echo html_escape($name); ?></td>
+        <td><?php echo html_escape($value); ?></td>
+    </tr>
+    <?php endforeach; ?>
+<?php endforeach; ?>
+</table>
 <?php foot();
