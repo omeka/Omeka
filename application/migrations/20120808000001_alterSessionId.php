8720c84391cb5dd41e26a8b6c118ee98b446ebea
John Flatness <john@zerocrates.org>
2012-08-09 16:50:59 -0400

    Fix session migration for different prefixes.

--- application/migrations/20120808000001_alterSessionId.php
+++ application/migrations/20120808000001_alterSessionId.php
@@ -4,7 +4,7 @@ class alterSessionId extends Omeka_Db_Migration_AbstractMigration
     public function up()
     {
         $sql = <<<SQL
-ALTER TABLE  `omeka_sessions` 
+ALTER TABLE  `{$this->db->Session}`
 CHANGE `id` `id` VARCHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  ''
 SQL;
         $this->db->query($sql);
