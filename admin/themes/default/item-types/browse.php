718717f4aee96556af2128cdee0514e4fcc5028a
Kim Nguyen <design@kimisgold.com>
2012-08-10 15:13:47 -0400

    Makes 'Edit' link translatable.

--- admin/themes/default/item-types/browse.php
+++ admin/themes/default/item-types/browse.php
@@ -25,7 +25,7 @@ head(array('title'=>$pageTitle,'bodyclass'=>'item-types')); ?>
         <a href="<?php echo html_escape(record_uri($itemtype, 'show', 'item-types')); ?>"><?php echo html_escape($itemtype->name); ?></a>
         <ul class="action-links group">
         <?php if (has_permission('ItemTypes', 'edit')): ?>
-            <li><a class="edit" href="<?php echo html_escape(uri('item-types/edit/'.$itemtype->id)); ?>">Edit</a></li>
+            <li><a class="edit" href="<?php echo html_escape(uri('item-types/edit/'.$itemtype->id)); ?>"><?php echo __('Edit'); ?></a></li>
         <?php endif; ?>        
         </ul>
     </td>
