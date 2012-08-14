a36164165c7d5e448a44c4a8f486d74d717863be
Jim Safley <jimsafley@gmail.com>
2012-08-13 22:58:00 -0400

    Fix unique key in search_texts

--- application/core/schema/search_texts.sql
+++ application/core/schema/search_texts.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%search_texts` (
   `title` tinytext COLLATE utf8_unicode_ci,
   `text` longtext COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `record_name` (`record_name`,`record_id`),
+  UNIQUE KEY `record_name` (`record_type`,`record_id`),
   FULLTEXT KEY `text` (`text`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
