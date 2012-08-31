911dd9bb7871dbd9f631ab7403786f52ade9812f
Will <wanderingwill@gmail.com>
2012-08-31 19:22:31 -0400

    Fixed issue #247.

--- admin/themes/default/collections/add.php
+++ admin/themes/default/collections/add.php
@@ -7,6 +7,17 @@ head(array('title'=>$pageTitle, 'bodyclass'=>'collections')); ?>
                 <div id="save" class="three columns omega">
                     <div class="panel">
                         <input type="submit" class="big green button" name="submit" value="<?php echo __('Save Collection'); ?>" />
+                        <div id="public-featured">
+                            <?php echo $this->formLabel('public', __('Public')); ?>
+                        <?php 
+                            echo $this->formCheckbox('public', $collection->public, array(), array('1', '0'));
+                        ?>
+
+                            <?php echo $this->formLabel('featured', __('Featured')); ?>
+                        <?php 
+                            echo $this->formCheckbox('featured', $collection->featured, array(), array('1', '0')); 
+                        ?>
+                        </div>
                     </div>
                 </div>
             </form>
