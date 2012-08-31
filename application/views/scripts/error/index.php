0fd2caee2ef4ea795e7ca9cc81693755bf8516a1
Kim Nguyen <design@kimisgold.com>
2012-08-31 18:40:30 -0400

    Updates error pages to new theme styles.

--- application/views/scripts/error/index.php
+++ application/views/scripts/error/index.php
@@ -9,26 +9,30 @@
     <meta charset="utf-8">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" media="all" href="<?php echo WEB_VIEW_SCRIPTS . '/css/style.css'; ?>">
+    <link rel="stylesheet" media="all" href="<?php echo WEB_VIEW_SCRIPTS . '/css/skeleton.css'; ?>">
+    <link rel="stylesheet" media="all" href="<?php echo WEB_VIEW_SCRIPTS . '/css/layout.css'; ?>">
 </head>
 <body id="debug">
-    <div id="content">
-        <h1><?php echo $title; ?></h1>
-        <?php if ($displayError): ?>
-            <?php if (is_string($e)): ?>
-            <p><?php echo nl2br($e); ?></p>
+    <div class="container container-sixteen">
+        <div id="content" class="eight columns offset-by-four">
+            <h1><?php echo $title; ?></h1>
+            <?php if ($displayError): ?>
+                <?php if (is_string($e)): ?>
+                <p><?php echo nl2br($e); ?></p>
+                <?php else: ?>
+                <dl id="error-message">
+                    <dt><?php echo get_class($e); ?></dt>
+                    <dd>
+                        <p><?php echo nl2br($e->getMessage()); ?></p>
+                    </dd>
+                </dl>
+                <pre id="backtrace"><?php echo $e->getTraceAsString(); ?></pre>
+                <?php endif; ?>
             <?php else: ?>
-            <dl id="error-message">
-                <dt><?php echo get_class($e); ?></dt>
-                <dd>
-                    <p><?php echo nl2br($e->getMessage()); ?></p>
-                </dd>
-            </dl>
-            <pre id="backtrace"><?php echo $e->getTraceAsString(); ?></pre>
+                <p><?php echo __('To learn how to see more detailed information about this error, see the Omeka Codex page on <a href="http://omeka.org/codex/Retrieving_error_messages">retrieving error messages</a>.'); ?>
+                </p>
             <?php endif; ?>
-        <?php else: ?>
-            <p><?php echo __('To learn how to see more detailed information about this error, see the Omeka Codex page on <a href="http://omeka.org/codex/Retrieving_error_messages">retrieving error messages</a>.'); ?>
-            </p>
-        <?php endif; ?>
+        </div>
     </div>
 </body>
 </html>
