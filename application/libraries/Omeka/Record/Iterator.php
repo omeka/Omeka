98c6ae28e1a66f9424e905e37fb4fcbe854dac9c
Jim Safley <jimsafley@gmail.com>
2012-09-05 23:59:57 -0400

    Allow loops within loops.

--- application/libraries/Omeka/Record/Iterator.php
+++ application/libraries/Omeka/Record/Iterator.php
@@ -1,5 +1,56 @@
 <?php
 class Omeka_Record_Iterator implements Iterator
 {
+    private $_recordParam;
+    private $_records;
+    private $_view;
     
+    /**
+     * Construct the record iterator.
+     * 
+     * @param string $recordType
+     * @param array $records
+     * @param Zend_View_Abstract|null $view
+     */
+    public function __construct($recordType, $records, $view = null)
+    {
+        $this->_recordParam = Inflector::underscore($recordType);
+        $this->_records = $records;
+        $this->_view = $view;
+    }
+    
+    public function rewind()
+    {
+        reset($this->_records);
+    }
+    
+    /**
+     * Return the current record, setting it to the view if applicable.
+     */
+    public function current()
+    {
+        if ($this->_view instanceof Zend_View_Abstract) {
+            $this->_view->{$this->_recordParam} = current($this->_records);
+        }
+        return current($this->_records);
+    }
+    
+    public function key()
+    {
+        return key($this->_records);
+    }
+    
+    /**
+     * Release the previous record and advance the pointer to the next one.
+     */
+    public function next()
+    {
+        release_object($this->_records[$this->key()]);
+        next($this->_records);
+    }
+    
+    public function valid()
+    {
+        return false !== $this->current();
+    }
 }
--- application/views/helpers/LoopRecords.php
+++ application/views/helpers/LoopRecords.php
@@ -1,53 +1,18 @@
 <?php
-class Omeka_View_Helper_LoopRecords extends Zend_View_Helper_Abstract implements Iterator
+class Omeka_View_Helper_LoopRecords extends Zend_View_Helper_Abstract
 {
-    private $_records = array();
-    private $_recordParam;
-    
     /**
-     * Return an iterator meant to be used for looping records.
-     * 
-     * This sets the current record to the view and returns it. It also releases 
-     * the previous record to prevent memory leaks.
+     * Return an iterator used for looping and array of records.
      * 
      * @param string $recordType
      * @param array|null $records
-     * @return Omeka_View_Helper_LoopRecords
+     * @return Omeka_Record_Iterator
      */
     public function loopRecords($recordType, $records = null)
     {
         if (!is_array($records)) {
             $records = $this->view->{Inflector::tableize($recordType)};
         }
-        $this->_records = $records;
-        $this->_recordParam = Inflector::underscore($recordType);
-        return $this;
-    }
-    
-    public function rewind()
-    {
-        reset($this->_records);
-    }
-    
-    public function current()
-    {
-        $this->view->{$this->_recordParam} = current($this->_records);
-        return current($this->_records);
-    }
-    
-    public function key()
-    {
-        return key($this->_records);
-    }
-    
-    public function next()
-    {
-        release_object($this->_records[$this->key()]);
-        next($this->_records);
-    }
-    
-    public function valid()
-    {
-        return false !== $this->current();
+        return new Omeka_Record_Iterator($recordType, $records, $this->view);
     }
 }
