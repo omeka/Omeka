2290346a4f567c9b67b4e0218ebfdee811a1df0c
Jim Safley <jimsafley@gmail.com>
2012-09-07 15:37:38 -0400

    Fix overlooked get_current_item.

--- admin/themes/default/items/change-type.php
+++ admin/themes/default/items/change-type.php
@@ -5,6 +5,6 @@
 </div>
 <?php 
 //Loop through all of the element records for the item's item type
-$elements = get_current_item()->getItemTypeElements(); 
-echo display_form_input_for_element($elements, get_current_item());
+$elements = get_current_record('item')->getItemTypeElements(); 
+echo display_form_input_for_element($elements, get_current_record('item'));
 ?>
