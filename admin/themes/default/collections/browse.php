2b114119d04bd85a70e11626008feaeca6cb2a0c
Kim Nguyen <design@kimisgold.com>
2012-09-10 16:44:12 -0400

    Remove extra button from empty collections page, update add button class.

--- admin/themes/default/collections/browse.php
+++ admin/themes/default/collections/browse.php
@@ -2,12 +2,11 @@
 $pageTitle = __('Browse Collections') . ' ' .  __('(%s total)', $total_records);
 head(array('title'=>$pageTitle, 'bodyclass'=>'collections')); ?>
 
-<?php if (has_permission('Collections', 'add')): ?>
-    <a href="<?php echo html_escape(uri('collections/add')); ?>" class="small green button"><?php echo __('Add a Collection'); ?></a>
-<?php endif; ?>
-
     <?php echo flash(); ?>
     <?php if (total_collections() > 0): ?>
+        <?php if (has_permission('Collections', 'add')): ?>
+            <a href="<?php echo html_escape(uri('collections/add')); ?>" class="small green button"><?php echo __('Add a Collection'); ?></a>
+        <?php endif; ?>
         <div class="pagination"><?php echo pagination_links(); ?></div>
       <?php if (has_loop_records('collections')): ?>
         <table id="collections" class="full" cellspacing="0" cellpadding="0">
