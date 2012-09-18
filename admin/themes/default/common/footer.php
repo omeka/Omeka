c1ce8b6a27577d128bb7b5ae1021315f16545849
Kim Nguyen <design@kimisgold.com>
2012-09-18 14:13:17 -0400

    Fix and relocate jQuery for mobile tables

--- admin/themes/default/common/footer.php
+++ admin/themes/default/common/footer.php
@@ -17,38 +17,6 @@
 
 </section>
 
-<script type="text/javascript">
-  // generate mobile version for each table
-  jQuery('table').each(function() {
-    var table = jQuery(this); // cache table object
-    var head = table.find('thead th');
-    var rows = table.find('tbody tr').clone(); // appending afterwards does not break original table
-
-    // create new table
-    var newtable = jQuery(
-      '<table class="mobile">' +
-      '  <tbody>' +
-      '  </tbody>' +
-      '</table>'
-    );
-
-    // cache tbody where we'll be adding data
-    var newtable_tbody = newtable.find('tbody');
-
-    rows.each(function(i) {
-      var cols = jQuery(this).find('td');
-      var classname = i % 2 ? 'even' : 'odd';
-      cols.each(function(k) {
-        var new_tr = jQuery('<tr class="' + classname + '"></tr>').appendTo(newtable_tbody);
-        new_tr.append(head.clone().get(k));
-        new_tr.append(jQuery(this));
-      });
-    });
-
-    jQuery(this).after(newtable);
-  });
-</script>
-
 </body>
 
 </html>
--- admin/themes/default/element-sets/edit.php
+++ admin/themes/default/element-sets/edit.php
@@ -7,26 +7,26 @@ head(array('title'=> $pageTitle, 'content_class' => 'vertical-nav', 'bodyclass'=
 <h2><?php echo __($elementSet->name); ?></h2>
 <p><?php echo __($elementSet->description); ?></p>
 <form method='post'>
-<table>
-    <thead>
-    <tr>
-        <th><?php echo __('Name'); ?></th>
-        <th><?php echo __('Description'); ?></th>
-        <th><?php echo __('Comment'); ?></th>
-        <th><?php echo __('Order'); ?></th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($elementSet->getElements() as $element): ?>
-    <tr>
-        <td><?php echo __($element->name); ?></td>
-        <td><?php echo __($element->description); ?></td>
-        <td><?php echo $this->formTextarea("elements[{$element->id}][comment]", $element->comment, array('rows' => '5', 'cols' => '40')); ?></td>
-        <td><?php echo $this->formText("elements[{$element->id}][order]", $element->order, array('size' => '3')); ?></td>
-    </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+    <table>
+        <thead>
+        <tr>
+            <th><?php echo __('Name'); ?></th>
+            <th><?php echo __('Description'); ?></th>
+            <th><?php echo __('Comment'); ?></th>
+            <th><?php echo __('Order'); ?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($elementSet->getElements() as $element): ?>
+        <tr>
+            <td><?php echo __($element->name); ?></td>
+            <td><?php echo __($element->description); ?></td>
+            <td><?php echo $this->formTextarea("elements[{$element->id}][comment]", $element->comment, array('rows' => '5', 'cols' => '40')); ?></td>
+            <td><?php echo $this->formText("elements[{$element->id}][order]", $element->order, array('size' => '3')); ?></td>
+        </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 <?php echo $this->formSubmit('submit_edit_elements', 'Edit Elements'); ?>
 </form>
 <?php foot(); ?>
