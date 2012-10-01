be8c3388e61a6ca01712f869f79bf5c0faa0697a
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-10-01 09:31:06 -0400

    Adds an AdminForm, with some elements and a decorator, to build forms on the admin side. See #297
    Idea is to have an AdminForm class that conforms to the new structure
    and CSS of the admin theme, which makes it easier for plugin builders
    and us add elements to the appropriate parts of the admin form.
    
    AdminForm has methods for adding elements to the main edit area and to
    the save panel. There are elements for common stuff in the save panel
    (save, public page link, delete link), hopefully in a way to help plugin
    builders add their own elements easily.
    
    Not sure if I've followed naming conventions properly.

---
+++ application/libraries/Omeka/Form/Admin.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * A Zend_Form subclass to set up a record editing form for the Omeka 2.0 admin user interface
+ * 
+ * 
+ */
+
+class Omeka_Form_Admin extends Omeka_Form
+{
+    protected $_editDisplayGroup;
+    
+    protected $_saveDisplayGroup;
+    
+    protected $_record;
+    
+    protected $_hasPublicPage = true;
+        
+    protected $_editGroupCssClass = 'seven columns alpha';
+    
+    protected $_saveGroupCssClass = 'three columns omega panel';
+    
+    
+    public function init()
+    {
+        parent::init();
+        
+        //instead of extending Zend_Form_DisplayGroup, setting up here so css classes can be options directly
+        //on instantiating the form. If those classes should never change, then this probably should go to 
+        //Omeka extensions of Zend_Form_DisplayGroup
+
+        $this->_editDisplayGroup = new Zend_Form_DisplayGroup('edit-form', $this->getPluginLoader(self::DECORATOR));
+        $this->_saveDisplayGroup = new Zend_Form_DisplayGroup('save', $this->getPluginLoader(self::DECORATOR));
+        
+        $this->addDisplayGroups(array($this->_editDisplayGroup, $this->_saveDisplayGroup));
+        
+        //create the decorators with CSS classes set up via options 
+        $editDecorator = new Zend_Form_Decorator_HtmlTag(array('tag'=>'div', 'class'=>$this->_editGroupCssClass));
+        $saveDecorator = new Zend_Form_Decorator_HtmlTag(array('tag'=>'div', 'id'=>'save', 'class'=>$this->_saveGroupCssClass));
+                
+        //Pro tip: order of adding decorators matters! if reversed, group elements would appear after the div!
+        $this->_editDisplayGroup->setDecorators(array('FormElements', $editDecorator));
+        $this->_saveDisplayGroup->setDecorators(array('FormElements', $saveDecorator));
+
+        $this->addElementToSaveGroup('AdminSave', 'submit');
+        
+        if($this->_record && $this->_hasPublicPage) {
+            $this->addElementToSaveGroup('AdminPublicPage', 'public-page');
+        }
+        
+        if($this->_record) {
+            $this->addElementToSaveGroup('AdminDelete', 'delete' , array('content'=>'Delete'));
+        }
+    }
+    
+    /**
+     * Add an element to the edit area
+     * 
+     * @see Zend_Form::addElement
+     * @param Zend_Form_Element|string $element
+     * @param string|null $name
+     * @param array|null $options
+     */
+    
+    public function addElementToEditGroup($element, $name, $options = null)
+    {
+        return $this->addElementToDisplayGroup('edit', $element, $name, $options );
+    }
+
+    /**
+     * Add an element to the save panel
+     *
+     * @see Zend_Form::addElement
+     * @param Zend_Form_Element|string $element
+     * @param string|null $name
+     * @param array|null $options
+     */
+    
+    public function addElementToSaveGroup($element, $name = null, $options = null)
+    {
+        return $this->addElementToDisplayGroup('save', $element, $name, $options );
+    }
+    
+    protected function addElementToDisplayGroup($group, $element, $name = null, $options = null)    
+    {
+        if(is_string($element) && is_null($name)) {
+            throw new Zend_Form_Exception('To add directly to a part of the admin edit page, you must give your element a name');
+        }
+        
+        //if no record is passed, assume we want to use the form's record on elements
+
+        if($options) {
+            if(!isset($options['record'])) {
+                $options['record'] = $this->_record;
+            }
+        } else {
+            $options = array('record'=>$this->_record);
+        }
+
+        
+        $this->addElement($element, $name, $options);
+        
+        $element = $this->getElement($name);
+
+        switch($group) {
+            case 'save':
+                $this->_saveDisplayGroup->addElement($element);
+                break;
+                
+            case 'edit':
+                $this->_editDisplayGroup->addElement($element);
+                break;
+        }     
+        return $this;           
+    }
+    
+    public function setEditGroupCssClass($cssClass)
+    {
+        $this->_editGroupCssClass = $cssClass;
+    }
+    
+    public function setSaveGroupCssClass($cssClass)
+    {
+        $this->_saveGroupCssClass = $cssClass;
+    }
+    
+    public function setRecord($record)
+    {
+        $this->_record = $record;
+    }
+    
+    public function setHasPublicPage($value = true)
+    {
+        $this->_hasPublicPage = $value;
+    }
+    
+}
---
+++ application/libraries/Omeka/Form/Element/AdminPublicPage.php
@@ -0,0 +1,21 @@
+<?php
+
+class Omeka_Form_Element_AdminPublicPage extends Omeka_Form_Element_AbstractAdmin
+{
+
+
+    protected function getDefaultDecorators()
+    {
+        //$decorators = parent::getDefaultDecorators();
+        $decorators = array();
+        set_theme_base_url('public');
+        $decorators[] =  new Omeka_Form_Decorator_Link(array('content'=>'View Public Page',                
+                'class'=>'big blue button',
+                'target'=>'_blank',
+                'href'=> record_url($this->_record, 'show')
+
+        ));
+        revert_theme_base_url();
+        return $decorators();
+    }
+}
