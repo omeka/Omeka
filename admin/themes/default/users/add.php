9ea5ed85dfee0ed777ab361cd46f9ad808b2409e
John Flatness <john@zerocrates.org>
2012-10-02 11:09:18 -0400

    Add flash messages to users form.

--- admin/themes/default/users/add.php
+++ admin/themes/default/users/add.php
@@ -2,6 +2,7 @@
 $pageTitle = __('Add New User');
 echo head(array('title'=>$pageTitle, 'content_class' => 'vertical-nav', 'bodyclass'=>'users primary'));?>
 
+<?php echo flash(); ?>
 <div class="seven columns alpha">
 <?php echo $this->form; ?>
 </div>
