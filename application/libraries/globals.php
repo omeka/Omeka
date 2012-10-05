omeka/Omeka

commit aa3086b8bd195c396d82e509e115a37b0a03b3a7

Add function that checks if a record has tags. Fix #137

application/libraries/globals.php
@@ -1901,7 +1901,7 @@ function item_has_tags($item = null)
     if (!$item) {
         $item = get_current_record('item');
     }
-    return (count($item->Tags) > 0);
+    return has_tags($item);
 }
 
 /**
@@ -2912,6 +2912,21 @@ function tag_string($recordOrTags = null, $link = 'items/browse', $delimiter = n
         }
     }
     return join(html_escape($delimiter), $tagStrings);
+}
+
+/**
+ * Check whether the specified record has tags.
+ *
+ * @param Omeka_Record $record
+ * @return bool
+ */
+function has_tags(Omeka_Record $record)
+{
+    try {
+        return (bool) $record->getTags();
+    } catch (BadMethodCallException $e) {
+        return false;
+    }
 }
 
 /**

commit 32098647e938672c714d452ca546a4e4562ef5a7

Fix bug introduced in aa3086b.

application/libraries/globals.php
@@ -2917,10 +2917,10 @@ function tag_string($recordOrTags = null, $link = 'items/browse', $delimiter = n
 /**
  * Check whether the specified record has tags.
  *
- * @param Omeka_Record $record
+ * @param Omeka_Record_AbstractRecord $record
  * @return bool
  */
-function has_tags(Omeka_Record $record)
+function has_tags(Omeka_Record_AbstractRecord $record)
 {
     try {
         return (bool) $record->getTags();