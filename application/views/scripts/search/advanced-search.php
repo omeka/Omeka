57bd74d8177206db606377db56de106dace53555
Jim Safley <jimsafley@gmail.com>
2012-10-08 18:21:22 -0400

    Search all record types by default. Ref #216

--- application/views/scripts/search/advanced-search.php
+++ application/views/scripts/search/advanced-search.php
@@ -8,7 +8,7 @@ echo head(array('title' => $pageTitle));
     <?php echo $this->formCheckbox('boolean', null, array('disableHidden' => true)); ?> boolean<br />
     Search only these record types:<br />   
     <?php foreach ($this->searchRecordTypes as $searchRecordType): ?>
-    <input type="checkbox" name="record_types[]" value="<?php echo $searchRecordType; ?>" /> <?php echo $searchRecordType; ?><br />
+    <input type="checkbox" name="record_types[]" value="<?php echo $searchRecordType; ?>" checked="checked" /> <?php echo $searchRecordType; ?><br />
     <?php endforeach; ?><br />
     <?php echo $this->formSubmit(null, __('Search Omeka')); ?>
 </form>
