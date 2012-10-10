0ec9d3a51ad83e447b8999885bc5ba175d91362c
Jim Safley <jimsafley@gmail.com>
2012-10-10 13:27:16 -0400

    Move paths.php to bootstrap.php.

--- admin/index.php
+++ admin/index.php
@@ -1,39 +1,15 @@
 <?php
 /**
- * Bootstrap for admin interface.  
+ * Bootstrap for admin interface.
  *
  * This is the same as the public interface bootstrap, except it defines an
- * ADMIN constant and sets an 'admin' parameter in the web request to ensure
- * that Omeka loads the correct view scripts (and any other theme-specific
- * behavior).  
+ * ADMIN constant used by the bootstrap script to ensure that Omeka loads the 
+ * correct view scripts (and any other theme-specific behavior).
  *
- * @copyright Roy Rosenzweig Center for History and New Media, 2007-2011
+ * @copyright Roy Rosenzweig Center for History and New Media, 2007-2012
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @package Omeka
- * @access private
  */
 
-// Flag this as the admin theme. Used by _define_web_root() function in paths.php.
+// Flag this as the admin theme.
 define('ADMIN', true);
-
-// Define the directory and web paths.
-include dirname(dirname(__FILE__)) . '/paths.php';
-
-// Define the admin theme directory path.
-define('THEME_DIR', ADMIN_THEME_DIR);
-
-$application = new Omeka_Application(APPLICATION_ENV);
-// Configure the Theme bootstrap resource with the correct paths/URLs.
-$application->getBootstrap()->setOptions(array(
-    'resources' => array(
-        'theme' => array(
-            'basePath' => THEME_DIR,
-            'webBasePath' => WEB_THEME
-        )
-    )
-));
-
-// This is used by the global is_admin_theme to detect that this is the admin.
-Zend_Controller_Front::getInstance()->setParam('admin', true);
-
-$application->initialize()->run();
+include dirname(dirname(__FILE__)) . '/bootstrap.php';
---
+++ bootstrap.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Bootstrap the Omeka application.
+ * 
+ * @copyright Roy Rosenzweig Center for History and New Media, 2009-2012
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+// Define the current version of Omeka.
+define('OMEKA_VERSION', '2.0-dev');
+
+// Define the application environment.
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (
+    getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'
+));
+
+// Define the name of the item type element set. This is used wherever it is 
+// important to distinguish this special case element set from others.
+define('ELEMENT_SET_ITEM_TYPE', 'Item Type Metadata');
+
+// Define directory path constants.
+
+define('BASE_DIR', dirname(__FILE__));
+define('APP_DIR', BASE_DIR . '/application');
+define('PLUGIN_DIR', BASE_DIR . '/plugins');
+define('ADMIN_DIR', BASE_DIR . '/admin');
+define('FILES_DIR', BASE_DIR . '/files');
+define('ADMIN_THEME_DIR', BASE_DIR . '/admin/themes');
+define('PUBLIC_THEME_DIR', BASE_DIR . '/themes');
+define('INSTALL_DIR', BASE_DIR . '/install');
+define('MODEL_DIR', APP_DIR . '/models');
+define('FORM_DIR', APP_DIR . '/forms');
+define('CONTROLLER_DIR', APP_DIR . '/controllers');
+define('LIB_DIR', APP_DIR . '/libraries');
+define('CONFIG_DIR', APP_DIR . '/config');
+define('LOGS_DIR', APP_DIR . '/logs');
+define('VIEW_SCRIPTS_DIR', APP_DIR . '/views/scripts');
+define('VIEW_HELPERS_DIR', APP_DIR . '/views/helpers');
+define('UPGRADE_DIR', APP_DIR . '/migrations');
+define('LANGUAGES_DIR', APP_DIR . '/languages');
+define('SCHEMA_DIR', APP_DIR . '/schema');
+define('SCRIPTS_DIR', APP_DIR . '/scripts');
+
+// Define the web address constants.
+
+// Set the scheme.
+$base_root = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
+
+// Set the domain.
+$base_url = $base_root .= '://' . preg_replace('/[^a-z0-9-:._]/i', '', $_SERVER['HTTP_HOST']);
+
+// Set to port, if any.
+$port = $_SERVER['SERVER_PORT'];
+if (($base_root == 'http' && $port != '80') || ($base_root == 'https' && $port != '443')) {
+    $base_url .= ":$port";
+}
+
+// Set the path.
+if ($dir = trim(dirname($_SERVER['SCRIPT_NAME']), '\,/')) {
+    $base_path  = "/$dir";
+    $base_url  .= $base_path;
+    $base_path .= '/';
+} else {
+    $base_path = '/';
+}
+
+// Remove the '/admin' part of the URL by regex, if necessary.
+if (defined('ADMIN')) {
+    $dir = preg_replace('/(.*)admin$/', '$1', $dir, 1);
+    $dir = rtrim($dir, '/');
+}
+
+// WEB_ROOT is always the root of the site, whereas WEB_DIR depends on the 
+// bootstrap used (public/admin)
+define('WEB_ROOT', $base_root . (!empty($dir) ? '/' . $dir : '') );
+define('WEB_DIR', $base_url);
+define('WEB_THEME', WEB_DIR . '/themes');
+define('WEB_PLUGIN', WEB_ROOT . '/plugins');
+define('WEB_FILES', WEB_ROOT . '/files');
+define('WEB_PUBLIC_THEME', WEB_ROOT . '/themes');
+define('WEB_VIEW_SCRIPTS', WEB_ROOT . '/application/views/scripts');
+
+// Get the directory that the bootstrap sits in.
+$dir = trim(dirname($_SERVER['SCRIPT_NAME']), '\,/');
+
+// current path should be empty and not a '/' if there is no directory path.
+$currentPath = !empty($dir) ? "/$dir" : '';
+$adminDir = 'admin';
+
+// This is how we determine whether or not we are in the admin bootstrap.
+if (defined('ADMIN')) {
+    $adminPath = $currentPath;
+    // Strip off the admin directory to get the public dir.
+    $publicPath = rtrim(preg_replace("/(.*)$adminDir$/", '$1', $currentPath, 1), '/');
+} else {
+    $adminPath = "$currentPath/$adminDir";
+    $publicPath = $currentPath;
+}
+
+define('ADMIN_BASE_URL', $adminPath);
+define('PUBLIC_BASE_URL', $publicPath);
+define('CURRENT_BASE_URL', $currentPath);
+
+// If date.timezone is not set, this will query the OS for the timezone and set 
+// that as the default. Workaround for PHP 5.3 behavior for timezones.
+date_default_timezone_set(@date_default_timezone_get());
+
+// Set the zlib config values if the extension has been loaded.
+if (extension_loaded('zlib')) {
+    ini_set('zlib.output_compression', true);
+    ini_set('zlib.output_compression_level', '5');
+}
+
+// Add the libraries and models directories to the include path.
+set_include_path(LIB_DIR. PATH_SEPARATOR . MODEL_DIR . PATH_SEPARATOR . get_include_path());
+
+// Set up the Zend_Loader autoloader to work for all classes. The Omeka 
+// namespace must be manually specified to avoid incompatibility with the
+// resource autoloader.
+require_once 'Zend/Loader/Autoloader.php';
+$autoloader = Zend_Loader_Autoloader::getInstance();
+$autoloader->registerNamespace('Omeka_');
+$autoloader->setFallbackAutoloader(true);
+$autoloader->suppressNotFoundWarnings(true);
+
+// Define the theme directory path.
+define('THEME_DIR', defined('ADMIN') ? ADMIN_THEME_DIR : PUBLIC_THEME_DIR);
+
+// Configure the bootstrap and run the application.
+$application = new Omeka_Application(APPLICATION_ENV);
+$application->getBootstrap()->setOptions(array(
+    'resources' => array(
+        'theme' => array(
+            'basePath' => THEME_DIR,
+            'webBasePath' => WEB_THEME
+        )
+    )
+));
+$application->initialize()->run();
