24d3f1d30a6ff619a8a57492b5c709c9924f3f31
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-10-10 16:01:27 -0400

    removes early return.

--- application/libraries/Omeka/Form/Decorator/SavePanelHook.php
+++ application/libraries/Omeka/Form/Decorator/SavePanelHook.php
@@ -4,7 +4,6 @@ class Omeka_Form_Decorator_SavePanelHook extends Zend_Form_Decorator_Abstract
 {    
     public function render($content)
     {
-        return $content;
         $prependHtml = fire_plugin_hooks('admin_append_to_panel_buttons');
         $appendHtml = fire_plugin_hooks('admin_append_to_panel_fields');
         return $prependHtml . $content . $appendHtml;
