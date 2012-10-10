484cad07498caa2ed2302b61119062083ca8e027
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-10-10 12:17:12 -0400

    Adds Decorator SavePanelHook back in.

---
+++ application/libraries/Omeka/Form/Decorator/SavePanelHook.php
@@ -0,0 +1,12 @@
+<?php
+
+class Omeka_Form_Decorator_SavePanelHook extends Zend_Form_Decorator_Abstract
+{    
+    public function render($content)
+    {
+        return $content;
+        $prependHtml = fire_plugin_hooks('admin_append_to_panel_buttons');
+        $appendHtml = fire_plugin_hooks('admin_append_to_panel_fields');
+        return $prependHtml . $content . $appendHtml;
+    }    
+}
