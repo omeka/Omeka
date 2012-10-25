9ca35d3f2580cca3d3a70dfe6b7f662610ff2d2d
Jeremy Boggs <jeremy@clioweb.org>
2012-10-25 11:46:21 -0400

    Fixes exception when there is no random featured collection.
    Updated `random_featured_collection` to not throw an exception if there is no
    random featured collection to display. The function instead displays a
    messsage that there are no featured collections available. Includes a test to
    check whether this message is displayed.

---
+++ application/tests/suite/Helpers/CollectionFunctions/RandomFeaturedCollectionTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright Roy Rosenzweig Center for History and New Media, 2011
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package Omeka
+ */
+
+/**
+ * Tests random_featured_collection()
+ *
+ * @package Omeka
+ */
+class Omeka_Helper_RandomFeaturedCollectionTest extends Omeka_Test_AppTestCase
+{
+
+    /**
+     * Checks that the correct message is displayed if there are no featured 
+     * collections to display.
+     */
+    public function testNoRandomFeaturedCollection()
+    {
+        $html = random_featured_collection();
+        $this->assertContains('No featured collections are available.', $html);
+    }
+
+}
