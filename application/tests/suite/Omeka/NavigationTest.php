6d5f62cbf78d030d463fe59a3f90e4ef071bcc83
Will <wanderingwill@gmail.com>
2012-10-31 02:55:34 -0400

    Add tests for adding Omeka_Navigation_Page_Uri pages to Omeka_Navigation.  Add test for adding a flat list of mixed pages to Omeka_Navigation.  Issue #346

--- application/tests/suite/Omeka/NavigationTest.php
+++ application/tests/suite/Omeka/NavigationTest.php
@@ -23,9 +23,9 @@ class Omeka_NavigationTest extends Omeka_Test_AppTestCase
     public function testEmptyOmekaNavigation()
     {
        $this->assertEquals(0, $this->_nav->count());
-       $this->assertEquals(false, $this->_nav->hasChildren());
-       $this->assertEquals(null, $this->_nav->getChildren());
-       $this->assertEquals(false, $this->_nav->hasPages());
+       $this->assertFalse($this->_nav->hasChildren());
+       $this->assertNull($this->_nav->getChildren());
+       $this->assertFalse($this->_nav->hasPages());
        $this->assertEquals(array(), $this->_nav->getPages());
        $this->assertEquals(array(), $this->_nav->toArray());
     }
@@ -436,5 +436,228 @@ class Omeka_NavigationTest extends Omeka_Test_AppTestCase
 
         $pageArray = $firstChild->toArray();
         $this->assertContains($pageArray, $this->_nav->toArray());
+    }
+    
+    public function testAddSingleExplicitVisibleOmekaNavigationPageUri()
+    {
+        $page = new Omeka_Navigation_Page_Uri(array(
+            'label' => __('Browse Items'),
+            'uri' => url('items/browse'),
+            'visible' => true
+        ));
+        
+        $this->assertNull($page->get('uid'));
+        
+        $this->_nav->addPage($page);
+        
+        $this->assertEquals(CURRENT_BASE_URL, PUBLIC_BASE_URL);
+        $uid = PUBLIC_BASE_URL . '/items/browse';
+        
+        $this->assertEquals($uid, $page->get('uid'));         
+        
+        $this->assertEquals(1, $this->_nav->count());
+        $this->assertTrue($this->_nav->hasChildren());
+        
+        $firstChild = $this->_nav->getChildren();
+        $this->assertEquals($page, $firstChild);
+        $this->assertInstanceOf('Omeka_Navigation_Page_Uri', $firstChild);
+        
+        $this->assertEquals(__('Browse Items'), $firstChild->getLabel());
+        $this->assertEquals(url('items/browse'), $firstChild->getUri());
+        $this->assertTrue($firstChild->getVisible());
+        $this->assertEquals(0, $firstChild->getOrder());
+        
+        $this->assertEquals($uid, $firstChild->get('uid'));
+        $this->assertEquals($uid, $firstChild->getHref());
+            
+        $this->assertTrue($this->_nav->hasPages());
+        $pages = $this->_nav->getPages();
+        
+        $this->assertContainsOnly('Omeka_Navigation_Page_Uri', $pages);
+        $this->assertContains($page, $pages);
+        $this->assertContains($firstChild, $pages);
+
+        $foundPage = $this->_nav->findOneBy('uid', $uid);
+        $this->assertEquals($page, $foundPage);
+        $this->assertEquals($firstChild, $foundPage);
+        
+        $pageArray = $firstChild->toArray();
+        $this->assertContains($pageArray, $this->_nav->toArray());
+    }
+    
+    public function testAddSingleImplicitVisibleOmekaNavigationPageUri()
+    {
+        $page = new Omeka_Navigation_Page_Uri(array(
+            'label' => __('Browse Items'),
+            'uri' => url('items/browse'),
+        ));
+        
+        $this->assertNull($page->get('uid'));
+        
+        $this->_nav->addPage($page);
+        
+        $this->assertEquals(CURRENT_BASE_URL, PUBLIC_BASE_URL);
+        $uid = PUBLIC_BASE_URL . '/items/browse';
+        
+        $this->assertEquals($uid, $page->get('uid'));         
+                
+        $this->assertEquals(1, $this->_nav->count());
+        $this->assertTrue($this->_nav->hasChildren());
+        
+        $firstChild = $this->_nav->getChildren();
+        $this->assertEquals($page, $firstChild);
+        $this->assertInstanceOf('Omeka_Navigation_Page_Uri', $firstChild);
+        
+        $this->assertEquals(__('Browse Items'), $firstChild->getLabel());
+        $this->assertEquals(url('items/browse'), $firstChild->getUri());
+        $this->assertTrue($firstChild->getVisible());
+        $this->assertEquals(0, $firstChild->getOrder());
+        
+        $this->assertEquals($uid, $firstChild->get('uid'));
+        $this->assertEquals($uid, $firstChild->getHref());
+            
+        $this->assertTrue($this->_nav->hasPages());
+        $pages = $this->_nav->getPages();
+        
+        $this->assertContainsOnly('Omeka_Navigation_Page_Uri', $pages);
+        $this->assertContains($page, $pages);
+        $this->assertContains($firstChild, $pages);
+
+        $foundPage = $this->_nav->findOneBy('uid', $uid);
+        $this->assertEquals($page, $foundPage);
+        $this->assertEquals($firstChild, $foundPage);
+        
+        $pageArray = $firstChild->toArray();
+        $this->assertContains($pageArray, $this->_nav->toArray());
+    }
+    
+    public function testAddSingleExplicitNotVisibleOmekaNavigationPageUri()
+    {
+        $page = new Omeka_Navigation_Page_Uri(array(
+            'label' => __('Browse Items'),
+            'uri' => url('items/browse'),
+            'visible' => false
+        ));
+        
+        $this->assertNull($page->get('uid'));
+        
+        $this->_nav->addPage($page);
+        
+        $this->assertEquals(CURRENT_BASE_URL, PUBLIC_BASE_URL);
+        $uid = PUBLIC_BASE_URL . '/items/browse';
+
+        $this->assertEquals($uid, $page->get('uid'));         
+        
+        $this->assertEquals(1, $this->_nav->count());
+        $this->assertTrue($this->_nav->hasChildren());
+        
+        $firstChild = $this->_nav->getChildren();
+        $this->assertEquals($page, $firstChild);
+        $this->assertInstanceOf('Omeka_Navigation_Page_Uri', $firstChild);
+        
+        $this->assertEquals(__('Browse Items'), $firstChild->getLabel());
+        $this->assertEquals(url('items/browse'), $firstChild->getUri());
+        $this->assertFalse($firstChild->getVisible());
+        $this->assertEquals(0, $firstChild->getOrder());
+        
+        $this->assertEquals($uid, $firstChild->get('uid'));
+        $this->assertEquals($uid, $firstChild->getHref());
+            
+        $this->assertTrue($this->_nav->hasPages());
+        $pages = $this->_nav->getPages();
+        
+        $this->assertContainsOnly('Omeka_Navigation_Page_Uri', $pages);
+        $this->assertContains($page, $pages);
+        $this->assertContains($firstChild, $pages);
+
+        $foundPage = $this->_nav->findOneBy('uid', $uid);
+        $this->assertEquals($page, $foundPage);
+        $this->assertEquals($firstChild, $foundPage);
+        
+        $pageArray = $firstChild->toArray();
+        $this->assertContains($pageArray, $this->_nav->toArray());
+    }
+    
+    public function addDifferentPagesFlatArray()
+    {
+        $explicitVisibleZendNavPageMvc = new Zend_Navigation_Page_Mvc(array(
+            'label' => __('Browse Items'),
+            'controller' => 'items',
+            'action' => 'browse',
+            'visible' => true
+        ));
+        
+        $implicitVisibleZendNavPageUri = new Zend_Navigation_Page_Uri(array(
+            'label' => __('Browse Collections'),
+            'uri' => url('collections/browse'),
+        ));
+        
+        $notVisibleNavPageUriArray = array(
+            'label' => __('Edit Items'),
+            'uri' => url('items/edit'),
+            'visible' => false
+        );
+        
+        $notVisibleOmekaNavPageUri = new Omeka_Navigation_Page_Uri(array(
+            'label' => __('Edit Collections'),
+            'uri' => url('collections/edit'),
+            'visible' => false
+        ));
+        
+        $explicitVisibleOmekaNavPageUri = new Omeka_Navigation_Page_Uri(array(
+            'label' => __('Omeka'),
+            'uri' => 'http://omeka.org',
+            'visible' => true
+        ));
+        
+        $pages = array(
+            $explicitVisibleZendNavPageMvc,
+            $implicitVisibleZendNavPageUri,
+            $notVisibleNavPageUriArray,
+            $notVisibleOmekaNavPageUri,
+            $explicitVisibleOmekaNavPageUri
+        );
+        
+        $this->_nav->addPages($pages);
+        
+        $this->assertEquals(5, $this->_nav->count());
+        $this->assertTrue($this->_nav->hasChildren());
+        $this->assertTrue($this->_nav->hasPages());
+        
+        $addedPages = $this->_nav->getPages();
+        $this->assertEquals(5, $addedPages);
+        
+        $afterPage1 = $addedPages[0];
+        $afterPage2 = $addedPages[1];
+        $afterPage3 = $addedPages[2];
+        $afterPage4 = $addedPages[3];
+        $afterPage5 = $addedPages[4];
+        
+        $this->assertEquals($explicitVisibleZendNavPageMvc, $afterPage1);
+        $this->assertNotEquals($implicitVisibleZendNavPageUri, $afterPage2);
+        $this->assertNotEquals($notVisibleNavPageUriArray, $afterPage3);
+        $this->assertEquals($notVisibleOmekaNavPageUri, $afterPage4);
+        $this->assertEquals($explicitVisibleOmekaNavPageUri, $afterPage5);
+
+        $this->assertInstanceOf('Omeka_Navigation_Page_Uri', $afterPage2);
+        $this->assertInstanceOf('Omeka_Navigation_Page_Uri', $afterPage3);
+
+        $this->assertEquals(0, $afterPage1->getOrder());
+        $this->assertEquals(1, $afterPage2->getOrder());
+        $this->assertEquals(2, $afterPage3->getOrder());
+        $this->assertEquals(3, $afterPage4->getOrder());
+        $this->assertEquals(4, $afterPage5->getOrder());
+        
+        $this->assertEquals(url('items/browse'), $afterPage1->getUid());
+        $this->assertEquals(url('collections/browse'), $afterPage2->getUid());
+        $this->assertEquals(url('items/edit'), $afterPage3->getUid());
+        $this->assertEquals(url('collections/edit'), $afterPage4->getUid());
+        $this->assertEquals('http://omeka.org', $afterPage5->getUid());
+
+        $this->assertEquals(__('Browse Items'), $afterPage1->getLabel());
+        $this->assertEquals(__('Browse Collections'), $afterPage2->getLabel());
+        $this->assertEquals(__('Edit Items'), $afterPage3->getLabel());
+        $this->assertEquals(__('Edit Collections'), $afterPage4->getLabel());
+        $this->assertEquals(__('Omeka'), $afterPage5->getLabel());
     }
 }
