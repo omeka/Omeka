abd3e95cc0cfcde618f8154a4dd83ba7bd01a8b4
Kim Nguyen <design@kimisgold.com>
2012-11-26 16:20:25 -0500

    Fix wrappers for sections, use ul for item list checkboxes.

--- admin/themes/default/items/batch-delete.php
+++ admin/themes/default/items/batch-delete.php
@@ -1,55 +1,55 @@
 <?php
 $title = __('Batch Delete Items');
 if (!$isPartial):
-    echo head(
-        array(
-            'title' => $title, 
-            'bodyclass' => 'items batch-delete'
-        )
-    );
-endif;
+    echo head(array('title' => $title, 
+               'bodyclass' => 'advanced-search', 
+               'bodyid' => 'advanced-search-page'));
 ?>
 
+<?php endif; ?>
+        
 <div title="<?php echo $title; ?>">
 
 <form id="batch-edit-form" action="<?php echo html_escape(url('items/batch-edit-save')); ?>" method="post" accept-charset="utf-8">
-    <section class="seven columns alpha">
-        <fieldset id="item-list" class="panel">
-            <h2 class="two columns alpha"><?php echo __('Items'); ?></h2>
-            <div class="five columns omega">
-                <div>
-                    <?php 
-                    $itemCheckboxes = array();
-                    $excludedItems = false;
-                    foreach ($itemIds as $id) {
-                        if (!($item = get_record_by_id('item', $id))) {
-                            continue;
-                        }
-                        
-                        if (is_allowed($item, 'delete')) {
-                            $itemCheckboxes[$id] = metadata($item, array('Dublin Core', 'Title'));
-                        } else {
-                            $excludedItems = true;
-                        }
-                        release_object($item);
-                    }
-                    echo $this->formMultiCheckbox('items[]', null, array('checked' => 'checked'), $itemCheckboxes);
-                    ?>
-                </div>
-                <?php if ($excludedItems): ?>
-                <p class="explanation"><?php echo __('Some items were excluded because you do not have permission to delete them.'); ?></p>
-                <?php endif; ?>
+
+    <div class="seven columns alpha">
+    <fieldset id="item-list" class="panel">
+        <h2 class="two columns alpha"><?php echo __('Items'); ?></h2>
+        <div class="five columns omega">
+            <ul>
+            <?php 
+            $itemCheckboxes = array();
+            $excludedItems = false;
+            foreach ($itemIds as $id) {
+                if (!($item = get_record_by_id('item', $id))) {
+                    continue;
+                }
                 
-                <p class="explanation"><?php echo __('Checked items will be deleted.'); ?></p>
-            </div>
-        </fieldset>
-    </section>
-    <section class="three columns omega">
-        <div id="save" class="panel">
+                if (is_allowed($item, 'delete')) {
+                    $itemCheckboxes[$id] = metadata($item, array('Dublin Core', 'Title'));
+                } else {
+                    $excludedItems = true;
+                }
+                release_object($item);
+            }
+            echo '<li>' . $this->formMultiCheckbox('items[]', null, array('checked' => 'checked'), $itemCheckboxes, '</li><li>') . '</li>'; ?>
+            </ul>
+            <?php if ($excludedItems): ?>
+            <p class="explanation"><?php echo __('Some items were excluded because you do not have permission to delete them.'); ?></p>
+            <?php endif; ?>
+            
+            <p class="explanation"><?php echo __('Checked items will be deleted.'); ?></p>
+    </fieldset>
+
+    <input type="hidden" name="delete" value="1">
+    
+    </div>
+
+    <div class="three columns omega">
+        <div  id="save" class="panel">
             <input type="submit" class="big red button" value="<?php echo __('Delete Items'); ?>">
         </div>
-    </section>
-    <input type="hidden" name="delete" value="1">
+    </div>
 
     <?php
     $hash = new Zend_Form_Element_Hash('batch_edit_hash');
@@ -58,8 +58,6 @@ endif;
     echo $hash;
     ?>
 </form>
-
-</div>
 <?php if (!$isPartial): ?>
 <?php echo foot(); ?>
 <?php endif; ?>
