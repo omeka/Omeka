8491b01ff6d5e8c78c5754efac878beb3f35c776
John Flatness <john@zerocrates.org>
2013-01-07 11:56:07 -0500

    Fix _log calls in backgrounded jobs.

--- application/scripts/background.php
+++ application/scripts/background.php
@@ -30,7 +30,7 @@ try {
 
 // Load a core set of resources.
 $application = new Omeka_Application(APPLICATION_ENV);
-$application->bootstrap(array('Autoloader', 'Config', 'Db', 'Options', 
+$application->bootstrap(array('Autoloader', 'Config', 'Db', 'Logger', 'Options',
                               'Pluginbroker', 'Plugins', 'Jobs', 'Storage', 'Mail',
 ));
 
