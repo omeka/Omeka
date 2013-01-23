06143954c6d5c5743a7ec1fb139e2316a3aa249b
Kim Nguyen <design@kimisgold.com>
2013-01-23 02:01:33 -0500

    Move item title heading outside of meta.

--- application/views/scripts/items/browse.php
+++ application/views/scripts/items/browse.php
@@ -26,10 +26,8 @@ $sortLinks[__('Date Added')] = 'added';
 
 <?php foreach (loop('items') as $item): ?>
 <div class="item hentry">
-    <div class="item-meta">
-
     <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
-
+    <div class="item-meta">
     <?php if (metadata('item', 'has thumbnail')): ?>
     <div class="item-img">
         <?php echo link_to_item(item_image('square_thumbnail')); ?>
