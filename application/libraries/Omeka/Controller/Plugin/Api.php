67e8c5cbd208b421bdbd2d9568db4d51d6f97695
Jim Safley <jimsafley@gmail.com>
2013-05-01 20:38:55 -0400

    Get API resources before checking for valid identity.

--- application/libraries/Omeka/Controller/Plugin/Api.php
+++ application/libraries/Omeka/Controller/Plugin/Api.php
@@ -77,14 +77,14 @@ class Omeka_Controller_Plugin_Api extends Zend_Controller_Plugin_Abstract
     {
         $front = Zend_Controller_Front::getInstance();
         
+        // Set the available API resources as a front controller param so they 
+        // are applied only once and globally accessible.
+        $front->setParam('api_resources', apply_filters('api_resources', $this->_apiResources));
+        
         // Throw an error if a key was given but there is no user identity.
         if (isset($_GET['key']) && !Zend_Auth::getInstance()->hasIdentity()) {
             throw new Omeka_Controller_Exception_403('Invalid key.');
         }
-        
-        // Set the available API resources as a front controller param so they 
-        // are applied only once and globally accessible.
-        $front->setParam('api_resources', apply_filters('api_resources', $this->_apiResources));
         
         // Set the API controller directories.
         $apiControllerDirectories = array();
