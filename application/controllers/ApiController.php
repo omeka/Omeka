306b4489da98818c87eacbcb10fa38f3164284f9
Jim Safley <jimsafley@gmail.com>
2013-06-11 12:32:08 -0400

    Add instructions at the API endpoint.

---
+++ application/controllers/ApiController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Omeka
+ * 
+ * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ */
+
+/**
+ * @package Omeka\Controller
+ */
+class ApiController extends Omeka_Controller_AbstractActionController
+{
+    public function indexAction()
+    {
+        if (is_admin_theme()) {
+            // There is no API endpoint on the admin theme.
+            $this->_helper->redirector('index', 'index');
+        }
+        $this->view->resource_url = Omeka_Record_Api_AbstractRecordAdapter::getResourceUrl('/resources');
+    }
+}
---
+++ application/views/scripts/api/index.php
@@ -0,0 +1,2 @@
+<p>This is the endpoint URL for the Omeka REST API. You can view available API 
+resources here: <a href="<?php echo $this->resource_url; ?>?pretty_print"><?php echo $this->resource_url; ?></a></p>
