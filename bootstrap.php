e957790173f1422fa3799622707ad8b00dec00d3
John Flatness <john@zerocrates.org>
2013-06-27 12:42:19 -0400

    Bump version to 2.0.4.

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@
  */
 
 // Define the current version of Omeka.
-define('OMEKA_VERSION', '2.0.3');
+define('OMEKA_VERSION', '2.0.4');
 
 // Define the application environment.
 if (!defined('APPLICATION_ENV')) {
