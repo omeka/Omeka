ef7b62476fbcefc685cbb95ce462e6d62974bb6d
Jim Safley <jimsafley@gmail.com>
2013-07-01 12:43:39 -0400

    Alter create table for plugins. Ref #492

--- application/schema/plugins.sql
+++ application/schema/plugins.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%plugins` (
   `id` int unsigned NOT NULL auto_increment,
   `name` varchar(255) collate utf8_unicode_ci NOT NULL,
   `active` tinyint NOT NULL,
-  `version` text collate utf8_unicode_ci NOT NULL,
+  `version` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `active_idx` (`active`)
