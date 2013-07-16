4a39028edb0c02417181b9b1529d3bab6f31e25f
Kim Nguyen <design@kimisgold.com>
2013-07-16 11:01:45 -0400

    Display item search filters for advanced item search.

--- application/views/scripts/items/browse.php
+++ application/views/scripts/items/browse.php
@@ -9,6 +9,8 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
     <?php echo public_nav_items(); ?>
 </nav>
 
+<?php echo item_search_filters(); ?>
+
 <?php echo pagination_links(); ?>
 
 <?php if ($total_results > 0): ?>
