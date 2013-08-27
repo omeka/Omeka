4f9cff72f6e02121397cfcdbb422442507b83706
patrickmj <patrickmjchnm@gmail.com>
2013-08-27 11:57:00 -0400

    update version and submodule inis

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@
  */
 
 // Define the current version of Omeka.
-define('OMEKA_VERSION', '2.1-rc1');
+define('OMEKA_VERSION', '2.1');
 
 // Define the application environment.
 if (!defined('APPLICATION_ENV')) {
