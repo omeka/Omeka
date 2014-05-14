557d204490b08f3cb6542db3a02854e0b91d48dd
John Flatness <john@zerocrates.org>
2014-05-13 21:34:34 -0400

    Set version to 2.2.

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@
  */
 
 // Define the current version of Omeka.
-define('OMEKA_VERSION', '2.2-dev');
+define('OMEKA_VERSION', '2.2');
 
 // Define the application environment.
 if (!defined('APPLICATION_ENV')) {
