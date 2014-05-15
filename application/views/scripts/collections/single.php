0e393118241d4aa83510e28fb673d6d5886cc86c
Kim Nguyen <design@kimisgold.com>
2014-05-15 16:29:39 -0400

    Use link_to() instead of link_to_collection().

--- application/views/scripts/collections/single.php
+++ application/views/scripts/collections/single.php
@@ -4,8 +4,8 @@
     $description = metadata($collection, array('Dublin Core', 'Description'), array('snippet' => 150));
     ?>
     <h3><?php echo link_to($this->collection, 'show', strip_formatting($title)); ?></h3>
-    <?php if ($collectionImage = record_image('collection', 'square_thumbnail')): ?>
-        <?php echo link_to_collection($collectionImage, array('class' => 'collection record-image')); ?>
+    <?php if ($collectionImage = record_image($collection, 'square_thumbnail')): ?>
+        <?php echo link_to($this->collection, 'show', $collectionImage, array('class' => 'collection record-image')); ?>
     <?php endif; ?>
     <?php if ($description): ?>
         <p class="collection-description"><?php echo $description; ?></p>
