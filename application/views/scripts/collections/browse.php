5a5ae0f6630dab1d39e9aa9bc81e0685de291b3c
John Flatness <john@zerocrates.org>
2014-05-28 17:30:21 -0400

    Make collection browse closer to item structure.

--- application/views/scripts/collections/browse.php
+++ application/views/scripts/collections/browse.php
@@ -25,18 +25,17 @@ $sortLinks[__('Date Added')] = 'added';
     <?php endif; ?>
 
     <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
-    <div class="element">
-        <h3><?php echo __('Description'); ?></h3>
-        <div class="element-text"><?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?></div>
+    <div class="collection-description">
+        <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?>
     </div>
     <?php endif; ?>
 
     <?php if ($collection->hasContributor()): ?>
-    <div class="element">
-        <h3><?php echo __('Contributors'); ?></h3>
-        <div class="element-text">
-            <p><?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?></p>
-        </div>
+    <div class="collection-contributors">
+        <p>
+        <strong><?php echo __('Contributors'); ?>:</strong>
+        <?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?>
+        </p>
     </div>
     <?php endif; ?>
 
