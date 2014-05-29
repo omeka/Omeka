e2b3d27f3c1c513827fee84fb672d964c9bf9c65
John Flatness <john@zerocrates.org>
2014-05-29 17:06:13 -0400

    Show placeholder thumbs for featured items.

--- application/views/scripts/items/single.php
+++ application/views/scripts/items/single.php
@@ -4,7 +4,7 @@
     $description = metadata($item, array('Dublin Core', 'Description'), array('snippet' => 150));
     ?>
     <h3><?php echo link_to($item, 'show', strip_formatting($title)); ?></h3>
-    <?php if (metadata($item, 'has thumbnail')) {
+    <?php if (metadata($item, 'has files')) {
         echo link_to_item(
             item_image('square_thumbnail', array(), 0, $item), 
             array('class' => 'image'), 'show', $item
