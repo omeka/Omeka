aefecbf615c4ed812de10f017e98bfb1aac3e2c7
John Flatness <john@zerocrates.org>
2014-06-02 15:44:52 -0400

    Add form tabs for files/edit.

--- admin/themes/default/files/edit.php
+++ admin/themes/default/files/edit.php
@@ -7,19 +7,24 @@ if ($fileTitle != '') {
 }
 $fileTitle = __('Edit File #%s', metadata('file', 'id')) . $fileTitle;
 
-queue_js_file(array('vendor/tiny_mce/tiny_mce', 'elements'));
+queue_js_file(array('vendor/tiny_mce/tiny_mce', 'elements', 'tabs'));
 echo head(array('title' => $fileTitle, 'bodyclass' => 'files edit'));
+include 'form-tabs.php';
 echo flash();
 ?>
 <form method="post" action="">
     <section class="seven columns alpha" id="edit-form">
         <?php echo file_markup($file); ?>
         <div id="file-metadata">
-            <?php foreach ($elementSets as $elementSet): ?>
-            <fieldset>
-                <h2><?php echo __($elementSet->name); ?></h2>    
-                <?php echo element_set_form($file, $elementSet->name); ?>
-            </fieldset>
+            <?php foreach ($tabs as $tabName => $tabContent): ?>
+            <?php if (!empty($tabContent)): ?>
+                <div id="<?php echo text_to_id(html_escape($tabName)); ?>-metadata">
+                    <fieldset class="set">
+                        <h2><?php echo html_escape(__($tabName)); ?></h2>
+                        <?php echo $tabContent; ?>
+                    </fieldset>
+                </div>
+            <?php endif; ?>
             <?php endforeach; ?>
         </div> <!-- end file-metadata div -->
         <?php fire_plugin_hook('admin_files_form', array('file' => $file, 'view' => $this)); ?>
@@ -36,6 +41,7 @@ echo flash();
 </form>
 <script type="text/javascript" charset="utf-8">
 jQuery(window).load(function () {
+    Omeka.Tabs.initialize();
     Omeka.wysiwyg({
         mode: "none",
         forced_root_block: ""
