38b9e401cd4538cfcfb751c756bc0ef0059d494e
Daniel Berthereau <Daniel.github@Berthereau.net>
2014-06-05 02:47:02 +0200

    Added table alias for SearchText query.

--- application/models/Table/SearchText.php
+++ application/models/Table/SearchText.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Omeka
- * 
+ *
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
@@ -13,7 +13,7 @@ class Table_SearchText extends Omeka_Db_Table
 {
     /**
      * Find search text by record.
-     * 
+     *
      * @param string $recordType
      * @param int $recordId
      * @return SearchText|null
@@ -25,56 +25,56 @@ class Table_SearchText extends Omeka_Db_Table
          $select->where('record_id = ?', $recordId);
          return $this->fetchObject($select);
     }
-    
+
     public function applySearchFilters($select, $params)
     {
         // Set the query string if not passed.
         if (!isset($params['query'])) {
             $params['query'] = '';
         }
-        
+
         // Set the query type if not passed.
         if (!isset($params['query_type'])) {
             $params['query_type'] = 'full_text';
         }
-        
+
         // Set the base select statement.
         $select->reset(Zend_Db_Select::COLUMNS);
         $select->columns(array('record_type', 'record_id', 'title'));
-        
+
         // Set the where clause according to the query type.
         if ('exact_match' == $params['query_type']) {
-            $where = '`text` LIKE ?';
+            $where = '`search_texts`.`text` LIKE ?';
             $params['query'] = "%{$params['query']}%";
         } else if ('boolean' == $params['query_type']) {
-            $where = 'MATCH (`text`) AGAINST (? IN BOOLEAN MODE)';
+            $where = 'MATCH (`search_texts`.`text`) AGAINST (? IN BOOLEAN MODE)';
         } else {
-            $where = 'MATCH (`text`) AGAINST (?)';
+            $where = 'MATCH (`search_texts`.`text`) AGAINST (?)';
         }
         $select->where($where, $params['query']);
-        
-        // Must fire the "search_sql" hook here instead of relying on the native 
-        // "search_text_browse_sql" hook to ensure that the subsequent WHERE 
-        // clauses are not reset. This hook can be used when a custom search 
+
+        // Must fire the "search_sql" hook here instead of relying on the native
+        // "search_text_browse_sql" hook to ensure that the subsequent WHERE
+        // clauses are not reset. This hook can be used when a custom search
         // strategy is added using the "search_query_types" filter.
         fire_plugin_hook('search_sql', array('select' => $select, 'params' => $params));
-        
+
         // Search only those record types that are configured to be searched.
         $searchRecordTypes = get_custom_search_record_types();
         if ($searchRecordTypes) {
-            $select->where('`record_type` IN (?)', array_keys($searchRecordTypes));
+            $select->where('`search_texts`.`record_type` IN (?)', array_keys($searchRecordTypes));
         }
-        
+
         // Search on an specific record type.
         if (isset($params['record_types'])) {
-            $select->where('`record_type` IN (?)', $params['record_types']);
+            $select->where('`search_texts`.`record_type` IN (?)', $params['record_types']);
         }
-        
+
         // Restrict access to private records.
         $showNotPublic = Zend_Registry::get('bootstrap')->getResource('Acl')
             ->isAllowed(current_user(), 'Search', 'showNotPublic');
         if (!$showNotPublic) {
-            $select->where('`public` = 1');
+            $select->where('`search_texts`.`public` = 1');
         }
     }
 }
