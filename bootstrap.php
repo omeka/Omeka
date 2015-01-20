32dd019520059ac3daa20ae6799c038a6c38620e
John Flatness <john@zerocrates.org>
2015-01-20 11:07:02 -0500

    Set version 2.3.

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@
  */
 
 // Define the current version of Omeka.
-define('OMEKA_VERSION', '2.2.2');
+define('OMEKA_VERSION', '2.3');
 
 // Define the application environment.
 if (!defined('APPLICATION_ENV')) {
