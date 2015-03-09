38720a3f5430ba9f78177716b9e326c433ec631c
John Flatness <john@zerocrates.org>
2015-03-09 13:09:28 -0400

    Remove redundant tabindex on fake dropdowns

--- admin/themes/default/items/quick-filters.php
+++ admin/themes/default/items/quick-filters.php
@@ -1,5 +1,5 @@
 <ul class="quick-filter-wrapper">
-    <li><a href="#" tabindex="0"><?php echo __('Quick Filter'); ?></a>
+    <li><a href="#"><?php echo __('Quick Filter'); ?></a>
     <ul class="dropdown">
         <li><span class="quick-filter-heading"><?php echo __('Quick Filter') ?></span></li>
         <li><a href="<?php echo url('items/browse'); ?>"><?php echo __('View All') ?></a></li>
