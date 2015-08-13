ec5e87bb6d9e209a9b0e00b3b83465114467f448
John Flatness <john@zerocrates.org>
2015-08-13 13:35:55 -0400

    Fix error on differing trailing spacing for tags

--- application/models/Table/Tag.php
+++ application/models/Table/Tag.php
@@ -17,7 +17,7 @@ class Table_Tag extends Omeka_Db_Table
         $sql = "
         SELECT tags.* 
         FROM {$db->Tag} tags
-        WHERE tags.name LIKE ?
+        WHERE tags.name = ?
         LIMIT 1";
         $tag = $this->fetchObject($sql, array($name));
         
