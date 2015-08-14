30a0f48620e65facbf9af22cca0ebec5c973e7c6
John Flatness <john@zerocrates.org>
2015-08-14 15:56:23 -0400

    Store session data in BLOB.
    Avoids problems with invalid-utf8 data that could otherwise be dropped

---
+++ application/migrations/20150814155100_storeSessionAsBlob.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Omeka
+ *
+ * @copyright Copyright 2007-2015 Roy Rosenzweig Center for History and New Media
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ */
+
+/**
+ * @package Omeka\Db\Migration
+ */
+class storeSessionAsBlob extends Omeka_Db_Migration_AbstractMigration
+{
+    public function up()
+    {
+        $sql = <<<SQL
+ALTER TABLE `{$this->db->Session}`
+MODIFY `data` BLOB
+SQL;
+        $this->db->query($sql);
+    }
+}
--- application/schema/sessions.sql
+++ application/schema/sessions.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%sessions` (
 `id` varchar(128),
 `modified` bigint,
 `lifetime` int,
-`data` text,
+`data` blob,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
