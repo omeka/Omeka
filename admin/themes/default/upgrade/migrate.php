1fe56952053034de358ff53473c5efce27dd9d0f
John Flatness <john@zerocrates.org>
2015-12-11 16:49:01 -0500

    Check PHP requirement at upgrade time

--- admin/themes/default/upgrade/migrate.php
+++ admin/themes/default/upgrade/migrate.php
@@ -24,11 +24,13 @@ endif;
             <p><?php echo link_to_admin_home_page(__('Return to Dashboard')); ?></p>
 <?php else: ?>
             <p class="error_text"><?php echo html_escape($error); ?></p>
+    <?php if (isset($exception)): ?>
             <pre id="backtrace"><?php echo utf8_htmlspecialchars($exception); ?></pre>
             <p class="instruction">
                 <?php echo __('Please restore from your database backup and try again.'); ?>
                 <?php echo __('If you have any questions please refer to <a href="http://omeka.org/codex">Omeka documentation</a> or post a message on the <a href="http://omeka.org/forums">Omeka forums</a>.'); ?>
             </p>
+    <?php endif; ?>
 <?php endif; ?>
         </section>
     </div>
