e707c22e29b08ca6a4456a7ec8830a8baebb44e0
John Flatness <john@zerocrates.org>
2016-01-05 17:11:03 -0500

    Add JS for fallback to plain link for media

--- application/views/scripts/javascripts/globals.js
+++ application/views/scripts/javascripts/globals.js
@@ -66,6 +66,12 @@ if (!Omeka) {
     };
 
     $(document).ready(function () {
-
+        $('.omeka-media').on('error', function () {
+            if (this.networkState === HTMLMediaElement.NETWORK_NO_SOURCE ||
+                this.networkState === HTMLMediaElement.NETWORK_EMPTY
+            ) {
+                $(this).replaceWith(this.innerHTML);
+            }
+        });
     });
 })(jQuery);
