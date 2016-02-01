a7e40993a53c2ee9fbbefaa5e97ff13e2a3a234c
John Flatness <john@zerocrates.org>
2016-02-01 15:37:45 -0500

    Switch to StandardAutoloader

--- bootstrap.php
+++ bootstrap.php
@@ -139,14 +139,11 @@ if (get_magic_quotes_gpc()) {
 // Add the libraries and models directories to the include path.
 set_include_path(LIB_DIR. PATH_SEPARATOR . MODEL_DIR . PATH_SEPARATOR . get_include_path());
 
-// Set up the Zend_Loader autoloader to work for all classes. The Omeka 
-// namespace must be manually specified to avoid incompatibility with the
-// resource autoloader.
-require_once 'Zend/Loader/Autoloader.php';
-$autoloader = Zend_Loader_Autoloader::getInstance();
-$autoloader->registerNamespace('Omeka_');
+// Set up the Zend autoloader to work for all classes.
+require_once 'Zend/Loader/StandardAutoloader.php';
+$autoloader = new Zend_Loader_StandardAutoloader;
 $autoloader->setFallbackAutoloader(true);
-$autoloader->suppressNotFoundWarnings(true);
+$autoloader->register();
 
 // Define the theme directory path.
 define('THEME_DIR', defined('ADMIN') ? ADMIN_THEME_DIR : PUBLIC_THEME_DIR);
