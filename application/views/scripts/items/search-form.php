a3edc789b9ce7e6ffae8b6a211d359bccb8e3143
Pasi Kallinen <pasi.kallinen@pttk.fi>
2016-05-10 13:00:50 +0300

    Add the missing id attributes to item search form

--- application/views/scripts/items/search-form.php
+++ application/views/scripts/items/search-form.php
@@ -103,7 +103,7 @@ $formAttributes['method'] = 'GET';
         </div>
     </div>
 
-    <div class="field">
+    <div id="search-by-collection" class="field">
         <?php echo $this->formLabel('collection-search', __('Search By Collection')); ?>
         <div class="inputs">
         <?php
@@ -117,7 +117,7 @@ $formAttributes['method'] = 'GET';
         </div>
     </div>
 
-    <div class="field">
+    <div id="search-by-type" class="field">
         <?php echo $this->formLabel('item-type-search', __('Search By Type')); ?>
         <div class="inputs">
         <?php
@@ -132,7 +132,7 @@ $formAttributes['method'] = 'GET';
     </div>
 
     <?php if(is_allowed('Users', 'browse')): ?>
-    <div class="field">
+    <div id="search-by-user" class="field">
     <?php
         echo $this->formLabel('user-search', __('Search By User'));?>
         <div class="inputs">
@@ -148,7 +148,7 @@ $formAttributes['method'] = 'GET';
     </div>
     <?php endif; ?>
 
-    <div class="field">
+    <div id="search-by-tags" class="field">
         <?php echo $this->formLabel('tag-search', __('Search By Tags')); ?>
         <div class="inputs">
         <?php
@@ -161,7 +161,7 @@ $formAttributes['method'] = 'GET';
 
 
     <?php if (is_allowed('Items','showNotPublic')): ?>
-    <div class="field">
+    <div id="search-by-public" class="field">
         <?php echo $this->formLabel('public', __('Public/Non-Public')); ?>
         <div class="inputs">
         <?php
@@ -179,7 +179,7 @@ $formAttributes['method'] = 'GET';
     </div>
     <?php endif; ?>
 
-    <div class="field">
+    <div id="search-by-featured" class="field">
         <?php echo $this->formLabel('featured', __('Featured/Non-Featured')); ?>
         <div class="inputs">
         <?php
