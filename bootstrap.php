c56ec56a0de82e799c169d6f14054967b4d79c49
Julian Maurice <julian.maurice@biblibre.com>
2017-03-31 09:02:43 +0200

    Change files.size type to bigint
    This allow to store files bigger than 4GiB

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@
  */
 
 // Define the current version of Omeka.
-define('OMEKA_VERSION', '2.5');
+define('OMEKA_VERSION', '2.5.1');
 
 // Define the application environment.
 if (!defined('APPLICATION_ENV')) {
