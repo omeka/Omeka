bebe707d0384be10632d915b71757dd0c13cfee3
patrickmj <patrickmjchnm@gmail.com>
2017-11-02 10:58:11 -0400

    add uninstall confirm message that data will go away
    fix #824

--- admin/themes/default/plugins/confirm-uninstall.php
+++ admin/themes/default/plugins/confirm-uninstall.php
@@ -10,6 +10,7 @@ echo flash();
     <?php endif; ?>
     <form action="<?php echo html_escape(url('plugins/uninstall')); ?>" method="post">
         <p><input type="checkbox" name="confirm" /> <?php echo __('Yes, I want to uninstall this plugin.'); ?></p>
+        <p><?php echo __('All data associated with this plugin will be permanently deleted from the database.'); ?>
         <input type="hidden" name="name" value="<?php echo html_escape($plugin->getDirectoryName()); ?>" />
 
         <p id="uninstall-confirm">
