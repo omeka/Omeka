e06b94c0f8733e5eafe5ec4bd350bb830b6dc3a3
Kim Nguyen <design@kimisgold.com>
2018-02-13 14:30:46 -0500

    Store item total as variable, pluralize link.
    (#827)

--- application/views/scripts/collections/show.php
+++ application/views/scripts/collections/show.php
@@ -1,5 +1,6 @@
 <?php
 $collectionTitle = metadata('collection', 'display_title');
+$totalItems = metadata('collection', 'total_items');
 ?>
 
 <?php echo head(array('title' => $collectionTitle, 'bodyclass' => 'collections show')); ?>
@@ -10,7 +11,7 @@ $collectionTitle = metadata('collection', 'display_title');
 
 <div id="collection-items">
     <h2><?php echo __('Collection Items'); ?></h2>
-    <?php if (metadata('collection', 'total_items') > 0): ?>
+    <?php if ($totalItems > 0): ?>
         <?php foreach (loop('items') as $item): ?>
         <?php $itemTitle = metadata('item', 'display_title'); ?>
         <div class="item hentry">
@@ -29,7 +30,7 @@ $collectionTitle = metadata('collection', 'display_title');
             <?php endif; ?>
         </div>
         <?php endforeach; ?>
-        <?php echo link_to_items_browse(__('View all %s items', metadata('collection', 'total_items')), array('collection' => metadata('collection', 'id')), array('class' => 'view-items-link')); ?>
+        <?php echo link_to_items_browse(__(plural('View item', 'View all %s items', $totalItems), $totalItems), array('collection' => metadata('collection', 'id')), array('class' => 'view-items-link')); ?>
     <?php else: ?>
         <p><?php echo __("There are currently no items within this collection."); ?></p>
     <?php endif; ?>
