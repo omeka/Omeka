33d9c2ce8022c08c531e73abb6dc0eb2f13c8b31
John Flatness <john@zerocrates.org>
2018-04-24 17:28:20 -0400

    Standardize tag display and linking

--- admin/themes/default/items/tag-list.php
+++ admin/themes/default/items/tag-list.php
@@ -2,7 +2,7 @@
     <ul class="tags">
         <?php foreach( $item->Tags as $key => $tag ): ?>
         <li class="tag">
-            <a href="<?php echo html_escape(url('items/browse/tag/' . urlencode($tag->name)));?>" rel="<?php echo html_escape($tag->id); ?>"><?php echo html_escape($tag); ?></a>
+            <a href="<?php echo html_escape(url('items/browse', array('tags' => $tag->name))); ?>"><?php echo html_escape($tag->name); ?></a>
         </li>
         <?php endforeach; ?>
     </ul>
--- admin/themes/default/tags/browse.php
+++ admin/themes/default/tags/browse.php
@@ -86,14 +86,14 @@ echo flash();
         <?php foreach ($tags as $tag): ?>
             <li>
             <?php if($browse_for == 'Item'):?>
-                <a href="<?php echo url('items/?tag=' . $tag->name); ?>" class="count"><?php echo $tag['tagCount']; ?></a>
+                <a href="<?php echo html_escape(url('items/browse', array('tags' => $tag->name)); ?>" class="count"><?php echo $tag['tagCount']; ?></a>
             <?php else: ?>
                 <span class="count"><?php echo $tag['tagCount']; ?></span>
             <?php endif; ?>
             <?php if ($canEdit): ?>
-                <span class="tag edit-tag" id="<?php echo $tag->id; ?>"><?php echo $tag->name; ?></span>
+                <span class="tag edit-tag" id="<?php echo $tag->id; ?>"><?php echo html_escape($tag->name); ?></span>
             <?php else: ?>
-                <span class="tag"><?php echo $tag->name; ?></span>
+                <span class="tag"><?php echo html_escape($tag->name); ?></span>
             <?php endif; ?>
             <?php if ($canDelete): ?>
                 <span class="delete-tag"><?php echo link_to($tag, 'delete-confirm', 'delete', array('class' => 'delete-confirm')); ?></span>
@@ -112,7 +112,7 @@ echo flash();
 <script type="text/javascript">
 jQuery(document).ready(function () {
     var editableURL = '<?php echo url('tags/rename-ajax'); ?>';
-    var tagURLBase = '<?php echo url('items/?tag='); ?>';
+    var tagURLBase = '<?php echo url('items/browse?tags='); ?>';
     var csrfToken = <?php echo js_escape($csrfToken); ?>;
     Omeka.Tags.enableEditInPlace(editableURL, tagURLBase, csrfToken);
 });
