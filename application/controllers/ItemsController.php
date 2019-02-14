70b447957499c03576fe0fb2c586b809953991f8
John Flatness <john@zerocrates.org>
2019-02-14 16:51:16 -0500

    Sort item tag-browse alpha by default

--- application/controllers/ItemsController.php
+++ application/controllers/ItemsController.php
@@ -153,7 +153,11 @@ class ItemsController extends Omeka_Controller_AbstractActionController
      */
     public function tagsAction()
     {
-        $params = array_merge($this->_getAllParams(), array('type' => 'Item'));
+        $params = array_merge(
+            array('sort_field' => 'name'),
+            $this->_getAllParams(),
+            array('type' => 'Item')
+        );
         $tags = $this->_helper->db->getTable('Tag')->findBy($params);
         $this->view->assign(compact('tags'));
     }
