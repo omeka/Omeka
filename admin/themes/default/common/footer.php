5cc0300ad72f3278e99da2b6ed9503cb60a32dd3
Kim Nguyen <design@kimisgold.com>
2019-03-13 15:27:52 -0400

    Add 'Back to top' link to footer.

--- admin/themes/default/common/footer.php
+++ admin/themes/default/common/footer.php
@@ -3,7 +3,7 @@
     <footer class="container" role="contentinfo">
         <div class="ten columns offset-by-two">
         <?php fire_plugin_hook('admin_footer', array('view'=>$this)); ?>
-        <p class="left"><a href="http://www.omeka.org" target="_blank"><?php echo __('Powered by Omeka') ?></a> | <a href="http://omeka.org/codex" target="_blank"><?php echo __('Documentation'); ?></a> | <a href="http://omeka.org/forums/" target="_blank"><?php echo __('Support Forums'); ?></a></p>
+        <p class="left"><a href="http://www.omeka.org" target="_blank"><?php echo __('Powered by Omeka') ?></a> | <a href="http://omeka.org/codex" target="_blank"><?php echo __('Documentation'); ?></a> | <a href="http://omeka.org/forums/" target="_blank"><?php echo __('Support Forums'); ?></a> | <a href="#content"><?php echo __('Back to top'); ?></a></p>
         
         <p class="right"><?php echo __('Version %s', OMEKA_VERSION); ?>
         <?php if (get_option('display_system_info') && is_allowed('SystemInfo', 'index')): ?>
