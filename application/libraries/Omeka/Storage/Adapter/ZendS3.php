3b4e9fd95b04879e1a2386d09d9621d6d37196ea
John Flatness <john@zerocrates.org>
2019-03-26 14:40:01 -0400

    Preserve slashes in S3 URLs

--- application/libraries/Omeka/Storage/Adapter/ZendS3.php
+++ application/libraries/Omeka/Storage/Adapter/ZendS3.php
@@ -166,7 +166,7 @@ class Omeka_Storage_Adapter_ZendS3 implements Omeka_Storage_Adapter_AdapterInter
     public function getUri($path)
     {
         $endpoint = isset($this->_force_ssl_display_endpoint) ? $this->_force_ssl_display_endpoint : $this->_s3->getEndpoint();
-        $object = urlencode($this->_getObjectName($path));
+        $object = str_replace('%2F', '/', rawurlencode($this->_getObjectName($path)));
 
         $uri = "$endpoint/$object";
 
