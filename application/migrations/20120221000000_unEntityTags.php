5f6b3a077ec35dd9c903fe95310e879577e39294
John Flatness <john@zerocrates.org>
2019-05-15 16:43:53 -0400

    Remove ALTER IGNORE from migration
    (fix #831)

--- application/migrations/20120221000000_unEntityTags.php
+++ application/migrations/20120221000000_unEntityTags.php
@@ -21,7 +21,14 @@ class unEntityTags extends Omeka_Db_Migration_AbstractMigration
     private function _updateSchema()
     {
         $this->db->query(<<<SQL
-ALTER IGNORE TABLE `{$this->db->Taggings}`
+DELETE t1 FROM `{$this->db->Taggings}` t1
+INNER JOIN `{$this->db->Taggings}` t2
+USING (relation_id, tag_id, type)
+WHERE t1.td > t2.id
+SQL
+);
+        $this->db->query(<<<SQL
+ALTER TABLE `{$this->db->Taggings}`
 DROP INDEX `tag`,
 DROP `entity_id`,
 ADD UNIQUE INDEX `tag` (`type`, `relation_id`, `tag_id`)
