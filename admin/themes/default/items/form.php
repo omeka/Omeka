d0850690bb1bdc6884513281372b021724a23216
Jeremy Guillette <jguillette@fas.harvard.edu>
2020-11-03 16:01:28 -0500

    Accessible labels for edit form inputs

--- admin/themes/default/items/form.php
+++ admin/themes/default/items/form.php
@@ -39,8 +39,8 @@ jQuery(document).bind('omeka:elementformload', function (event) {
         <?php if (!empty($tabContent)): ?>
             <div id="<?php echo text_to_id(html_escape($tabName)); ?>-metadata">
             <fieldset class="set">
-                <h2><?php echo html_escape(__($tabName)); ?></h2>
-                <?php echo $tabContent; ?>        
+              <legend><h2><?php echo html_escape(__($tabName)); ?></h2></legend>
+              <?php echo $tabContent; ?>
             </fieldset>
             </div>     
         <?php endif; ?>
