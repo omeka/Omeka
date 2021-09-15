30da478e8f64fba466fc5b1c4c9026fe7e6fe123
John Flatness <john@zerocrates.org>
2021-09-15 14:18:06 -0400

    Update admin footer links
    (fix #956)

--- admin/themes/default/common/footer.php
+++ admin/themes/default/common/footer.php
@@ -6,9 +6,9 @@
         <div class="ten columns offset-by-two">
         	<?php fire_plugin_hook('admin_footer', array('view'=>$this)); ?>
 			<p class="left">
-				<a href="http://www.omeka.org" target="_blank"><?php echo __('Powered by Omeka') ?></a>
-				<a href="http://omeka.org/codex" target="_blank"><?php echo __('Documentation'); ?></a>
-				<a href="http://omeka.org/forums/" target="_blank"><?php echo __('Support Forums'); ?></a>
+				<a href="https://omeka.org" target="_blank"><?php echo __('Powered by Omeka') ?></a>
+				<a href="https://omeka.org/classic/docs/" target="_blank"><?php echo __('Documentation'); ?></a>
+				<a href="https://forum.omeka.org" target="_blank"><?php echo __('Support Forums'); ?></a>
 				<a href="#content"><?php echo __('Back to top'); ?></a>
 			</p>
         
--- admin/themes/default/common/login-footer.php
+++ admin/themes/default/common/login-footer.php
@@ -3,7 +3,7 @@
         <footer class="login-footer">
             <?php fire_plugin_hook('admin_footer', array('view'=>$this)); ?>
             <p>
-                <a href="http://omeka.org" id="omeka-logo"><?php echo __('Powered by Omeka'); ?></a>
+                <a href="https://omeka.org" id="omeka-logo"><?php echo __('Powered by Omeka'); ?></a>
                 |
                 <?php echo __('Version %s', OMEKA_VERSION); ?>
             </p>
