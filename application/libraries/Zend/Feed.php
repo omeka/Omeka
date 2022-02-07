543905b28ea7cc1067df84c322ded6d27d7231ec
John Flatness <john@zerocrates.org>
2022-02-07 18:12:53 -0500

    PHP 8: track_errors/php_errormsg removal

--- application/libraries/Zend/Feed.php
+++ application/libraries/Zend/Feed.php
@@ -257,15 +257,15 @@ class Zend_Feed
      */
     public static function importFile($filename)
     {
-        @ini_set('track_errors', 1);
         $feed = @file_get_contents($filename);
-        @ini_restore('track_errors');
         if ($feed === false) {
+            $error = error_get_last();
+            $errormsg = $error ? $error['message'] : '';
             /**
              * @see Zend_Feed_Exception
              */
             require_once 'Zend/Feed/Exception.php';
-            throw new Zend_Feed_Exception("File could not be loaded: $php_errormsg");
+            throw new Zend_Feed_Exception("File could not be loaded: $errormsg");
         }
         return self::importString($feed);
     }
@@ -297,16 +297,17 @@ class Zend_Feed
         $contents = $response->getBody();
 
         // Parse the contents for appropriate <link ... /> tags
-        @ini_set('track_errors', 1);
         $pattern = '~(<link[^>]+)/?>~i';
         $result = @preg_match_all($pattern, $contents, $matches);
-        @ini_restore('track_errors');
         if ($result === false) {
+            $error = error_get_last();
+            $errormsg = $error ? $error['message'] : '';
+
             /**
              * @see Zend_Feed_Exception
              */
             require_once 'Zend/Feed/Exception.php';
-            throw new Zend_Feed_Exception("Internal error: $php_errormsg");
+            throw new Zend_Feed_Exception("Internal error: $errormsg");
         }
 
         // Try to fetch a feed for each link tag that appears to refer to a feed
