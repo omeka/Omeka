07803090226e89d508567ef55119c1e2357f3e93
John Flatness <john@zerocrates.org>
2022-03-08 15:31:21 -0500

    PHP 8.1: Fix low-hanging return type deprecations

--- application/libraries/Zend/Paginator/Adapter/DbSelect.php
+++ application/libraries/Zend/Paginator/Adapter/DbSelect.php
@@ -175,6 +175,7 @@ class Zend_Paginator_Adapter_DbSelect implements Zend_Paginator_Adapter_Interfac
      *
      * @return integer
      */
+    #[\ReturnTypeWillChange]
     public function count()
     {
         if ($this->_rowCount === null) {
