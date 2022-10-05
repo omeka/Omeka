7d3619313a9c8332ca885582a2696a53099354e3
Kim Nguyen <design@kimisgold.com>
2022-10-05 17:10:24 -0400

    Improve search form a11y labeling.
    * Provide icon spans, hidden from screen readers. In previous state, screen readers
      would sometimes read pseudo element content used for icons.
    * Provide aria labels for each form element.
    * Move markup from js to partial.

--- application/views/scripts/javascripts/globals.js
+++ application/views/scripts/javascripts/globals.js
@@ -18,13 +18,11 @@ if (!Omeka) {
     // Show advanced options for site-wide search.
     Omeka.showAdvancedForm = function () {
         var advanced_form = $('#advanced-form');
-        var show_advanced = '<a href="#" class="show-advanced button">Advanced Search</a>';
-        var search_submit = $('#search-form button');
 
         // Set up classes and DOM elements jQuery will use.
         if (advanced_form.length > 0) {
             $('#search-container').addClass('with-advanced');
-            advanced_form.addClass('closed').before(show_advanced);
+            advanced_form.addClass('closed');
         }
 
         $('.show-advanced').click(function(e) {
