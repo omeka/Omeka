782eb362a1259d81cdbcbe234988f04225bfe2a0
Kim Nguyen <design@kimisgold.com>
2022-10-11 16:55:40 -0400

    Fix variables and video markup.

--- application/views/helpers/Lightgallery.php
+++ application/views/helpers/Lightgallery.php
@@ -9,10 +9,11 @@ class Omeka_View_Helper_Lightgallery extends Zend_View_Helper_Abstract
     public function lightGallery($files = null, $supported = true) {
         
         $sortedMedia = $this->_prepareLightgalleryFiles($files);
+        $html = '';
 
         if ($supported) {
 
-            $html = '<div id="itemfiles">';
+            $html .= '<div id="itemfiles">';
             $mediaCaption = get_theme_option('media_caption');
 
             foreach ($sortedMedia['lightMedia'] as $index) {
@@ -51,20 +52,22 @@ class Omeka_View_Helper_Lightgallery extends Zend_View_Helper_Abstract
                         }
                     }
                     $videoSrcJson = json_encode($videoSrcObject);
-                    $videoThumbnail = ($file->hasThumbnail()) ? metadata($media, 'thumbnail_uri') : img('fallback-video.png');
-                    $html .=  '<div data-video="' . html_escape($videoSrcJson) . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape($videoThumbnail) . '" data-download-url="' . $source . '" class="media resource">';
+                    $videoThumbnail = ($media->hasThumbnail()) ? metadata($media, 'thumbnail_uri') : img('fallback-video.png');
+                    $html .= '<div data-video="' . html_escape($videoSrcJson) . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape($videoThumbnail) . '" data-download-url="' . $source . '" class="media resource">';
+                    $html .= '<img src="' . $videoThumbnail . '" alt="" role="presentation">';
                 } else if ($mediaType == 'application/pdf') {
-                    $html .=  '<div data-iframe="' . html_escape($source) . '" '. $mediaCaptionAttribute . 'data-src="' . $source . '" data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
+                    $html .= '<div data-iframe="' . html_escape($source) . '" '. $mediaCaptionAttribute . 'data-src="' . $source . '" data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
+                    $html .= file_markup($media);
                 } else {
                     $html .=  '<div data-src="' . $source . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
+                    $html .= file_markup($media);
                 }
-                $html .= file_markup($media);
                 $html .= '</div>';
             }
             $html .= '</div>';
-        } else {
+        } elseif (isset($sortedMedia['otherMedia'])) {
             $otherMedia = $sortedMedia['otherMedia'];
-            $html = '<div id="other-media" class="element">';
+            $html .= '<div id="other-media" class="element">';
             $html .= '<h3>' . get_view()->translate('Other Media') . '</h3>';
             foreach($otherMedia as $media) {
                 $html .= '<div class="element-text">' . link_to($media, null, metadata($media, 'display_title')) . '</div>';
@@ -87,7 +90,7 @@ class Omeka_View_Helper_Lightgallery extends Zend_View_Helper_Abstract
             if (in_array($mediaType, $whitelist)) {
                 $sortedMedia['lightMedia'][$mediaCount]['media'] = $media;
                 if (strpos($mediaType,'video') !== false) {
-                    $html5videos[$mediaCount] = pathinfo($file->filename, PATHINFO_FILENAME);
+                    $html5videos[$mediaCount] = pathinfo($media->filename, PATHINFO_FILENAME);
                     $sortedMedia['lightMedia'][$mediaCount]['tracks'] = [];
                 }
                 $mediaCount++;
