6c28fd79773c934eac519e0b69e220e1a984ec8b
Kim Nguyen <design@kimisgold.com>
2022-10-20 15:34:46 -0400

    Check if no lightgallery media is present.

--- application/views/helpers/Lightgallery.php
+++ application/views/helpers/Lightgallery.php
@@ -16,55 +16,58 @@ class Omeka_View_Helper_Lightgallery extends Zend_View_Helper_Abstract
             $html .= '<div id="itemfiles">';
             $mediaCaption = get_theme_option('media_caption');
 
-            foreach ($sortedMedia['lightMedia'] as $index) {
-                $media = $index['media'];
-                $source = $media->getProperty('uri');
-                $mediaCaptionOptions = [
-                    'none' => '',
-                    'title' => 'data-sub-html="' . metadata($media, 'display_title') . '"',
-                    'description' => 'data-sub-html="'. metadata($media, array('Dublin Core', 'Description')) . '"'
-                ];
-                $mediaCaptionAttribute = ($mediaCaption) ? $mediaCaptionOptions[$mediaCaption] : '';
-                $mediaType = ($media->mime_type == 'video/quicktime') ? 'video/mp4' : $media->mime_type;
-                if (strpos($mediaType, 'video') !== false) {
-                    $videoSrcObject = [
-                        'source' => [
-                            [
-                                'src' => $source, 
-                                'type' => $mediaType,
-                            ]
-                        ], 
-                        'attributes' => [
-                            'preload' => false, 
-                            'playsinline' => true, 
-                            'controls' => true,
-                        ],
+            if(isset($sortedMedia['lightMedia'])) {
+                
+                foreach ($sortedMedia['lightMedia'] as $index) {
+                    $media = $index['media'];
+                    $source = $media->getProperty('uri');
+                    $mediaCaptionOptions = [
+                        'none' => '',
+                        'title' => 'data-sub-html="' . metadata($media, 'display_title') . '"',
+                        'description' => 'data-sub-html="'. metadata($media, array('Dublin Core', 'Description')) . '"'
                     ];
-                    if (isset($index['tracks'])) {
-                        foreach ($index['tracks'] as $key => $track) {
-                            $label = metadata($track, 'display_title');
+                    $mediaCaptionAttribute = ($mediaCaption) ? $mediaCaptionOptions[$mediaCaption] : '';
+                    $mediaType = ($media->mime_type == 'video/quicktime') ? 'video/mp4' : $media->mime_type;
+                    if (strpos($mediaType, 'video') !== false) {
+                        $videoSrcObject = [
+                            'source' => [
+                                [
+                                    'src' => $source, 
+                                    'type' => $mediaType,
+                                ]
+                            ], 
+                            'attributes' => [
+                                'preload' => false, 
+                                'playsinline' => true, 
+                                'controls' => true,
+                            ],
+                        ];
+                        if (isset($index['tracks'])) {
+                            foreach ($index['tracks'] as $key => $track) {
+                                $label = metadata($track, 'display_title');
                                 $srclang = (metadata($track, array('Dublin Core', 'Language'))) ? metadata($track, array('Dublin Core', 'Language'), array('no_escape' => true)) : '';
                                 $type = (metadata($track, array('Dublin Core', 'Type'))) ? metadata($track, array('Dublin Core', 'Type'), array('no_escape' => true)) : 'captions';
-                            $videoSrcObject['tracks'][$key]['src'] = $track->getWebPath();
-                            $videoSrcObject['tracks'][$key]['label'] = $label;
-                            $videoSrcObject['tracks'][$key]['srclang'] = $srclang;
-                            $videoSrcObject['tracks'][$key]['kind'] = $type;
+                                $videoSrcObject['tracks'][$key]['src'] = $track->getWebPath();
+                                $videoSrcObject['tracks'][$key]['label'] = $label;
+                                $videoSrcObject['tracks'][$key]['srclang'] = $srclang;
+                                $videoSrcObject['tracks'][$key]['kind'] = $type;
+                            }
                         }
+                        $videoSrcJson = json_encode($videoSrcObject);
+                        $videoThumbnail = ($media->hasThumbnail()) ? metadata($media, 'thumbnail_uri') : img('fallback-video.png');
+                        $html .= '<div data-video="' . html_escape($videoSrcJson) . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape($videoThumbnail) . '" data-download-url="' . $source . '" class="media resource">';
+                        $html .= '<img src="' . $videoThumbnail . '" alt="" role="presentation">';
+                    } else if ($mediaType == 'application/pdf') {
+                        $html .= '<div data-iframe="' . html_escape($source) . '" '. $mediaCaptionAttribute . 'data-src="' . $source . '" data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
+                        $html .= file_markup($media);
+                    } else {
+                        $html .=  '<div data-src="' . $source . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
+                        $html .= file_markup($media);
                     }
-                    $videoSrcJson = json_encode($videoSrcObject);
-                    $videoThumbnail = ($media->hasThumbnail()) ? metadata($media, 'thumbnail_uri') : img('fallback-video.png');
-                    $html .= '<div data-video="' . html_escape($videoSrcJson) . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape($videoThumbnail) . '" data-download-url="' . $source . '" class="media resource">';
-                    $html .= '<img src="' . $videoThumbnail . '" alt="" role="presentation">';
-                } else if ($mediaType == 'application/pdf') {
-                    $html .= '<div data-iframe="' . html_escape($source) . '" '. $mediaCaptionAttribute . 'data-src="' . $source . '" data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
-                    $html .= file_markup($media);
-                } else {
-                    $html .=  '<div data-src="' . $source . '" ' . $mediaCaptionAttribute . 'data-thumb="' . html_escape(metadata($media, 'thumbnail_uri')) . '" data-download-url="' . $source . '" class="media resource">';
-                    $html .= file_markup($media);
+                    $html .= '</div>';
                 }
                 $html .= '</div>';
             }
-            $html .= '</div>';
         } elseif (isset($sortedMedia['otherMedia'])) {
             $otherMedia = $sortedMedia['otherMedia'];
             $html .= '<div id="other-media" class="element">';
