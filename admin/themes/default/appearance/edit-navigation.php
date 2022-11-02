2c3c1b3a02778c3edcda5427e3ac788d12dbc81f
Kim Nguyen <design@kimisgold.com>
2022-11-02 16:15:04 -0400

    Improve add navigation link panel aria labeling.

--- admin/themes/default/appearance/edit-navigation.php
+++ admin/themes/default/appearance/edit-navigation.php
@@ -16,14 +16,18 @@ echo head(array('title'=>$pageTitle, 'bodyclass'=>'settings')); ?>
     . 'in the main navigation. Click and drag the links into the preferred ' 
     . 'display order.'); ?></p>
     <?php echo $this->form->displayNavigationLinks(); ?>
+    <?php echo $this->partial('settings/edit-navigation-link.php', array('template' => true)); ?>
     <?php echo $this->form->getElement(Omeka_Form_Navigation::HIDDEN_ELEMENT_ID); ?>
-        <div class="add-new"><?php echo __('Add a Link to the Navigation'); ?></div>
-        <div class="drawer-contents opened">
-            <label for="new_nav_link_label"><?php echo __('Label'); ?></label>
-            <input type="text" id="new_nav_link_label" name="new_nav_link_label" />
-            <label for="new_nav_link_uri"><?php echo __('URL'); ?></label>
-            <input type="text" id="new_nav_link_uri" name="new_nav_link_uri" />
-            <a href="" id="new_nav_link_button_link" class="blue button"><?php echo __('Add Link'); ?></a>
+        <div class="add-new" id="add-new-heading"><?php echo __('Add a Link to the Navigation'); ?></div>
+        <div id="add-new-options" class="drawer-contents opened">
+            <div id="new-link-success" class="sr-only flash" style="display: none" aria-live="polite"><?php echo __('New Link Added. Total links: '); ?><span class="link-count"></span></div>
+            <label id="new_nav_link_label_label"><?php echo __('Label'); ?></label>
+            <input type="text" id="new_nav_link_label" aria-labelledby="add-new-heading new_nav_link_label_label" name="new_nav_link_label" />
+            <div class="flash alert" style="display: none;" id="label-required" aria-live="polite"><?php echo __('required field'); ?> <span class="sr-only"><?php echo __('Label'); ?></span></div>
+            <label id="new_nav_link_uri_label"><?php echo __('URL'); ?></label>
+            <input type="text" id="new_nav_link_uri" name="new_nav_link_uri" aria-labelledby="add-new-heading new_nav_link_uri_label" />
+            <div class="flash alert" style="display: none;" id="uri-required" aria-live="polite"><?php echo __('required field'); ?> <span class="sr-only"><?php echo __('URI'); ?></span></div>
+            <button type="button" id="new_nav_link_button_link" class="blue button"><?php echo __('Add Link'); ?></button>
         </div>
     <?php fire_plugin_hook('admin_appearance_navigation_form', array('form' => $form, 'view' => $this)); ?>
 </section>
