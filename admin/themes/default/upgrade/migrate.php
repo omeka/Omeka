0edc0be1685a4fcee470db7fb75fad967ac0c405
John Flatness <john@zerocrates.org>
2023-05-31 15:11:54 -0400

    Remove obsolete references to font Cabin

--- admin/themes/default/upgrade/migrate.php
+++ admin/themes/default/upgrade/migrate.php
@@ -13,7 +13,7 @@ endif;
     <link rel="stylesheet" media="all" href="<?php echo WEB_RELATIVE_VIEW_SCRIPTS . '/css/style.css'; ?>">
     <link rel="stylesheet" media="all" href="<?php echo WEB_RELATIVE_VIEW_SCRIPTS . '/css/skeleton.css'; ?>">
     <link rel="stylesheet" media="all" href="<?php echo WEB_RELATIVE_VIEW_SCRIPTS . '/css/layout.css'; ?>">
-    <link href='//fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic|Cabin:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Arvo:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
 
 <body id="upgrade">
