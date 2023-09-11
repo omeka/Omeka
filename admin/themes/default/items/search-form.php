f086957e5d7c82a3fa67667bcc8534a4a2d1db89
John Flatness <john@zerocrates.org>
2023-09-11 18:36:58 -0400

    Restore "is not exactly" search to admin form

--- admin/themes/default/items/search-form.php
+++ admin/themes/default/items/search-form.php
@@ -95,6 +95,7 @@ $formAttributes['method'] = 'GET';
                             'contains' => __('contains'),
                             'does not contain' => __('does not contain'),
                             'is exactly' => __('is exactly'),
+                            'is not exactly' => __('is not exactly'),
                             'is empty' => __('is empty'),
                             'is not empty' => __('is not empty'),
                             'starts with' => __('starts with'),
