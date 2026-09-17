f521d2b0e209af8a6729884dba1ff6b4be2c7842
matthewjmckinley <matthewjamesmckinley@gmail.com>
2026-09-17 11:04:46 -0700

    Update shared browse view scripts

--- application/views/scripts/collections/browse.php
+++ application/views/scripts/collections/browse.php
@@ -6,12 +6,8 @@ echo head(['title' => $pageTitle, 'bodyclass' => 'collections browse']);
 <h1><?php echo $pageTitle; ?> <?php echo __('(%s total)', $total_results); ?></h1>
 <?php echo pagination_links(['attributes' => ['aria-label' => __('Pagination')]]); ?>
 
-<?php
-$sortLinks[__('Title')] = 'Dublin Core,Title';
-$sortLinks[__('Date Added')] = 'added';
-?>
 <div id="sort-links">
-    <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
+    <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links_for('collections'); ?>
 </div>
 
 <?php foreach (loop('collections') as $collection): ?>
--- application/views/scripts/items/browse.php
+++ application/views/scripts/items/browse.php
@@ -15,13 +15,8 @@ echo head(['title' => $pageTitle, 'bodyclass' => 'items browse']);
 
 <?php if ($total_results > 0): ?>
 
-<?php
-$sortLinks[__('Title')] = 'Dublin Core,Title';
-$sortLinks[__('Creator')] = 'Dublin Core,Creator';
-$sortLinks[__('Date Added')] = 'added';
-?>
 <div id="sort-links">
-    <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
+    <span class="sort-label"><?php echo __('Sort by: '); ?></span><?php echo browse_sort_links_for('items'); ?>
 </div>
 
 <?php endif; ?>
